14c8b36c23de764f1c3227c72936e24b117b5b4d
Greg Kapka <greg@oraclize.it>
2020-03-14 10:46:32 +0000

    feat(btc-on-eos): factor out init utils from EOS initializer

---
+++ src/btc_on_eos/eos/initialize_eos/eos_init_utils.rs
@@ -0,0 +1,67 @@
+use crate::btc_on_eos::{
+    types::Result,
+    traits::DatabaseInterface,
+    eos::{
+        eos_state::EosState,
+        eos_types::ProcessedTxIds,
+        eos_database_utils::{
+            put_eos_chain_id_in_db,
+            put_eos_account_name_in_db,
+            put_eos_token_ticker_in_db,
+            put_processed_tx_ids_in_db,
+        },
+    },
+};
+
+pub fn get_eos_init_output<D>(
+    _state: EosState<D>
+) -> Result<String>
+    where D: DatabaseInterface
+{
+    Ok("{eos_core_initialized:true}".to_string())
+}
+
+pub fn put_eos_account_name_in_db_and_return_state<D>(
+    account_name: String,
+    state: EosState<D>,
+) -> Result<EosState<D>>
+    where D: DatabaseInterface
+{
+    put_eos_account_name_in_db(&state.db, &account_name)
+        .map(|_| state)
+}
+
+pub fn put_eos_token_ticker_in_db_and_return_state<D>(
+    token_ticker: String,
+    state: EosState<D>,
+) -> Result<EosState<D>>
+    where D: DatabaseInterface
+{
+    put_eos_token_ticker_in_db(&state.db, &token_ticker)
+        .map(|_| state)
+}
+
+pub fn put_empty_processed_tx_ids_in_db_and_return_state<D>(
+    state: EosState<D>,
+) -> Result<EosState<D>>
+    where D: DatabaseInterface
+{
+    put_processed_tx_ids_in_db(
+        &state.db,
+        &ProcessedTxIds::init()
+    )
+        .map(|_| state)
+}
+
+pub fn put_eos_chain_id_in_db_and_return_state<D>(
+    chain_id: String,
+    state: EosState<D>
+) -> Result<EosState<D>>
+    where D: DatabaseInterface
+{
+    put_eos_chain_id_in_db(
+        &state.db,
+        &chain_id,
+    )
+        .map(|_| state)
+}
--- src/btc_on_eos/eos/initialize_eos/initialize_eos_core.rs
+++ src/btc_on_eos/eos/initialize_eos/initialize_eos_core.rs
@@ -1,76 +1,25 @@
 use crate::btc_on_eos::{
     types::Result,
     traits::DatabaseInterface,
-    utils::convert_hex_to_checksum256,
     eos::{
         eos_state::EosState,
         eos_types::ProcessedTxIds,
-        initialize_eos::is_eos_core_initialized::is_eos_core_initialized,
         eos_database_utils::{
-            put_eos_chain_id_in_db,
             end_eos_db_transaction,
             start_eos_db_transaction,
-            put_eos_account_name_in_db,
-            put_eos_token_ticker_in_db,
-            put_processed_tx_ids_in_db,
-            get_processed_tx_ids_from_db,
+        },
+        initialize_eos::{
+            is_eos_core_initialized::is_eos_core_initialized,
+            eos_init_utils::{
+                get_eos_init_output,
+                put_eos_chain_id_in_db_and_return_state,
+                put_eos_account_name_in_db_and_return_state,
+                put_eos_token_ticker_in_db_and_return_state,
+                put_empty_processed_tx_ids_in_db_and_return_state,
+            },
         },
     },
 };
-
-// TODO move to init_utils
-fn get_eos_init_output<D>(
-    _state: EosState<D>
-) -> Result<String>
-    where D: DatabaseInterface
-{
-    Ok("{eos_core_initialized:true}".to_string())
-}
-
-fn put_eos_account_name_in_db_and_return_state<D>(
-    account_name: String,
-    state: EosState<D>,
-) -> Result<EosState<D>>
-    where D: DatabaseInterface
-{
-    put_eos_account_name_in_db(&state.db, &account_name)
-        .map(|_| state)
-}
-
-fn put_eos_token_ticker_in_db_and_return_state<D>(
-    token_ticker: String,
-    state: EosState<D>,
-) -> Result<EosState<D>>
-    where D: DatabaseInterface
-{
-    put_eos_token_ticker_in_db(&state.db, &token_ticker)
-        .map(|_| state)
-}
-
-fn put_empty_processed_tx_ids_in_db_and_return_state<D>(
-    state: EosState<D>,
-) -> Result<EosState<D>>
-    where D: DatabaseInterface
-{
-    put_processed_tx_ids_in_db(
-        &state.db,
-        &ProcessedTxIds::init()
-    )
-        .map(|_| state)
-}
-
-pub fn put_eos_chain_id_in_db_and_return_state<D>(
-    chain_id: String,
-    state: EosState<D>
-) -> Result<EosState<D>>
-    where D: DatabaseInterface
-{
-    put_eos_chain_id_in_db(
-        &state.db,
-        &convert_hex_to_checksum256(&chain_id)?,
-    )
-        .map(|_| state)
-}
 
 pub fn maybe_initialize_eos_core<D>(
     db: D,
@@ -88,10 +37,7 @@ pub fn maybe_initialize_eos_core<D>(
         }
         false => {
             info!("✔ Initializing core for EOS...");
-            get_processed_tx_ids_from_db(&db)
-                .and_then(|tx_ids|
-                    start_eos_db_transaction(EosState::init(db, tx_ids))
-                )
+            start_eos_db_transaction(EosState::init(db, ProcessedTxIds::init()))
                 .and_then(|state| {
                     put_empty_processed_tx_ids_in_db_and_return_state(state)
                 })
