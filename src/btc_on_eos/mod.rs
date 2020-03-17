7c85f7bc621b4c33ae4e0b531e629183f82fd1e5
Greg Kapka <greg@oraclize.it>
2020-03-17 19:10:30 +0000

    feat(btc-on-eos): adds enclave getter mod for BTC side

--- src/btc_on_eos/mod.rs
+++ src/btc_on_eos/mod.rs
@@ -11,5 +11,6 @@ pub mod crypto_utils;
 pub mod utxo_manager;
 pub mod database_utils;
 pub mod check_debug_mode;
+pub mod get_enclave_state;
 pub mod get_latest_block_numbers;
 pub mod check_core_is_initialized;
