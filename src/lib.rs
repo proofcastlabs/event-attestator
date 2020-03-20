97a7670f313311b42f866f9da10acddd75c5e0d3
Greg Kapka <greg@oraclize.it>
2020-03-20 12:30:16 +0000

    feat(btc-on-eos): adds test dependencies for logging

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod btc_on_eos;
 #[macro_use] extern crate log;
 #[macro_use] extern crate lazy_static;
 #[macro_use] extern crate serde_derive;
+#[cfg(test)] extern crate simple_logger;
 
 // TODO Refactor the app since the import structure can no longer be flat.
 // TODO ||, expose only the API we want.
