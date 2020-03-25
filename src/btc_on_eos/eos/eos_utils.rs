3a95c3e7e5b19161db0425fd78c08a4e361f7f8a
Greg Kapka <greg@oraclize.it>
2020-03-25 14:53:00 +0000

    feat(btc-on-eos): rm all usize conversions w/r/t EosNetwork enum

--- src/btc_on_eos/eos/eos_utils.rs
+++ src/btc_on_eos/eos/eos_utils.rs
@@ -8,10 +8,6 @@ use crate::btc_on_eos::{
         Bytes,
         Result,
     },
-    utils::{
-        convert_usize_to_bytes,
-        convert_bytes_to_usize,
-    },
 };
 
 pub fn safely_convert_string_to_eos_account(
@@ -41,34 +37,5 @@ pub fn safely_convert_string_to_eos_account(
                 )
             }
         }
-    }
-}
-
-pub fn convert_eos_network_to_bytes(network: &EosNetwork) -> Result<Bytes> {
-    match network {
-        EosNetwork::Mainnet => Ok(convert_usize_to_bytes(&0)),
-        EosNetwork::Testnet => Ok(convert_usize_to_bytes(&1)),
-    }
-}
-
-pub fn convert_bytes_to_eos_network(bytes: &Bytes) -> Result<EosNetwork> {
-    match convert_bytes_to_usize(bytes)? {
-        1 => Ok(EosNetwork::Testnet),
-        _ => Ok(EosNetwork::Mainnet),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn should_serde_eos_network() {
-        let eos_network = EosNetwork::Mainnet;
-        let bytes = convert_eos_network_to_bytes(&eos_network)
-            .unwrap();
-        let result = convert_bytes_to_eos_network(&bytes)
-            .unwrap();
-        assert_eq!(result, eos_network);
     }
 }
