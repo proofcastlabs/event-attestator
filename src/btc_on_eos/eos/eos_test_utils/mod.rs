671d414d3ca93aacd02eb50a9c997d5243cb521d
Greg Kapka <greg@oraclize.it>
2020-03-30 14:52:48 +0100

    feat(btc-on-eos): adds `blockroot_merkles` field to EOS samples

--- src/btc_on_eos/eos/eos_test_utils/mod.rs
+++ src/btc_on_eos/eos/eos_test_utils/mod.rs
@@ -47,6 +47,9 @@ use crate::btc_on_eos::{
     },
 };
 
+pub const SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_1: &str =
+    "src/btc_on_eos/eos/eos_test_utils/eos-block-81784220.json";
+
 pub const SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_2: &str =
     "src/btc_on_eos/eos/eos_test_utils/eos-block-80440580.json";
 
@@ -55,9 +58,6 @@ pub const SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_3: &str =
 
 pub const SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_4: &str =
     "src/btc_on_eos/eos/eos_test_utils/eos-block-81772484.json";
-
-pub const SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_5: &str =
-    "src/btc_on_eos/eos/eos_test_utils/eos-block-81784220.json";
 
 pub const EOS_JUNGLE_CHAIN_ID: &str =
     "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473";
@@ -94,10 +94,10 @@ pub fn get_sample_eos_submission_material_string_n(
     num: usize,
 ) -> Result<String> {
     let path = match num {
+        1 => Ok(SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_1),
         2 => Ok(SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_2),
         3 => Ok(SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_3),
         4 => Ok(SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_4),
-        5 => Ok(SAMPLE_EOS_BLOCK_AND_ACTION_JSON_PATH_5),
         _ => Err(AppError::Custom(
             format!("Cannot find sample block num: {}", num)
         ))
