3a6f6f05d4675244d4ff3b5ec901ae92baad625a
Bogdan Arabadzhi <bogdan.today@gmail.com>
2020-03-31 12:42:15 +0200

    feat(eos-action-proof-validation): add filter_proofs_with_wrong_action_mroot tests

--- src/btc_on_eos/eos/filter_proofs_with_wrong_action_mroot.rs
+++ src/btc_on_eos/eos/filter_proofs_with_wrong_action_mroot.rs
@@ -36,3 +36,37 @@ pub fn maybe_filter_out_proofs_with_wrong_action_mroot<D>(
     )
         .and_then(|proofs| state.replace_action_proofs(proofs))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::btc_on_eos::{
+        eos::eos_test_utils::get_sample_action_proof_n,
+        utils::convert_hex_to_checksum256,
+    };
+
+    #[test]
+    fn should_not_filter_proofs_with_valid_merkle_proofs() {
+        let action_proofs = vec![
+            get_sample_action_proof_n(5),
+            get_sample_action_proof_n(5),
+            get_sample_action_proof_n(5),
+        ];
+        let action_mroot = convert_hex_to_checksum256("6ba2320b7d71d69770735f92b22f0d986d7e5d72f8842fa93b5604c63dd515c7").unwrap();
+        let result = filter_proofs_with_wrong_action_mroot(&action_mroot, &action_proofs).unwrap();
+
+        assert_eq!(result, action_proofs);
+    }
+
+    #[test]
+    fn should_filter_proofs_with_invalid_merkle_proofs() {
+        let action_proofs = vec![
+            get_sample_action_proof_n(4),
+            get_sample_action_proof_n(5),
+        ];
+        let action_mroot = convert_hex_to_checksum256("10c0518e15ae178bdd622e3f31249f0f12071c68045dd565a267a522df8ba96c").unwrap();
+        let result = filter_proofs_with_wrong_action_mroot(&action_mroot, &action_proofs).unwrap();
+
+        assert_eq!(result, [get_sample_action_proof_n(4)]);
+    }
+}
