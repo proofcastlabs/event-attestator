76322b9f6d1335e0dbf5d538034e440844d2e8de
Greg Kapka <greg@oraclize.it>
2020-04-06 16:08:53 +0100

    feat(btc-on-eos): adds EOS schedule saving step in EOS initter

--- src/btc_on_eos/eos/initialize_eos/eos_init_utils.rs
+++ src/btc_on_eos/eos/initialize_eos/eos_init_utils.rs
@@ -5,7 +5,9 @@ use crate::btc_on_eos::{
         eos_state::EosState,
         eos_types::ProcessedTxIds,
         eos_crypto::eos_private_key::EosPrivateKey,
+        parse_submission_material::parse_producer_schedule_from_json_string,
         eos_database_utils::{
+            put_eos_schedule_in_db,
             put_eos_chain_id_in_db,
             put_eos_private_key_in_db,
             put_eos_account_name_in_db,
@@ -16,6 +18,17 @@ use crate::btc_on_eos::{
     },
 };
 
+pub fn put_eos_schedule_in_db_and_return_state<D>(
+    schedule_json: &String,
+    state: EosState<D>,
+) -> Result<EosState<D>>
+    where D: DatabaseInterface
+{
+    parse_producer_schedule_from_json_string(schedule_json)
+        .and_then(|schedule| put_eos_schedule_in_db(&state.db, &schedule))
+        .and(Ok(state))
+}
+
 pub fn generated_eos_key_save_in_db_and_return_state<D>(
     state: EosState<D>,
 ) -> Result<EosState<D>>
@@ -25,7 +38,7 @@ pub fn generated_eos_key_save_in_db_and_return_state<D>(
         &state.db,
         &EosPrivateKey::generate_random()?,
     )
-        .map(|_| state)
+        .and(Ok(state))
 }
 
 pub fn get_eos_init_output<D>(
@@ -43,7 +56,7 @@ pub fn put_eos_account_name_in_db_and_return_state<D>(
     where D: DatabaseInterface
 {
     put_eos_account_name_in_db(&state.db, &account_name)
-        .map(|_| state)
+        .and(Ok(state))
 }
 
 pub fn put_eos_account_nonce_in_db_and_return_state<D>(
@@ -52,7 +65,7 @@ pub fn put_eos_account_nonce_in_db_and_return_state<D>(
     where D: DatabaseInterface
 {
     put_eos_account_nonce_in_db(&state.db, 0)
-        .map(|_| state)
+        .and(Ok(state))
 }
 
 pub fn put_eos_token_symbol_in_db_and_return_state<D>(
@@ -62,7 +75,7 @@ pub fn put_eos_token_symbol_in_db_and_return_state<D>(
     where D: DatabaseInterface
 {
     put_eos_token_symbol_in_db(&state.db, &token_symbol)
-        .map(|_| state)
+        .and(Ok(state))
 }
 
 pub fn put_empty_processed_tx_ids_in_db_and_return_state<D>(
@@ -74,7 +87,7 @@ pub fn put_empty_processed_tx_ids_in_db_and_return_state<D>(
         &state.db,
         &ProcessedTxIds::init()
     )
-        .map(|_| state)
+        .and(Ok(state))
 }
 
 pub fn put_eos_chain_id_in_db_and_return_state<D>(
@@ -87,5 +100,5 @@ pub fn put_eos_chain_id_in_db_and_return_state<D>(
         &state.db,
         &chain_id,
     )
-        .map(|_| state)
+        .and(Ok(state))
 }
