227d53f33906ca2a764aa87763bb0fbd55b2c3f6
Greg Kapka <greg@oraclize.it>
2020-04-15 18:13:35 +0100

    fix(btc-on-eos): rm unused imports in EOS debug fxns

--- src/btc_on_eos/debug_functions/mod.rs
+++ src/btc_on_eos/debug_functions/mod.rs
@@ -1,18 +1,12 @@
-#![allow(dead_code)] // TODO rm!
-#![allow(unused_imports)] // TODO rm!
 use crate::{
     types::Result,
     traits::DatabaseInterface,
     check_debug_mode::check_debug_mode,
     btc_on_eos::{
+        check_core_is_initialized::check_core_is_initialized,
         utxo_manager::utxo_database_utils::{
             get_utxo_from_db,
             get_all_utxo_db_keys,
-        },
-        check_core_is_initialized::{
-            check_core_is_initialized,
-            check_core_is_initialized_and_return_eos_state,
-            check_core_is_initialized_and_return_btc_state,
         },
         eos::{
             eos_database_utils::put_eos_schedule_in_db,
@@ -20,31 +14,8 @@ use crate::{
             parse_submission_material::parse_producer_schedule_from_json_string,
         },
         btc::{
-            btc_state::BtcState,
             btc_types::BtcUtxoAndValue,
-            save_utxos_to_db::maybe_save_utxos_to_db,
-            filter_utxos::maybe_filter_utxos_in_state,
             btc_constants::BTC_PRIVATE_KEY_DB_KEY as BTC_KEY,
-            validate_btc_merkle_root::validate_btc_merkle_root,
-            filter_minting_params::maybe_filter_minting_params_in_state,
-            validate_btc_block_header::validate_btc_block_header_in_state,
-            filter_p2sh_deposit_txs::filter_p2sh_deposit_txs_and_add_to_state,
-            btc_database_utils::{
-                end_btc_db_transaction,
-                start_btc_db_transaction,
-            },
-            get_deposit_info_hash_map::{
-                get_deposit_info_hash_map_and_put_in_state,
-            },
-            validate_btc_proof_of_work::{
-                validate_proof_of_work_of_btc_block_in_state,
-            },
-            extract_utxos_from_p2sh_txs::{
-                maybe_extract_utxos_from_p2sh_txs_and_put_in_state
-            },
-            parse_minting_params_from_p2sh_deposits::{
-                parse_minting_params_from_p2sh_deposits_and_add_to_state,
-            },
         },
     },
 };
@@ -64,65 +35,6 @@ pub fn debug_add_new_eos_schedule<D>(
         .and_then(|_| db.end_transaction())
         .map(|_| "{debug_adding_eos_schedule_succeeded:true}".to_string())
 }
-
-/*
-pub fn debug_reprocess_btc_block<D>(
-    db: D,
-    btc_block_json: String,
-) -> Result<String>
-    where D: DatabaseInterface
-{
-    parse_btc_block_and_id_and_put_in_state(
-        btc_block_json,
-        BtcState::init(db),
-    )
-        .and_then(check_core_is_initialized_and_return_btc_state)
-        .and_then(start_btc_db_transaction)
-        .and_then(validate_btc_block_header_in_state)
-        .and_then(validate_proof_of_work_of_btc_block_in_state)
-        .and_then(validate_btc_merkle_root)
-        .and_then(get_deposit_info_hash_map_and_put_in_state)
-        .and_then(filter_op_return_deposit_txs_and_add_to_state)
-        .and_then(filter_p2sh_deposit_txs_and_add_to_state)
-        .and_then(parse_minting_params_from_op_return_deposits_and_add_to_state)
-        .and_then(parse_minting_params_from_p2sh_deposits_and_add_to_state)
-        .and_then(maybe_extract_utxos_from_op_return_txs_and_put_in_state)
-        .and_then(maybe_extract_utxos_from_p2sh_txs_and_put_in_state)
-        .and_then(maybe_filter_utxos_in_state)
-        .and_then(maybe_save_utxos_to_db)
-        .and_then(maybe_filter_minting_params_in_state)
-        .and_then(|state| {
-            get_eth_signed_txs(
-                &get_signing_params_from_db(&state.db)?,
-                &state.minting_params,
-            )
-                .and_then(|signed_txs| state.add_eth_signed_txs(signed_txs))
-        })
-        .and_then(maybe_increment_eth_nonce_in_db)
-        .and_then(|state| {
-            let signatures = serde_json::to_string(
-                &match &state.eth_signed_txs {
-                    None => Ok(vec![]),
-                    Some(txs) =>
-                        get_eth_signed_tx_info_from_eth_txs(
-                            txs,
-                            &state.minting_params,
-                            get_eth_account_nonce_from_db(&state.db)?,
-                        )
-                }?
-            )?;
-            info!("✔ BTC signatures: {}", signatures);
-            state.add_output_json_string(signatures)
-        })
-        .and_then(end_btc_db_transaction)
-        .map(|state|
-            match state.output_json_string {
-                None => format!("✘ No signatures signed ∴ no output!"),
-                Some(output) => output
-            }
-        )
-}
-*/
 
 pub fn debug_set_key_in_db_to_value<D>(
     db: D,
