69f4ddbe3ab0fa4b7838dfefdd06a54e45244a3e
Greg Kapka <greg@oraclize.it>
2020-04-24 12:26:20 +0100

    ref(btc-on-eos): udpate all BTC constants to be different from `pbtc-on-eth` ones

--- src/btc_on_eos/btc/btc_constants.rs
+++ src/btc_on_eos/btc/btc_constants.rs
@@ -12,133 +12,109 @@ pub const DEFAULT_BTC_ADDRESS: &str =
     "msTgHeQgPZ11LRcUdtfzagEfiZyKF57DhR";
 
 pub const DEFAULT_BTC_SEQUENCE: u32 = 4_294_967_295; // NOTE: 0xFFFFFFFF
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-difficulty').slice(2), 'hex')
-// )
-// 0ed532c16cd0bcc543cdcd01132c38349fd25e85b2d7f4609b66943bc8500a7c
+
+// NOTE: Keccak256 hash of: 'btc-on-eos-btc-difficulty'
+// 84f5182962ac8586f52ed82dd741b60277a36fa62bd712b2edb293aa0d299a8a
 pub static BTC_DIFFICULTY_THRESHOLD: [u8; 32] = [
-  14, 213, 50, 193, 108, 208, 188, 197,
-  67, 205, 205, 1, 19, 44, 56, 52,
-  159, 210, 94, 133, 178, 215, 244, 96,
-  155, 102, 148, 59, 200, 80, 10, 124
+     132, 245, 24, 41, 98, 172, 133, 134,
+     245, 46, 216, 45, 215, 65, 182, 2,
+     119, 163, 111, 166, 43, 215, 18, 178,
+     237, 178, 147, 170, 13, 41, 154, 138,
+
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-address').slice(2), 'hex')
-// )
-// bdf6e75595f2a65ce048e0416b8c2a8462288116db886b551b2891adceb0a53a
+// NOTE: Keccak256 hash of: 'btc-on-eos-btc-address-key'
+// 3c7be3aeea8a5e50c65b1d915e9f4134e81ea00150abca6af6a7f93342b20a70
 pub static BTC_ADDRESS_KEY: [u8; 32] = [
-  189, 246, 231, 85, 149, 242, 166, 92,
-  224, 72, 224, 65, 107, 140, 42, 132,
-  98, 40, 129, 22, 219, 136, 107, 85,
-  27, 40, 145, 173, 206, 176, 165, 58
+     60, 123, 227, 174, 234, 138, 94, 80,
+     198, 91, 29, 145, 94, 159, 65, 52,
+     232, 30, 160, 1, 80, 171, 202, 106,
+     246, 167, 249, 51, 66, 178, 10, 112,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-canon-block').slice(2), 'hex')
-// )
-// ed228247ba940027aa9406ef39c2aa07f650bfa53f0b8478f2d90836615912b8
+// NOTE: Keccak256 hash of: 'btc-on-eos-btc-canon-block'
+// 3b9f77d819a54fa0efec77baa53e4d1f3f578a9489668b9b0400324f23298efb
 pub static BTC_CANON_BLOCK_HASH_KEY: [u8; 32] = [
-  237, 34, 130, 71, 186, 148, 0, 39,
-  170, 148, 6, 239, 57, 194, 170, 7,
-  246, 80, 191, 165, 63, 11, 132, 120,
-  242, 217, 8, 54, 97, 89, 18, 184
+     59, 159, 119, 216, 25, 165, 79, 160,
+     239, 236, 119, 186, 165, 62, 77, 31,
+     63, 87, 138, 148, 137, 102, 139, 155,
+     4, 0, 50, 79, 35, 41, 142, 251,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-latest-block').slice(2), 'hex')
-// )
-// 22f781fdf51ac53605f603b9abeaddd618d29eb7ebed285a919abf128379a0a2
+// NOTE: Keccak256 hash of: 'btc-on-eos-btc-latest-block'
+// 1ffbbc434bbd4c52f1399e39ac2e98bdeb8ecfaacabd0ac1de507a76a23dc001
 pub static BTC_LATEST_BLOCK_HASH_KEY: [u8; 32] = [
-  34, 247, 129, 253, 245, 26, 197, 54,
-  5, 246, 3, 185, 171, 234, 221, 214, 24,
-  210, 158, 183, 235, 237, 40, 90, 145,
-  154, 191, 18, 131, 121, 160, 162
+     31, 251, 188, 67, 75, 189, 76, 82,
+     241, 57, 158, 57, 172, 46, 152, 189,
+     235, 142, 207, 170, 202, 189, 10, 193,
+     222, 80, 122, 118, 162, 61, 192, 1,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-linker-hash').slice(2), 'hex')
-// )
-// 98e63aa8f93943b3bfea2ee4d0e063942415618cfc0cd51828de4de7b4698039
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-linker-hash'
+// 5bce7b2fef3886cf00bbb255290d2ca2272b3846fd82c70eed836b295ebf2629
 pub static BTC_LINKER_HASH_KEY: [u8; 32] = [
-  152, 230, 58, 168, 249, 57, 67, 179,
-  191, 234, 46, 228, 208, 224, 99, 148,
-  36, 21, 97, 140, 252, 12, 213, 24,
-  40, 222, 77, 231, 180, 105, 128, 57
+     91, 206, 123, 47, 239, 56, 134, 207,
+     0, 187, 178, 85, 41, 13, 44, 162,
+     39, 43, 56, 70, 253, 130, 199, 14,
+     237, 131, 107, 41, 94, 191, 38, 41,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-anchor-block').slice(2), 'hex')
-// )
-// bb005e5d49d23fc16c62b7971672f0f44043866cf19e4aa2d77db7f9632d0d83
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-anchor-block'
+// c39cc52cb0f285effaa18b1e5875dde3eb0c7119731b7655a56d32fc3d5c23bc
 pub static BTC_ANCHOR_BLOCK_HASH_KEY: [u8; 32] = [
-  187, 0, 94, 93, 73, 210, 63, 193,
-  108, 98, 183, 151, 22, 114, 240, 244,
-  64, 67, 134, 108, 241, 158, 74, 162,
-  215, 125, 183, 249, 99, 45, 13, 131
+     195, 156, 197, 44, 176, 242, 133, 239,
+     250, 161, 139, 30, 88, 117, 221, 227,
+     235, 12, 113, 25, 115, 27, 118, 85,
+     165, 109, 50, 252, 61, 92, 35, 188,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-private-key').slice(2), 'hex')
-// )
-// d8c4da823c79e9245163a8db18b7e9d6107f7487e624a4db9bdc3acb788902de
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-private-key'
+// 0904995590eae77780c1fd1644a2a28d58ea0baa2c4ca99a57b497b71ea78428
 pub static BTC_PRIVATE_KEY_DB_KEY: [u8; 32] = [
-  216, 196, 218, 130, 60, 121, 233, 36,
-  81, 99, 168, 219, 24, 183, 233, 214,
-  16, 127, 116, 135, 230, 36, 164, 219,
-  155, 220, 58, 203, 120, 137, 2, 222
+     9, 4, 153, 85, 144, 234, 231, 119,
+     128, 193, 253, 22, 68, 162, 162, 141,
+     88, 234, 11, 170, 44, 76, 169, 154,
+     87, 180, 151, 183, 30, 167, 132, 40,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-canon-to-tip-length').slice(2), 'hex')
-// )
-// 2d9b6327983926c2dd9636f3c8bc13b811af80858c08fe1b9d019ebdcf73049c
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-canon-to-tip-length'
+// f9ffc7ca216da72306045a02d983cfece50587c0a0fa5261725035a054e53034
 pub static BTC_CANON_TO_TIP_LENGTH_KEY: [u8; 32] = [
-  45, 155, 99, 39, 152, 57, 38, 194,
-  221, 150, 54, 243, 200, 188, 19, 184,
-  17, 175, 128, 133, 140, 8, 254, 27,
-  157, 1, 158, 189, 207, 115, 4, 156
+     249, 255, 199, 202, 33, 109, 167, 35,
+     6, 4, 90, 2, 217, 131, 207, 236,
+     229, 5, 135, 192, 160, 250, 82, 97,
+     114, 80, 53, 160, 84, 229, 48, 52,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('provable-ptoken').slice(2), 'hex')
-// )
-// 7eb2e65416dd107602495454d1ed094ae475cff2f3bfb2e2ae68a1c52bc0d66f
+// NOTE: Keccak256 hash of:: 'btc-on-eos-provable-ptoken'
+// a2addefebfcd68ac908f89facd59dbc12124bc295e0bfc76e15ff2063aa8cd89
 pub static PTOKEN_GENESIS_HASH: [u8; 32] = [
-  126, 178, 230, 84, 22, 221, 16, 118,
-  2, 73, 84, 84, 209, 237, 9, 74,
-  228, 117, 207, 242, 243, 191, 178, 226,
-  174, 104, 161, 197, 43, 192, 214, 111
+     162, 173, 222, 254, 191, 205, 104, 172,
+     144, 143, 137, 250, 205, 89, 219, 193,
+     33, 36, 188, 41, 94, 11, 252, 118,
+     225, 95, 242, 6, 58, 168, 205, 137,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-network-key').slice(2), 'hex')
-// )
-// 'f2321e29a0792487edd90debfc9a85fcb39856a5343801e794c5c915aa341ee8'
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-network-key'
+// 9645ae91e6564b91f128fd8fa100f77280bdb0d3f4b4833730ab964fdaf2c782
 pub static BTC_NETWORK_KEY: [u8; 32] = [
-  242, 50, 30, 41, 160, 121, 36, 135,
-  237, 217, 13, 235, 252, 154, 133, 252,
-  179, 152, 86, 165, 52, 56, 1, 231,
-  148, 197, 201, 21, 170, 52, 30, 232
+     150, 69, 174, 145, 230, 86, 75, 145,
+     241, 40, 253, 143, 161, 0, 247, 114,
+     128, 189, 176, 211, 244, 180, 131, 55,
+     48, 171, 150, 79, 218, 242, 199, 130,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-fee-key').slice(2), 'hex')
-// )
-// 6ded8f6cf1097edaf81e815dec1810946dd32327ecdc9de506ca7d1535c34801
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-fee-key'
+// a7f7862e056c18f0dc8851802e89b5e15735d1e0d4ba81b36ac35bc27f2d2d1a
 pub static BTC_FEE_KEY: [u8; 32] = [
-  109, 237, 143, 108, 241, 9, 126, 218,
-  248, 30, 129, 93, 236, 24, 16, 148,
-  109, 211, 35, 39, 236, 220, 157, 229,
-  6, 202, 125, 21, 53, 195, 72, 1
+     167, 247, 134, 46, 5, 108, 24, 240,
+     220, 136, 81, 128, 46, 137, 181, 225,
+     87, 53, 209, 224, 212, 186, 129, 179,
+     106, 195, 91, 194, 127, 45, 45, 26,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-account-nonce-key').slice(2), 'hex')
-// )
-// 48236d034b7d7fac3b4550bdbe5682eb012d1717bb345c39c5add04be5139880
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-account-nonce-key'
+// 192da73fcc968b8022a3bd80873b1dbace075a50ef53ddf5059e7459a459f3e1
 pub static BTC_ACCOUNT_NONCE_KEY: [u8; 32] = [
-  72, 35, 109, 3, 75, 125, 127, 172,
-  59, 69, 80, 189, 190, 86, 130, 235,
-  1, 45, 23, 23, 187, 52, 92, 57,
-  197, 173, 208, 75, 229, 19, 152, 128
+     25, 45, 167, 63, 204, 150, 139, 128,
+     34, 163, 189, 128, 135, 59, 29, 186,
+     206, 7, 90, 80, 239, 83, 221, 245,
+     5, 158, 116, 89, 164, 89, 243, 225,
 ];
-// NOTE (javascript): new Uint8Array(
-//   Buffer.from(web3.utils.keccak256('btc-tail-block-hash-key').slice(2), 'hex')
-// )
-// 0x26ab99d609131225d7ecf087632b5b6771468931273d0f6c16b09c9bbe316f71
+// NOTE: Keccak256 hash of:: 'btc-on-eos-btc-tail-block-hash-key'
+// bc6cdffc2b733d2758db92cab0b704c8d7a523bcdec8fa612d055b6edf4aacc4
 pub static BTC_TAIL_BLOCK_HASH_KEY: [u8; 32] = [
-  38, 171, 153, 214, 9, 19, 18, 37,
-  215, 236, 240, 135, 99, 43, 91, 103,
-  113, 70, 137, 49, 39, 61, 15, 108,
-  22, 176, 156, 155, 190, 49, 111, 113
+     188, 108, 223, 252, 43, 115, 61, 39,
+     88, 219, 146, 202, 176, 183, 4, 200,
+     215, 165, 35, 188, 222, 200, 250, 97,
+     45, 5, 91, 110, 223, 74, 172, 196,
 ];
