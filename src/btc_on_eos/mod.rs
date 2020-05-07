50c86209a37f9e97fb81241b59aba0fb6ca6892c
Greg Kapka <greg@oraclize.it>
2020-05-07 12:00:41 +0100

    chore(repo): adds some doc descriptions to pbtc-on-eos & main modules

--- src/btc_on_eos/mod.rs
+++ src/btc_on_eos/mod.rs
@@ -1,8 +1,24 @@
+//! # The `pBTC-on-EOS` pToken Core
+//!
+//! Here lies the functionality required for the cross-chain conversions between
+//! native bitcoins and the `pBTC` pToken on the host EOS blockchain. This core
+//! consists of two light clients that manage the state of the two chains, along
+//! with the creation and signing of transactions related to each chain.
+//!
+//! __NOTE:__ All `debug_` prefixed functions can only be used if the core is
+//! built with the `debug` feaure flag enabled in the `Cargo.toml`:
+//!
+//! ```
+//! ptokens_core = { version = "1.0.0", features = ["debug"] }
+//! ```
+
 pub use get_enclave_state::get_enclave_state;
 pub use eos::add_new_schedule::add_new_schedule_to_db;
 pub use btc::submit_btc_block::submit_btc_block_to_core;
 pub use eos::submit_eos_block::submit_eos_block_to_core;
 pub use get_latest_block_numbers::get_latest_block_numbers;
+pub use eos::enable_protocol_feature::enable_eos_protocol_feature;
+pub use eos::disable_protocol_feature::disable_eos_protocol_feature;
 pub use debug_functions::{
     debug_get_all_utxos,
     debug_clear_all_utxos,
