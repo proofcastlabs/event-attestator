9202dc4c262f2ddf02bed67a72f5cb669cd8ef2d
Greg Kapka <greg@oraclize.it>
2020-05-19 18:05:59 +0100

    feat(core): adds helper fxn to create feature-specific DB keys

--- src/chains/btc/utxo_manager/utxo_constants.rs
+++ src/chains/btc/utxo_manager/utxo_constants.rs
@@ -1,34 +1,25 @@
-use tiny_keccak::keccak256;
+use crate::utils::get_prefixed_db_key_hash;
 
-#[cfg(any(feature="pbtc-on-eth", feature="pbtc-on-eos"))]
 lazy_static! {
-    pub static ref UTXO_FIRST: [u8; 32] = keccak256(b"utxo-first");
+    pub static ref UTXO_FIRST: [u8; 32] = get_prefixed_db_key_hash(
+        "utxo-first"
+    );
 }
 
-#[cfg(any(feature="pbtc-on-eth", feature="pbtc-on-eos"))]
 lazy_static! {
-    pub static ref UTXO_LAST: [u8; 32] = keccak256(b"utxo-last");
+    pub static ref UTXO_LAST: [u8; 32] = get_prefixed_db_key_hash(
+        "utxo-last"
+    );
 }
 
-#[cfg(any(feature="pbtc-on-eth", feature="pbtc-on-eos"))]
 lazy_static! {
-    pub static ref UTXO_BALANCE: [u8; 32] = keccak256(b"utxo-balance");
+    pub static ref UTXO_BALANCE: [u8; 32] = get_prefixed_db_key_hash(
+        "utxo-balance"
+    );
 }
 
-#[cfg(any(feature="pbtc-on-eth", feature="pbtc-on-eos"))]
 lazy_static! {
-    pub static ref UTXO_NONCE: [u8; 32] = keccak256(b"utxo-nonce");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn should_get_expected_keccak_hash() {
-        let expected_result =
-            "2674b2e116a8fe42de73cd7e81f67c7e42c788c2da9711f2e5f628a001368b22";
-        let result = hex::encode(keccak256(b"utxo-first"));
-        assert_eq!(result, expected_result);
-    }
+    pub static ref UTXO_NONCE: [u8; 32] = get_prefixed_db_key_hash(
+        "utxo-nonce"
+    );
 }
