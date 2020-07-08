527410deca75665bc3135bf4cc5dd8f7d9837eea
Greg Kapka <greg@oraclize.it>
2020-07-08 13:10:13 +0100

    feat(utxo-filter): <- adds another of those based on whether utxo is already in db or not

--- src/chains/btc/filter_utxos.rs
+++ src/chains/btc/filter_utxos.rs
@@ -13,6 +13,24 @@ use crate::{
     },
 };
 
+pub fn filter_out_utxos_that_already_exist_in_db<D>(
+    db: &D,
+    utxos: &[BtcUtxoAndValue]
+) -> Result<BtcUtxosAndValues>
+    where D: DatabaseInterface
+{
+    utxos_exist_in_db(db, utxos)
+        .map(|bool_arr| {
+            utxos
+                .iter()
+                .enumerate()
+                .filter(|(i, _)| bool_arr[*i])
+                .map(|(_, utxo)| utxo)
+                .cloned()
+                .collect::<BtcUtxosAndValues>()
+        })
+}
+
 pub fn filter_out_utxos_whose_value_is_too_low(utxos: &[BtcUtxoAndValue]) -> Result<BtcUtxosAndValues> {
     Ok(
         utxos
