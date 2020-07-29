d6e0b63e02dc1bb5af6082238c966bc25d7937ed
Greg Kapka <greg@oraclize.it>
2020-07-29 15:11:23 +0100

    feat(eth-metadata): adds version byte serde & tests

--- src/chains/eth/metadata.rs
+++ src/chains/eth/metadata.rs
@@ -1,5 +1,22 @@
-use std::fmt;
-use crate::types::Byte;
+#![allow(dead_code)] // TODO rm!
+use std::{
+    fmt,
+    str::FromStr,
+};
+use crate::{
+    types::{
+        Byte,
+        Bytes,
+        Result,
+    },
+    errors::AppError,
+    utils::decode_hex_with_err_msg,
+    btc_on_eth::btc::btc_types::MintingParamStruct as BtcOnEthMintingParamStruct,
+};
+use bitcoin::{
+    hashes::sha256d,
+    util::address::Address as BtcAddress,
+};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum EthMetadataVersion {
@@ -7,10 +24,18 @@ pub enum EthMetadataVersion {
 }
 
 impl EthMetadataVersion {
-    pub fn get_version_byte(&self) -> Byte {
+    pub fn as_byte(&self) -> Byte {
         match self {
             EthMetadataVersion::V1 => 0x01,
         }
+    }
+
+    pub fn from_byte(byte: &Byte) -> Result<Self> {
+        match byte {
+            1u8 => Ok(EthMetadataVersion::V1),
+            _ => Err(AppError::Custom(format!("✘ Unrecognized version byte for `EthMetadataVersion`: {:?}", byte)))
+        }
+
     }
 }
 
@@ -30,7 +55,18 @@ mod tests {
     fn should_get_metadata_v1_byte() {
         let expected_result = 0x01;
         let metadata_version = EthMetadataVersion::V1;
-        let result = metadata_version.get_version_byte();
+        let result = metadata_version.as_byte();
         assert_eq!(result ,expected_result);
+    }
+
+    #[test]
+    fn should_fail_to_get_metadata_version_correctly() {
+        let byte = 255u8;
+        let expected_error = format!("✘ Unrecognized version byte for `EthMetadataVersion`: {:?}", byte);
+        match EthMetadataVersion::from_byte(&byte) {
+            Err(AppError::Custom(err)) => assert_eq!(err, expected_error),
+            Err(err) => panic!("Wrong error received: {}", err),
+            Ok(_) => panic!("Should not have succeeded!"),
+        }
     }
 }
