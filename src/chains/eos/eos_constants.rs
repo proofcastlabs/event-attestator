29134ad4586c9c2421f3d4f5739f7ad6a3b55a67
Greg Kapka <greg@oraclize.it>
2020-08-03 12:45:42 +0100

    fix(eos-expiration): <- drop that to 59 minutes. Closes #115.

--- src/chains/eos/eos_constants.rs
+++ src/chains/eos/eos_constants.rs
@@ -8,12 +8,13 @@ pub const PRODUCER_REPS: u64 = 12;
 pub const PUBLIC_KEY_SIZE: usize = 33;
 pub const PBTC_MINT_FXN_NAME: &str = "issue";
 pub const REDEEM_ACTION_NAME: &str = "redeem";
-pub const EOS_MAX_EXPIRATION_SECS: u32 = 3600;
 pub const PUBLIC_KEY_CHECKSUM_SIZE: usize = 4;
 pub const MEMO: &str = "BTC -> pBTC complete!";
 pub const EOS_SCHEDULE_DB_PREFIX: &str = "EOS_SCHEDULE_";
 pub const PEOS_ACCOUNT_PERMISSION_LEVEL: &str = "active";
 pub const PUBLIC_KEY_WITH_CHECKSUM_SIZE: usize = PUBLIC_KEY_SIZE + PUBLIC_KEY_CHECKSUM_SIZE;
+// NOTE: We use 59 minutes rather than 60 to give a little wiggle room for the clocks on the TEE devices.
+pub const EOS_MAX_EXPIRATION_SECS: u32 = 3540;
 
 pub fn get_eos_constants_db_keys() -> JsonValue {
     json!({
