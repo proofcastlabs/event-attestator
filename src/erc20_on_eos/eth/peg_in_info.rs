357cb9c9fb53436c1e9bf3a68992b879fcc493b3
Greg Kapka <greg@oraclize.it>
2020-09-23 18:26:38 +0100

    feat(erc20-peg-in): <- adds mod for types to hold params required for those

---
+++ src/erc20_on_eos/eth/peg_in_info.rs
@@ -0,0 +1,46 @@
+use ethereum_types::{
+    U256,
+    H256 as EthHash,
+    Address as EthAddress,
+};
+use crate::{
+    types::Result,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Erc20OnEosPegInInfo {
+    pub token_amount: U256,
+    pub eos_address: String,
+    pub token_sender: EthAddress,
+    pub token_contract: EthAddress,
+    pub originating_tx_hash: EthHash,
+}
+
+impl Erc20OnEosPegInInfo {
+    pub fn new(
+        token_amount: U256,
+        token_sender: EthAddress,
+        token_contract: EthAddress,
+        eos_address: String,
+        originating_tx_hash: EthHash
+    ) -> Erc20OnEosPegInInfo {
+        Erc20OnEosPegInInfo { token_amount, token_contract, eos_address, originating_tx_hash, token_sender }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Erc20OnEosPegInInfos(pub Vec<Erc20OnEosPegInInfo>);
+
+impl Erc20OnEosPegInInfos {
+    pub fn new(redeem_infos: &[Erc20OnEosPegInInfo]) -> Self {
+        Self(redeem_infos.to_vec())
+    }
+
+    pub fn sum(&self) -> U256 {
+        self.0.iter().fold(U256::zero(), |acc, params| acc + params.token_amount)
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
