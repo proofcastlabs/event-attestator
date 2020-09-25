f02bfd385590a2c514cb107dda151e6c50731d6c
Greg Kapka <greg@oraclize.it>
2020-09-25 14:04:59 +0100

    feat(eos-constants): <- adds new db key to that for `EosErc20AccountNames`

--- src/chains/eos/eos_constants.rs
+++ src/chains/eos/eos_constants.rs
@@ -29,6 +29,7 @@ pub fn get_eos_constants_db_keys() -> JsonValue {
         "EOS_LAST_SEEN_BLOCK_ID": hex::encode(EOS_LAST_SEEN_BLOCK_ID.to_vec()),
         "EOS_PRIVATE_KEY_DB_KEY": hex::encode(EOS_PRIVATE_KEY_DB_KEY.to_vec()),
         "EOS_LAST_SEEN_BLOCK_NUM": hex::encode(EOS_LAST_SEEN_BLOCK_NUM.to_vec()),
+        "EOS_ERC20_ACCOUNT_NAMES": hex::encode(EOS_ERC20_ACCOUNT_NAMES.to_vec()),
     })
 }
 
@@ -43,3 +44,4 @@ lazy_static! { pub static ref EOS_PROTOCOL_FEATURES: [u8; 32] = get_prefixed_db_
 lazy_static! { pub static ref EOS_PRIVATE_KEY_DB_KEY: [u8; 32] = get_prefixed_db_key("eos-private-key-db-key"); }
 lazy_static! { pub static ref EOS_LAST_SEEN_BLOCK_ID: [u8; 32] = get_prefixed_db_key("eos-last-seen-block-id"); }
 lazy_static! { pub static ref EOS_LAST_SEEN_BLOCK_NUM: [u8; 32] = get_prefixed_db_key("eos-last-seen-block-num"); }
+lazy_static! { pub static ref EOS_ERC20_ACCOUNT_NAMES: [u8; 32] = get_prefixed_db_key("eos-erc20-account-names"); }
