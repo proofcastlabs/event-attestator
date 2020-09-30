121408a16b612eed153a137a7d13638b91dee4e7
Greg Kapka <greg@oraclize.it>
2020-09-30 10:28:22 +0100

    feat(erc20-on-eos): <- adds eos initialization to that

--- src/erc20_on_eos/eos/mod.rs
+++ src/erc20_on_eos/eos/mod.rs
@@ -2,5 +2,6 @@ pub mod submit_eos_block;
 
 pub(crate) mod redeem_info;
 pub(crate) mod get_eos_output;
+pub(crate) mod initialize_eos_core;
 pub(crate) mod sign_normal_eth_txs;
 pub(crate) mod increment_eth_nonce;
