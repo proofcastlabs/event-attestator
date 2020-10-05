28a900cd20ee3d4134ab2714e2b1f316603b4789
Greg Kapka <greg@oraclize.it>
2020-10-05 19:12:48 +0100

    feat(erc20-on-eos): <- adds zero asset value filterer method to peg-in info

--- src/erc20_on_eos/eth/peg_in_info.rs
+++ src/erc20_on_eos/eth/peg_in_info.rs
@@ -11,7 +11,10 @@ use crate::{
     types::Result,
     traits::DatabaseInterface,
     chains::{
-        eos::eos_erc20_dictionary::EosErc20Dictionary,
+        eos::{
+            eos_utils::remove_symbol_from_eos_asset,
+            eos_erc20_dictionary::EosErc20Dictionary,
+        },
         eth::{
             eth_state::EthState,
             eth_database_utils::get_eth_canon_block_from_db,
@@ -60,8 +63,22 @@ impl Erc20OnEosPegInInfos {
         self.0.iter().fold(U256::zero(), |acc, params| acc + params.token_amount)
     }
 
-    pub fn len(&self) -> usize {
-        self.0.len()
+    pub fn filter_out_zero_eos_values(&self) -> Self {
+        Self::new(
+            self
+                .iter()
+                .filter(|peg_in_info|
+                    match remove_symbol_from_eos_asset(&peg_in_info.eos_asset_amount).parse::<f64>() != Ok(0.0) {
+                        true => true,
+                        false => {
+                            info!("✘ Filtering out peg in info due to zero EOS asset amount: {:?}", peg_in_info);
+                            false
+                        }
+                    }
+                )
+                .cloned()
+                .collect::<Vec<Erc20OnEosPegInInfo>>()
+        )
     }
 }
 
@@ -86,4 +103,31 @@ pub fn maybe_parse_peg_in_info_and_add_to_state<D>(
                 }
             }
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_sample_zero_eos_asset_peg_in_info() -> Erc20OnEosPegInInfo {
+        Erc20OnEosPegInInfo::new(
+            U256::from_dec_str("1337").unwrap(),
+            EthAddress::from_slice(&hex::decode("edB86cd455ef3ca43f0e227e00469C3bDFA40628").unwrap()),
+            EthAddress::from_slice(&hex::decode("d879D3C8782aB95a43C69Fa73d8DCC50C8815d5e").unwrap()),
+            "aneosaddress".to_string(),
+            EthHash::from_slice(&hex::decode("7b7f73183fe4d1d6e23c494ba0b579718c7dd6e1c62426fd5411a6a21b3203aa").unwrap()),
+            "aneosaccount".to_string(),
+            "0.000000000 SAM".to_string(),
+        )
+    }
+
+    #[test]
+    fn should_filter_out_zero_eos_asset_peg_ins() {
+        let expected_num_peg_ins_before = 1;
+        let expected_num_peg_ins_after = 0;
+        let peg_ins = Erc20OnEosPegInInfos::new(vec![get_sample_zero_eos_asset_peg_in_info()]);
+        assert_eq!(peg_ins.len(), expected_num_peg_ins_before);
+        let result = peg_ins.filter_out_zero_eos_values();
+        assert_eq!(result.len(), expected_num_peg_ins_after);
+    }
 }
