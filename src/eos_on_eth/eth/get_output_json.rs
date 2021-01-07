b2003cac9621298fbae5ecfc5fe88505ec7c247b
Greg Kapka <greg@oraclize.it>
2021-01-07 12:35:30 +0000

    feat(eos-on-eth): <- adds mod to that to get output for ETH block submission

---
+++ src/eos_on_eth/eth/get_output_json.rs
@@ -0,0 +1,92 @@
+use crate::{
+    chains::{
+        eos::{
+            eos_database_utils::{get_eos_account_nonce_from_db, get_latest_eos_block_number},
+            eos_types::EosSignedTransaction,
+        },
+        eth::{eth_database_utils::get_eth_latest_block_from_db, eth_state::EthState},
+    },
+    eos_on_eth::eth::eth_tx_info::EosOnEthEthTxInfo,
+    traits::DatabaseInterface,
+    types::Result,
+};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EosOnEthEthOutputDetails {
+    pub _id: String,
+    pub broadcast: bool,
+    pub eos_tx_amount: String,
+    pub eth_tx_amount: String,
+    pub eos_account_nonce: u64,
+    pub eos_tx_recipient: String,
+    pub eos_tx_signature: String,
+    pub witnessed_timestamp: u64,
+    pub eos_serialized_tx: String,
+    pub host_token_address: String,
+    pub originating_tx_hash: String,
+    pub originating_address: String,
+    pub eos_latest_block_number: u64,
+    pub native_token_address: String,
+    pub broadcast_tx_hash: Option<String>,
+    pub broadcast_timestamp: Option<String>,
+}
+
+impl EosOnEthEthOutputDetails {
+    pub fn new(
+        eos_tx: &EosSignedTransaction,
+        tx_info: &EosOnEthEthTxInfo,
+        eos_account_nonce: u64,
+        eos_latest_block_number: u64,
+    ) -> Result<EosOnEthEthOutputDetails> {
+        Ok(EosOnEthEthOutputDetails {
+            broadcast: false,
+            eos_account_nonce,
+            eos_latest_block_number,
+            broadcast_tx_hash: None,
+            broadcast_timestamp: None,
+            eos_tx_amount: eos_tx.amount.to_string(),
+            eos_tx_signature: eos_tx.signature.clone(),
+            eos_tx_recipient: eos_tx.recipient.clone(),
+            eos_serialized_tx: eos_tx.transaction.clone(),
+            eth_tx_amount: tx_info.token_amount.to_string(),
+            _id: format!("peos-on-eth-{}", eos_account_nonce),
+            host_token_address: tx_info.eos_token_address.to_string(),
+            originating_address: format!("0x{}", hex::encode(tx_info.token_sender)),
+            witnessed_timestamp: SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs(),
+            originating_tx_hash: format!("0x{}", hex::encode(&tx_info.originating_tx_hash)),
+            native_token_address: format!("0x{}", hex::encode(&tx_info.eth_token_address)),
+        })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EosOnEthEthOutput {
+    pub eth_latest_block_number: u64,
+    pub eos_signed_transactions: Vec<EosOnEthEthOutputDetails>,
+}
+
+pub fn get_output_json<D: DatabaseInterface>(state: EthState<D>) -> Result<String> {
+    info!("✔ Getting `eos-on-eth` ETH submission output json...");
+    Ok(serde_json::to_string(&EosOnEthEthOutput {
+        eth_latest_block_number: get_eth_latest_block_from_db(&state.db)?.get_block_number()?.as_u64(),
+        eos_signed_transactions: match state.eos_transactions {
+            None => vec![],
+            Some(ref eos_txs) => {
+                let start_nonce = get_eos_account_nonce_from_db(&state.db)? - eos_txs.len() as u64;
+                eos_txs
+                    .iter()
+                    .enumerate()
+                    .map(|(i, eos_tx)| {
+                        EosOnEthEthOutputDetails::new(
+                            &eos_tx,
+                            &state.eos_on_eth_eth_tx_infos[i],
+                            start_nonce + i as u64,
+                            get_latest_eos_block_number(&state.db)?,
+                        )
+                    })
+                    .collect::<Result<Vec<EosOnEthEthOutputDetails>>>()?
+            },
+        },
+    })?)
+}
