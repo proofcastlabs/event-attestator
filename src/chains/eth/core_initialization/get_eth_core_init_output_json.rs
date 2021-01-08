018723a4a4448083f24cb80181de81cb7cfab764
Greg Kapka <greg@oraclize.it>
2021-01-08 10:49:16 +0000

    feat(eth-init-output): <- update that to work with inits that don't result in contract txs

--- src/chains/eth/core_initialization/get_eth_core_init_output_json.rs
+++ src/chains/eth/core_initialization/get_eth_core_init_output_json.rs
@@ -19,41 +19,49 @@ use serde_json::to_string;
 pub struct EthInitializationOutput {
     pub eth_address: String,
     pub eth_latest_block_num: usize,
-    pub eth_ptoken_contract_tx: String,
-    pub smart_contract_address: String,
+    pub eth_ptoken_contract_tx: Option<String>,
+    pub smart_contract_address: Option<String>,
 }
 
 impl EthInitializationOutput {
-    fn init<D: DatabaseInterface>(db: &D, contract_address: &EthAddress, contract_tx: &str) -> Result<Self> {
+    fn init<D: DatabaseInterface>(
+        db: &D,
+        contract_address: Option<&EthAddress>,
+        contract_tx: Option<&str>,
+    ) -> Result<Self> {
         Ok(Self {
             eth_address: format!("0x{}", hex::encode(get_public_eth_address_from_db(db)?.as_bytes())),
             eth_latest_block_num: get_latest_eth_block_number(db)?,
-            eth_ptoken_contract_tx: contract_tx.to_string(),
-            smart_contract_address: format!("0x{}", hex::encode(contract_address)),
+            eth_ptoken_contract_tx: match contract_tx {
+                Some(tx) => Some(tx.to_string()),
+                None => None,
+            },
+            smart_contract_address: match contract_address {
+                Some(address) => Some(format!("0x{}", hex::encode(address))),
+                None => None,
+            },
         })
     }
 
     pub fn new_for_eos_on_eth<D: DatabaseInterface>(state: EthState<D>) -> Result<String> {
-        Ok(to_string(&Self::init(
-            &state.db,
-            &get_eos_on_eth_smart_contract_address_from_db(&state.db)?,
-            &state.get_misc_string()?,
-        )?)?)
+        let contract_tx = None;
+        let contract_address = None;
+        Ok(to_string(&Self::init(&state.db, contract_address, contract_tx)?)?)
     }
 
     pub fn new_for_btc_on_eth<D: DatabaseInterface>(state: EthState<D>) -> Result<String> {
         Ok(to_string(&Self::init(
             &state.db,
-            &get_erc777_contract_address_from_db(&state.db)?,
-            &state.get_misc_string()?,
+            Some(&get_erc777_contract_address_from_db(&state.db)?),
+            Some(&state.get_misc_string()?),
         )?)?)
     }
 
     pub fn new_for_erc20_on_eth<D: DatabaseInterface>(state: EthState<D>) -> Result<String> {
         Ok(to_string(&Self::init(
             &state.db,
-            &get_erc20_on_eos_smart_contract_address_from_db(&state.db)?,
-            &state.get_misc_string()?,
+            Some(&get_erc20_on_eos_smart_contract_address_from_db(&state.db)?),
+            Some(&state.get_misc_string()?),
         )?)?)
     }
 }
