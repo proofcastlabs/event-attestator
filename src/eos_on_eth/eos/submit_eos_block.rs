cd6186c9250c8004905f07f8e94f24f2bde69eb7
Greg Kapka <greg@oraclize.it>
2021-01-11 15:16:19 +0000

    fix(eos-on-eth): <- rm unneeded step where proofs are filtered for account names

--- src/eos_on_eth/eos/submit_eos_block.rs
+++ src/eos_on_eth/eos/submit_eos_block.rs
@@ -13,7 +13,6 @@ use crate::{
             maybe_filter_duplicate_proofs_from_state,
             maybe_filter_out_action_proof_receipt_mismatches_and_return_state,
             maybe_filter_out_invalid_action_receipt_digests,
-            maybe_filter_out_proofs_for_wrong_eos_account_name,
             maybe_filter_out_proofs_with_invalid_merkle_proofs,
             maybe_filter_out_proofs_with_wrong_action_mroot,
         },
@@ -71,7 +70,6 @@ pub fn submit_eos_block_to_core<D: DatabaseInterface>(db: D, block_json: &str) -
         .and_then(maybe_filter_out_invalid_action_receipt_digests)
         .and_then(maybe_filter_out_proofs_with_invalid_merkle_proofs)
         .and_then(maybe_filter_out_proofs_with_wrong_action_mroot)
-        .and_then(maybe_filter_out_proofs_for_wrong_eos_account_name)
         .and_then(maybe_parse_eos_on_eth_eos_tx_infos_and_put_in_state)
         .and_then(maybe_filter_out_already_processed_tx_ids_from_state)
         .and_then(maybe_filter_out_value_too_low_txs_from_state)
