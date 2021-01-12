8bb4448b3d719fbe1b07f8dacab02baf48516c54
Greg Kapka <greg@oraclize.it>
2021-01-12 11:38:55 +0000

    fix(eth-init): <- initialize state in instances when initializing ETH cores

--- src/btc_on_eth/eth/initialize_eth_core.rs
+++ src/btc_on_eth/eth/initialize_eth_core.rs
@@ -21,19 +21,17 @@ pub fn maybe_initialize_eth_enclave<D: DatabaseInterface>(
     canon_to_tip_length: u64,
     bytecode_path: &str,
 ) -> Result<String> {
-    check_for_existence_of_eth_contract_byte_code(bytecode_path)
-        .map(|_| EthState::init(db))
-        .and_then(|state| match is_eth_core_initialized(&state.db) {
-            true => Ok("{eth_core_initialized:true}".to_string()),
-            false => initialize_eth_core(
-                block_json,
-                chain_id,
-                gas_price,
-                canon_to_tip_length,
-                bytecode_path,
-                state,
-            )
-            .and_then(generate_and_store_btc_on_eth_contract_address)
-            .and_then(EthInitializationOutput::new_for_btc_on_eth),
-        })
+    check_for_existence_of_eth_contract_byte_code(bytecode_path).and_then(|_| match is_eth_core_initialized(&db) {
+        true => Ok("{eth_core_initialized:true}".to_string()),
+        false => initialize_eth_core(
+            block_json,
+            chain_id,
+            gas_price,
+            canon_to_tip_length,
+            bytecode_path,
+            EthState::init(db),
+        )
+        .and_then(generate_and_store_btc_on_eth_contract_address)
+        .and_then(EthInitializationOutput::new_for_btc_on_eth),
+    })
 }
--- src/eos_on_eth/eth/initialize_eth_core.rs
+++ src/eos_on_eth/eth/initialize_eth_core.rs
@@ -2,6 +2,7 @@ use crate::{
     chains::eth::{
         core_initialization::{
             check_eth_core_is_initialized::is_eth_core_initialized,
+            eth_core_init_utils::check_for_existence_of_eth_contract_byte_code,
             generate_eth_contract_address::generate_and_store_eos_on_eth_contract_address,
             get_eth_core_init_output_json::EthInitializationOutput,
             initialize_eth_core::initialize_eth_core_with_no_contract_tx,
@@ -47,12 +48,18 @@ pub fn maybe_initialize_eth_core<D: DatabaseInterface>(
     chain_id: u8,
     gas_price: u64,
     canon_to_tip_length: u64,
+    bytecode_path: &str,
 ) -> Result<String> {
-    let state = EthState::init(db);
-    match is_eth_core_initialized(&state.db) {
+    check_for_existence_of_eth_contract_byte_code(bytecode_path).and_then(|_| match is_eth_core_initialized(&db) {
         true => Ok("{eth_core_initialized:true}".to_string()),
-        false => initialize_eth_core_with_no_contract_tx(block_json, chain_id, gas_price, canon_to_tip_length, state)
-            .and_then(generate_and_store_eos_on_eth_contract_address)
-            .and_then(EthInitializationOutput::new_for_eos_on_eth),
-    }
+        false => initialize_eth_core_with_no_contract_tx(
+            block_json,
+            chain_id,
+            gas_price,
+            canon_to_tip_length,
+            EthState::init(db),
+        )
+        .and_then(generate_and_store_eos_on_eth_contract_address)
+        .and_then(EthInitializationOutput::new_for_eos_on_eth),
+    })
 }
