004c3c363e216c7dd7d6c2f6efc9d7cf94c5e088
Greg Kapka <greg@oraclize.it>
2021-02-03 13:29:54 +0000

    fix(eos-init): <- move DB tx ops to instance initters to ensure smart-contract address is saved

--- src/btc_on_eth/eth/initialize_eth_core.rs
+++ src/btc_on_eth/eth/initialize_eth_core.rs
@@ -8,6 +8,10 @@ use crate::{
             initialize_eth_core::initialize_eth_core,
         },
         eth_constants::ETH_CORE_IS_INITIALIZED_JSON,
+        eth_database_transactions::{
+            end_eth_db_transaction_and_return_state,
+            start_eth_db_transaction_and_return_state,
+        },
         eth_state::EthState,
     },
     traits::DatabaseInterface,
@@ -19,20 +23,15 @@ pub fn maybe_initialize_eth_enclave<D: DatabaseInterface>(
     block_json: &str,
     chain_id: u8,
     gas_price: u64,
-    canon_to_tip_length: u64,
+    confs: u64,
     bytecode_path: &str,
 ) -> Result<String> {
     check_for_existence_of_eth_contract_byte_code(bytecode_path).and_then(|_| match is_eth_core_initialized(&db) {
         true => Ok(ETH_CORE_IS_INITIALIZED_JSON.to_string()),
-        false => initialize_eth_core(
-            block_json,
-            chain_id,
-            gas_price,
-            canon_to_tip_length,
-            bytecode_path,
-            EthState::init(db),
-        )
-        .and_then(generate_and_store_btc_on_eth_contract_address)
-        .and_then(EthInitializationOutput::new_for_btc_on_eth),
+        false => start_eth_db_transaction_and_return_state(EthState::init(db))
+            .and_then(|state| initialize_eth_core(block_json, chain_id, gas_price, confs, bytecode_path, state))
+            .and_then(generate_and_store_btc_on_eth_contract_address)
+            .and_then(end_eth_db_transaction_and_return_state)
+            .and_then(EthInitializationOutput::new_for_btc_on_eth),
     })
 }
