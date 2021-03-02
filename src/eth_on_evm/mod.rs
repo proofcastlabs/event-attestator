134bc1fdaae756c9209abb0a31d6dd0d6ea825db
Greg Kapka <greg@oraclize.it>
2021-03-02 10:32:40 +0000

    feat(eth-on-evm): <- re-exports the submission pipeline fxns for that

--- src/eth_on_evm/mod.rs
+++ src/eth_on_evm/mod.rs
@@ -19,35 +19,8 @@ pub mod get_enclave_state;
 pub mod get_latest_block_numbers;
 
 pub(crate) mod check_core_is_initialized;
-// pub(crate) mod constants;
-// pub(crate) mod test_utils;
-//
-pub use crate::eth_on_evm::eth::{
-    initialize_eth_core::maybe_initialize_eth_core,
-    submit_eth_block::submit_eth_block_to_core,
+
+pub use crate::eth_on_evm::{
+    eth::{initialize_eth_core::maybe_initialize_eth_core, submit_eth_block::submit_eth_block_to_core},
+    evm::{initialize_evm_core::maybe_initialize_evm_core, submit_evm_block::submit_evm_block_to_core},
 };
-// chains::{
-// eos::{
-// core_initialization::initialize_eos_core::maybe_initialize_eos_core_with_eos_account_without_symbol as
-// maybe_initialize_eos_core, disable_protocol_feature::{disable_eos_protocol_feature, disable_protocol_feature},
-// enable_protocol_feature::enable_eos_protocol_feature,
-// },
-// eth::eth_message_signer::{sign_ascii_msg_with_eth_key_with_no_prefix, sign_hex_msg_with_eth_key_with_prefix},
-// },
-// eos_on_eth::{
-// debug_functions::{
-// debug_add_eos_eth_token_dictionary_entry,
-// debug_add_new_eos_schedule,
-// debug_get_all_db_keys,
-// debug_get_key_from_db,
-// debug_remove_eos_eth_token_dictionary_entry,
-// debug_reprocess_eos_block,
-// debug_reprocess_eth_block,
-// debug_set_key_in_db_to_value,
-// debug_update_incremerkle,
-// },
-// eos::submit_eos_block::submit_eos_block_to_core,
-// get_enclave_state::get_enclave_state,
-// get_latest_block_numbers::get_latest_block_numbers,
-// },
-// };
