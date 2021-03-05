47c6c3b503ba69c8c4ae94b74a0243f53625a186
Greg Kapka <greg@oraclize.it>
2021-03-05 18:02:58 +0000

    feat(eth-constants): <- use correct topic for `ETH-on-EVM` vault contract

--- src/chains/eth/eth_constants.rs
+++ src/chains/eth/eth_constants.rs
@@ -26,7 +26,7 @@ pub const EMPTY_NIBBLES: Nibbles = Nibbles {
 };
 pub const ERC20_PEG_IN_EVENT_TOPIC_HEX: &str = "42877668473c4cba073df41397388516dc85c3bbae14b33603513924cec55e36";
 pub const ETH_ON_EVM_PEG_IN_EVENT_TOPIC_HEX: &str = "f7279bc9441f2a98e9a2e0f58cf873aee8f8300a76f99114d073c852ae1b234f";
-pub const ETH_ON_EVM_REDEEM_EVENT_TOPIC_HEX: &str = BTC_ON_ETH_REDEEM_EVENT_TOPIC_HEX;
+pub const ETH_ON_EVM_REDEEM_EVENT_TOPIC_HEX: &str = "a3aff243d9d0c944de98e4e2a6a4dd983fd817a1b8835e0e00f103a6d27e4513";
 pub const BTC_ON_ETH_REDEEM_EVENT_TOPIC_HEX: &str = "78e6c3f67f57c26578f2487b930b70d844bcc8dd8f4d629fb4af81252ab5aa65";
 pub const EOS_ON_ETH_ETH_TX_INFO_EVENT_TOPIC_HEX: &str = BTC_ON_ETH_REDEEM_EVENT_TOPIC_HEX;
 pub const ETH_MESSAGE_PREFIX: &[u8; 26] = b"\x19Ethereum Signed Message:\n";
@@ -46,9 +46,8 @@ lazy_static! {
 lazy_static! {
     pub static ref ETH_ON_EVM_REDEEM_EVENT_TOPIC: [EthHash; 1] = {
         [EthHash::from_slice(
-            // NOTE: Same topic as `pBTC_ON_ETH`! // FIXME Not any more! Change it when we have a
-            // peg out example!
-            &hex::decode(BTC_ON_ETH_REDEEM_EVENT_TOPIC_HEX).expect("✘ Invalid hex in `BTC_ON_ETH_REDEEM_EVENT_TOPIC_HEX`!"),
+            &hex::decode(ETH_ON_EVM_REDEEM_EVENT_TOPIC_HEX)
+                .expect("✘ Invalid hex in `ETH_ON_EVM_REDEEM_EVENT_TOPIC_HEX`!"),
         )]
     };
 }
