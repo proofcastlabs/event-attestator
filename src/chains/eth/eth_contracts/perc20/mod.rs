c868af6dfc99c99ddbb93927e7af45618fc172f3
Greg Kapka <greg@oraclize.it>
2021-03-13 12:44:30 +0000

    ref(eth): <- rm unused fxns from perc20 contract mod

--- src/chains/eth/eth_contracts/perc20/mod.rs
+++ src/chains/eth/eth_contracts/perc20/mod.rs
@@ -1,11 +1,9 @@
-#![allow(dead_code)] // FIXME: rm!
-
 use derive_more::Constructor;
-use ethabi::{decode as eth_abi_decode, ParamType as EthAbiParamType, Token as EthAbiToken};
+use ethabi::Token as EthAbiToken;
 use ethereum_types::{Address as EthAddress, U256};
 
 use crate::{
-    chains::eth::{eth_contracts::encode_fxn_call, eth_log::EthLog},
+    chains::eth::eth_contracts::encode_fxn_call,
     types::{Bytes, Result},
 };
 
@@ -52,89 +50,10 @@ pub struct Perc20PegInEventParams {
     pub destination_address: String,
 }
 
-impl Perc20PegInEventParams {
-    fn get_err_msg(field: &str) -> String {
-        format!("Error getting `{}` from `Perc20PegInEvent`!", field)
-    }
-
-    fn from_log_with_user_data(log: &EthLog) -> Result<Self> {
-        let tokens = eth_abi_decode(
-            &vec![
-                EthAbiParamType::Address,
-                EthAbiParamType::Address,
-                EthAbiParamType::Uint(256),
-                EthAbiParamType::String,
-                EthAbiParamType::Bytes,
-            ],
-            &log.data,
-        )?;
-        Ok(Self {
-            token_address: match tokens[0] {
-                EthAbiToken::Address(value) => Ok(value),
-                _ => Err(Self::get_err_msg("token_address").to_string()),
-            }?,
-            token_sender: match tokens[1] {
-                EthAbiToken::Address(value) => Ok(value),
-                _ => Err(Self::get_err_msg("token_sender").to_string()),
-            }?,
-            token_amount: match tokens[2] {
-                EthAbiToken::Uint(value) => Ok(value),
-                _ => Err(Self::get_err_msg("token_amount").to_string()),
-            }?,
-            destination_address: match tokens[3] {
-                EthAbiToken::String(ref value) => Ok(value.to_string()),
-                _ => Err(Self::get_err_msg("destination_address").to_string()),
-            }?,
-            user_data: match tokens[4] {
-                EthAbiToken::Bytes(ref value) => Ok(value.clone()),
-                _ => Err(Self::get_err_msg("user_data").to_string()),
-            }?,
-        })
-    }
-
-    fn from_log_without_user_data(log: &EthLog) -> Result<Self> {
-        let tokens = eth_abi_decode(
-            &vec![
-                EthAbiParamType::Address,
-                EthAbiParamType::Address,
-                EthAbiParamType::Uint(256),
-                EthAbiParamType::String,
-            ],
-            &log.data,
-        )?;
-        Ok(Self {
-            user_data: vec![],
-            token_address: match tokens[0] {
-                EthAbiToken::Address(value) => Ok(value),
-                _ => Err(Self::get_err_msg("token_address").to_string()),
-            }?,
-            token_sender: match tokens[1] {
-                EthAbiToken::Address(value) => Ok(value),
-                _ => Err(Self::get_err_msg("token_sender").to_string()),
-            }?,
-            token_amount: match tokens[2] {
-                EthAbiToken::Uint(value) => Ok(value),
-                _ => Err(Self::get_err_msg("token_amount").to_string()),
-            }?,
-            destination_address: match tokens[3] {
-                EthAbiToken::String(ref value) => Ok(value.to_string()),
-                _ => Err(Self::get_err_msg("destination_address").to_string()),
-            }?,
-        })
-    }
-
-    pub fn from_log(log: &EthLog) -> Result<Self> {
-        match Self::from_log_with_user_data(log) {
-            Ok(res) => Ok(res),
-            Err(_) => Self::from_log_without_user_data(log),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::chains::eth::eth_test_utils::{get_sample_eth_address, get_sample_log_with_erc20_peg_in_event_2};
+    use crate::chains::eth::eth_test_utils::get_sample_eth_address;
 
     #[test]
     fn should_encode_peg_out_fxn_data() {
@@ -169,19 +88,5 @@ mod tests {
         let address = get_sample_eth_address();
         let result = encode_perc20_remove_supported_token_fx_data(address).unwrap();
         assert_eq!(hex::encode(&result), expected_result);
-    }
-
-    #[test]
-    fn should_get_per20_peg_in_event_params_from_log_without_user_data() {
-        let log = get_sample_log_with_erc20_peg_in_event_2().unwrap();
-        let result = Perc20PegInEventParams::from_log(&log).unwrap();
-        let expected_result = Perc20PegInEventParams {
-            user_data: vec![],
-            token_amount: U256::from_dec_str("50000000000000").unwrap(),
-            token_sender: EthAddress::from_slice(&hex::decode("7344d31d7025f72bd1d3c08645fa6b12d406fc05").unwrap()),
-            token_address: EthAddress::from_slice(&hex::decode("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2").unwrap()),
-            destination_address: "whateverxxxx".to_string(),
-        };
-        assert_eq!(result, expected_result);
     }
 }
