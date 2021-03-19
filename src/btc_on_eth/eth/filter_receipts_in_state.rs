9363c6cb5b6dff778c884e8bde08076ee71de993
Greg Kapka <greg@oraclize.it>
2021-03-19 20:45:40 +0000

    fix(btc-on-eth): <- filter for both old and new event topics in that

--- src/btc_on_eth/eth/filter_receipts_in_state.rs
+++ src/btc_on_eth/eth/filter_receipts_in_state.rs
@@ -1,6 +1,6 @@
 use crate::{
     chains::eth::{
-        eth_constants::BTC_ON_ETH_REDEEM_EVENT_TOPIC,
+        eth_contracts::erc777::{ERC_777_REDEEM_EVENT_TOPIC_WITHOUT_USER_DATA,ERC_777_REDEEM_EVENT_TOPIC_WITH_USER_DATA},
         eth_database_utils::get_erc777_contract_address_from_db,
         eth_state::EthState,
     },
@@ -16,7 +16,7 @@ pub fn filter_receipts_for_btc_on_eth_redeem_events_in_state<D: DatabaseInterfac
         .get_eth_submission_material()?
         .get_receipts_containing_log_from_address_and_with_topics(
             &get_erc777_contract_address_from_db(&state.db)?,
-            &BTC_ON_ETH_REDEEM_EVENT_TOPIC.to_vec(),
+            &[*ERC_777_REDEEM_EVENT_TOPIC_WITHOUT_USER_DATA, *ERC_777_REDEEM_EVENT_TOPIC_WITH_USER_DATA],
         )
         .and_then(|filtered_block_and_receipts| state.update_eth_submission_material(filtered_block_and_receipts))
 }
