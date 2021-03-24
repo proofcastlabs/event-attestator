ce3af33a3e9f257bea362ff674440d879f50957a
Greg Kapka <greg@oraclize.it>
2021-03-24 12:17:36 +0000

    feat(metadata): <- adds mod to get metadata version

--- src/metadata/metadata_version.rs
+++ src/metadata/metadata_version.rs
@@ -8,15 +8,21 @@ pub enum MetadataVersion {
 impl MetadataVersion {
     pub fn as_byte(&self) -> Byte {
         match self {
-            MetadataVersion::V1 => 0x01,
+            Self::V1 => 0x01,
         }
     }
 
     pub fn from_byte(byte: &Byte) -> Result<Self> {
         match byte {
-            1u8 => Ok(MetadataVersion::V1),
+            1u8 => Ok(Self::V1),
             _ => Err(format!("✘ Unrecognized version byte for `MetadataVersion`: {:?}", byte).into()),
         }
+    }
+
+    #[cfg(test)]
+    fn get_all() -> Vec<Self> {
+        // TODO How to ensure this list is complete?
+        vec![Self::V1]
     }
 }
 
@@ -26,9 +32,10 @@ mod tests {
 
     #[test]
     fn should_make_metadata_version_bytes_roundtrip() {
-        let metadata_version = MetadataVersion::V1;
-        let byte = metadata_version.as_byte();
-        let result = MetadataVersion::from_byte(&byte).unwrap();
-        assert_eq!(result, metadata_version);
+        MetadataVersion::get_all().iter().for_each(|id| {
+            let byte = id.as_byte();
+            let result = MetadataVersion::from_byte(&byte).unwrap();
+            assert_eq!(&result, id);
+        });
     }
 }
