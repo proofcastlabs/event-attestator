4a5ff2e444fa0039d0939689150f40d44d2f1dfd
Greg Kapka <greg@oraclize.it>
2021-03-24 12:16:36 +0000

    feat(metadata): <- adds mod for blockchain chain ID enum

---
+++ src/metadata/blockchain_chain_id.rs
@@ -0,0 +1,141 @@
+use std::fmt;
+
+use ethereum_types::H256 as EthHash;
+
+use crate::{
+    chains::eth::eth_crypto_utils::keccak_hash_bytes,
+    types::{Byte, Bytes, Result},
+};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum BlockchainChainId {
+    EthereumMainnet, // NOTE: 0x5fe7f9
+    EthereumRinkeby, // NOTE: 0x69c322
+    EthereumRopsten, // NOTE: 0xf34368
+    BitcoinMainnet,  // NOTE: 0xec97de
+    BitcoinTestnet,  // NOTE: 0x8afeb2
+    EosMainnet,      // NOTE: 0xe7261c
+    TelosMainnet,    // NOTE: 0x8c7109
+}
+
+impl BlockchainChainId {
+    fn get_all() -> Vec<Self> {
+        // TODO How to ensure this vec always contains all members?
+        vec![
+            Self::EthereumMainnet,
+            Self::EthereumRopsten,
+            Self::EthereumRinkeby,
+            Self::BitcoinMainnet,
+            Self::BitcoinTestnet,
+            Self::EosMainnet,
+            Self::TelosMainnet,
+        ]
+    }
+
+    fn to_hash(&self) -> EthHash {
+        keccak_hash_bytes(&match self {
+            Self::EthereumMainnet => {
+                let chain_id = 1u8;
+                chain_id.to_le_bytes().to_vec()
+            },
+            Self::EthereumRinkeby => {
+                let chain_id = 3u8;
+                chain_id.to_le_bytes().to_vec()
+            },
+            Self::EthereumRopsten => {
+                let chain_id = 4u8;
+                chain_id.to_le_bytes().to_vec()
+            },
+            Self::BitcoinMainnet => {
+                let chain_id = "Bitcoin";
+                chain_id.as_bytes().to_vec()
+            },
+            Self::BitcoinTestnet => {
+                let chain_id = "Testnet";
+                chain_id.as_bytes().to_vec()
+            },
+            Self::EosMainnet => {
+                let chain_id = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906";
+                hex::decode(chain_id).unwrap_or_else(|_| vec![])
+            },
+            Self::TelosMainnet => {
+                let chain_id = "4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11";
+                hex::decode(chain_id).unwrap_or_else(|_| vec![])
+            },
+        })
+    }
+
+    fn to_hex(&self) -> String {
+        hex::encode(self.to_bytes())
+    }
+
+    fn to_bytes(&self) -> Bytes {
+        self.to_hash()[..3].to_vec()
+    }
+
+    fn from_bytes(bytes: &[Byte]) -> Result<Self> {
+        let maybe_self = Self::get_all()
+            .iter()
+            .map(|id| {
+                let id_bytes = id.to_bytes();
+                if id_bytes == bytes {
+                    Some(id.clone())
+                } else {
+                    None
+                }
+            })
+            .filter(|x| x.is_some())
+            .collect::<Vec<Option<Self>>>();
+        match maybe_self.len() {
+            1 => maybe_self[0]
+                .clone()
+                .ok_or_else(|| "Failed to unwrap `maybe_self` from option!".into()),
+            0 => Err(format!(
+                "Unrecognized version byte for `BlockchainChainId`: 0x{}",
+                hex::encode(bytes)
+            )
+            .into()),
+            _ => {
+                Err(format!("`BlockchainChainId` collision! > 1 chain ID has the same 1st 3 bytes when hashed!").into())
+            },
+        }
+    }
+
+    #[cfg(test)]
+    fn print_all() {
+        Self::get_all().iter().for_each(|id| println!("{}", id))
+    }
+}
+
+impl fmt::Display for BlockchainChainId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::EosMainnet => write!(f, "Eos Mainnet: 0x{}", self.to_hex()),
+            Self::TelosMainnet => write!(f, "Telos Mainnet: 0x{}", self.to_hex()),
+            Self::BitcoinMainnet => write!(f, "Bitcoin Mainnet: 0x{}", self.to_hex()),
+            Self::BitcoinTestnet => write!(f, "Bitcoin Testnet: 0x{}", self.to_hex()),
+            Self::EthereumMainnet => write!(f, "Ethereum Mainnet: 0x{}", self.to_hex()),
+            Self::EthereumRinkeby => write!(f, "Ethereum Rinkeby Testnet: 0x{}", self.to_hex()),
+            Self::EthereumRopsten => write!(f, "Ethereum Ropsten Testnet: 0x{}", self.to_hex()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_print_all_ids() {
+        BlockchainChainId::print_all();
+    }
+
+    #[test]
+    fn should_perform_blockchain_chain_ids_bytes_round_trip() {
+        BlockchainChainId::get_all().iter().for_each(|id| {
+            let byte = id.to_bytes();
+            let result = BlockchainChainId::from_bytes(&byte).unwrap();
+            assert_eq!(&result, id);
+        });
+    }
+}
