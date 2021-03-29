55a614defe28a3c35f7e1329d993f9a3c573bd0b
Greg Kapka <greg@oraclize.it>
2021-03-29 11:20:15 +0000

    ref(erc20-on-eos): <- fix log contains topic check to use the topic directly

--- src/erc20_on_eos/eth/peg_in_info.rs
+++ src/erc20_on_eos/eth/peg_in_info.rs
@@ -9,10 +9,7 @@ use crate::{
         eos::eos_utils::remove_symbol_from_eos_asset,
         eth::{
             eth_constants::{ETH_ADDRESS_SIZE_IN_BYTES, ETH_WORD_SIZE_IN_BYTES},
-            eth_contracts::erc20_vault::{
-                ERC20_VAULT_PEG_IN_EVENT_WITHOUT_USER_DATA_TOPIC,
-                ERC20_VAULT_PEG_IN_EVENT_WITHOUT_USER_DATA_TOPIC_HEX,
-            },
+            eth_contracts::erc20_vault::ERC20_VAULT_PEG_IN_EVENT_WITHOUT_USER_DATA_TOPIC,
             eth_database_utils::{get_erc20_on_eos_smart_contract_address_from_db, get_eth_canon_block_from_db},
             eth_log::{EthLog, EthLogs},
             eth_receipt::{EthReceipt, EthReceipts},
@@ -64,9 +61,7 @@ impl Erc20OnEosPegInInfos {
     }
 
     fn is_log_erc20_peg_in(log: &EthLog) -> Result<bool> {
-        Ok(log.contains_topic(&EthHash::from_slice(
-            &hex::decode(&ERC20_VAULT_PEG_IN_EVENT_WITHOUT_USER_DATA_TOPIC_HEX)?[..],
-        )))
+        Ok(log.contains_topic(&ERC20_VAULT_PEG_IN_EVENT_WITHOUT_USER_DATA_TOPIC))
     }
 
     pub fn is_log_supported_erc20_peg_in(log: &EthLog, token_dictionary: &EosEthTokenDictionary) -> Result<bool> {
