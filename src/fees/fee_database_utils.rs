e8179a580d8e5a681fa41598e2e8a86e2e789391
Greg Kapka <greg@oraclize.it>
2021-04-13 12:17:39 +0100

    feat(fee-db-utils): <- adds tests to those

--- src/fees/fee_database_utils.rs
+++ src/fees/fee_database_utils.rs
@@ -34,12 +34,18 @@ pub fn get_btc_on_eth_peg_out_basis_points_from_db<D: DatabaseInterface>(db: &D)
 }
 
 pub fn put_btc_on_eth_peg_in_basis_points_in_db<D: DatabaseInterface>(db: &D, basis_points: u64) -> Result<()> {
-    debug!("✔ Putting `BTC_ON_ETH_PEG_IN_BASIS_POINTS_KEY` of {} in db...", basis_points);
+    debug!(
+        "✔ Putting `BTC_ON_ETH_PEG_IN_BASIS_POINTS_KEY` of {} in db...",
+        basis_points
+    );
     put_u64_in_db(db, &BTC_ON_ETH_PEG_IN_BASIS_POINTS_KEY.to_vec(), basis_points)
 }
 
 pub fn put_btc_on_eth_peg_out_basis_points_in_db<D: DatabaseInterface>(db: &D, basis_points: u64) -> Result<()> {
-    debug!("✔ Putting `BTC_ON_ETH_PEG_OUT_BASIS_POINTS_KEY` of {} in db...", basis_points);
+    debug!(
+        "✔ Putting `BTC_ON_ETH_PEG_OUT_BASIS_POINTS_KEY` of {} in db...",
+        basis_points
+    );
     put_u64_in_db(db, &BTC_ON_ETH_PEG_OUT_BASIS_POINTS_KEY.to_vec(), basis_points)
 }
 
@@ -57,4 +63,72 @@ pub fn increment_btc_on_eth_accrued_fees<D: DatabaseInterface>(db: &D, increment
         debug!("✔        Total after incremeneting: {}", total_after_incrementing);
         put_btc_on_eth_accrued_fees_in_db(db, total_after_incrementing)
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_utils::get_test_database;
+
+    #[test]
+    fn should_put_and_get_btc_on_eth_peg_in_basis_points_in_db() {
+        let basis_points: u64 = 1337;
+        let db = get_test_database();
+        put_btc_on_eth_peg_in_basis_points_in_db(&db, basis_points).unwrap();
+        let result = get_btc_on_eth_peg_in_basis_points_from_db(&db).unwrap();
+        assert_eq!(result, basis_points);
+    }
+
+    #[test]
+    fn should_put_and_get_btc_on_eth_peg_out_basis_points_in_db() {
+        let basis_points: u64 = 1337;
+        let db = get_test_database();
+        put_btc_on_eth_peg_out_basis_points_in_db(&db, basis_points).unwrap();
+        let result = get_btc_on_eth_peg_out_basis_points_from_db(&db).unwrap();
+        assert_eq!(result, basis_points);
+    }
+
+    #[test]
+    fn should_put_and_get_btc_on_eth_accrued_fees_in_db() {
+        let fees: u64 = 1337;
+        let db = get_test_database();
+        put_btc_on_eth_accrued_fees_in_db(&db, fees).unwrap();
+        let result = get_btc_on_eth_accrued_fees_from_db(&db).unwrap();
+        assert_eq!(result, fees);
+    }
+
+    #[test]
+    fn get_btc_on_eth_peg_in_basis_points_from_db_should_default_to_zero() {
+        let db = get_test_database();
+        let expected_result = 0;
+        let result = get_btc_on_eth_peg_in_basis_points_from_db(&db).unwrap();
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn get_btc_on_eth_peg_out_basis_points_from_db_should_default_to_zero() {
+        let db = get_test_database();
+        let expected_result = 0;
+        let result = get_btc_on_eth_peg_out_basis_points_from_db(&db).unwrap();
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn get_btc_on_eth_accrued_fees_from_db_should_default_to_zero() {
+        let db = get_test_database();
+        let expected_result = 0;
+        let result = get_btc_on_eth_accrued_fees_from_db(&db).unwrap();
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_increment_btc_on_eth_accrued_fees_in_db() {
+        let db = get_test_database();
+        let start_value = 1337;
+        let increment_amount = 1;
+        put_btc_on_eth_accrued_fees_in_db(&db, start_value).unwrap();
+        increment_btc_on_eth_accrued_fees(&db, increment_amount).unwrap();
+        let result = get_btc_on_eth_accrued_fees_from_db(&db).unwrap();
+        assert_eq!(result, start_value + increment_amount);
+    }
 }
