6488ab1d7c2886e4af2ecf5376258fe1f34a3be0
Greg Kapka <greg@oraclize.it>
2021-04-13 17:32:41 +0100

    feat(btc-on-eth): <- adds fee handling to peg outs of those

--- src/btc_on_eth/eth/redeem_info.rs
+++ src/btc_on_eth/eth/redeem_info.rs
@@ -182,6 +182,7 @@ pub fn maybe_parse_redeem_infos_and_add_to_state<D: DatabaseInterface>(state: Et
             false => {
                 info!("✔ Receipts in canon block ∴ parsing infos...");
                 BtcOnEthRedeemInfos::from_eth_submission_material(&submission_material)
+                    .and_then(|infos| infos.maybe_account_for_fees(&state.db))
                     .and_then(|infos| state.add_btc_on_eth_redeem_infos(infos))
             },
         }
