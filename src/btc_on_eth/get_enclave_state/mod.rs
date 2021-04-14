462c26621b51b09a77f1b9d64774aeed37e51fe7
Greg Kapka <greg@oraclize.it>
2021-04-14 14:00:30 +0100

    feat(fee-enclave-state): <- update that to work with multi-token setups

--- src/btc_on_eth/get_enclave_state/mod.rs
+++ src/btc_on_eth/get_enclave_state/mod.rs
@@ -4,7 +4,7 @@ use crate::{
     btc_on_eth::check_core_is_initialized::check_core_is_initialized,
     chains::{btc::btc_enclave_state::BtcEnclaveState, eth::eth_enclave_state::EthEnclaveState},
     enclave_info::EnclaveInfo,
-    fees::fee_enclave_state::FeeEnclaveState,
+    fees::fee_enclave_state::FeesEnclaveState,
     traits::DatabaseInterface,
     types::Result,
 };
@@ -14,7 +14,7 @@ struct EnclaveState {
     info: EnclaveInfo,
     btc: BtcEnclaveState,
     eth: EthEnclaveState,
-    fees: FeeEnclaveState,
+    fees: FeesEnclaveState,
 }
 
 impl EnclaveState {
@@ -23,7 +23,7 @@ impl EnclaveState {
             info: EnclaveInfo::new(),
             btc: BtcEnclaveState::new(db)?,
             eth: EthEnclaveState::new_for_btc_on_eth(db)?,
-            fees: FeeEnclaveState::new_for_btc_on_eth(db)?,
+            fees: FeesEnclaveState::new_for_btc_on_eth(db)?,
         })
     }
 
--- src/fees/fee_enclave_state.rs
+++ src/fees/fee_enclave_state.rs
@@ -1,3 +1,4 @@
+use derive_more::{Constructor, Deref};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -14,22 +15,40 @@ use crate::{
 };
 
 #[derive(Serialize, Deserialize)]
-pub struct FeeEnclaveState {
+pub struct FeesEnclaveState {
+    fees_enabled: bool,
+    fees: FeeStateForTokens,
+}
+
+impl FeesEnclaveState {
+    pub fn new_for_btc_on_eth<D: DatabaseInterface>(db: &D) -> Result<Self> {
+        Ok(Self {
+            fees_enabled: !DISABLE_FEES,
+            fees: FeeStateForTokens::new_for_btc_on_eth(db)?,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FeeStateForToken {
+    token_symbol: String,
     accrued_fees: u64,
     peg_in_basis_points: u64,
     peg_out_basis_points: u64,
     accrued_fees_db_key: String,
-    fees_enabled: bool,
 }
 
-impl FeeEnclaveState {
+#[derive(Serialize, Deserialize, Deref, Constructor)]
+pub struct FeeStateForTokens(Vec<FeeStateForToken>);
+
+impl FeeStateForTokens {
     pub fn new_for_btc_on_eth<D: DatabaseInterface>(db: &D) -> Result<Self> {
-        Ok(Self {
+        Ok(Self::new(vec![FeeStateForToken {
+            token_symbol: "BTC".to_string(),
             accrued_fees: get_btc_on_eth_accrued_fees_from_db(db)?,
             accrued_fees_db_key: hex::encode(*BTC_ON_ETH_ACCRUED_FEES_KEY),
             peg_in_basis_points: get_btc_on_eth_peg_in_basis_points_from_db(db)?,
             peg_out_basis_points: get_btc_on_eth_peg_out_basis_points_from_db(db)?,
-            fees_enabled: !DISABLE_FEES,
-        })
+        }]))
     }
 }
