3bc23e3ee8786cd2b0e13f220f571db05a6235d1
Greg Kapka <greg@oraclize.it>
2021-05-05 11:13:45 +0100

    feat(erc20-on-evm): <- split fee accounting up into 2 steps on EVM side of that

--- src/erc20_on_evm/evm/account_for_fees.rs
+++ src/erc20_on_evm/evm/account_for_fees.rs
@@ -7,19 +7,43 @@ use crate::{
     types::Result,
 };
 
-pub fn maybe_account_for_fees<D: DatabaseInterface>(state: EvmState<D>) -> Result<EvmState<D>> {
-    info!("✔ Maybe accounting for fees during EVM block submission...");
+fn update_accrued_fees_in_dictionary_and_return_state<D: DatabaseInterface>(state: EvmState<D>) -> Result<EvmState<D>> {
     if DISABLE_FEES {
-        info!("✔ Taking fees is disabled ∴ not taking any fees!");
+        info!("✔ Fees are disabled ∴ not accounting for any in `EthOnEvmEthTxInfos`!");
         Ok(state)
     } else if state.erc20_on_evm_eth_tx_infos.is_empty() {
-        info!("✔ Not tx info in state ∴ not taking any fees!");
+        info!("✔ Not `EthOnEvmEthTxInfos` in state ∴ not taking any fees!");
+        Ok(state)
+    } else {
+        info!("✔ Accruing fees during EVM block submission...");
+        EthEvmTokenDictionary::get_from_db(&state.db)
+            .and_then(|ref dictionary| {
+                dictionary.increment_accrued_fees_and_save_in_db(
+                    &state.db,
+                    state.erc20_on_evm_eth_tx_infos.get_fees(dictionary)?,
+                )
+            })
+            .and(Ok(state))
+    }
+}
+
+pub fn account_for_fees_in_eth_tx_infos_in_state<D: DatabaseInterface>(state: EvmState<D>) -> Result<EvmState<D>> {
+    if DISABLE_FEES {
+        info!("✔ Fees are disabled ∴ not accounting for any in `EthOnEvmEthTxInfos`!");
+        Ok(state)
+    } else if state.erc20_on_evm_eth_tx_infos.is_empty() {
+        info!("✔ Not `EthOnEvmEthTxInfos` in state ∴ not taking any fees!");
         Ok(state)
     } else {
         info!("✔ Accounting for fees in `EthOnEvmEthTxInfos` during EVM block submission...");
-        let tx_infos = state.erc20_on_evm_eth_tx_infos.clone();
-        let dictionary = EthEvmTokenDictionary::get_from_db(&state.db)?;
-        dictionary.increment_accrued_fees_and_save_in_db(&state.db, tx_infos.get_fees(&dictionary)?)?;
-        state.replace_erc20_on_evm_eth_tx_infos(tx_infos.subtract_fees(&dictionary)?)
+        EthEvmTokenDictionary::get_from_db(&state.db).and_then(|ref dictionary| {
+            let tx_infos = state.erc20_on_evm_eth_tx_infos.clone();
+            state.replace_erc20_on_evm_eth_tx_infos(tx_infos.subtract_fees(dictionary)?)
+        })
     }
+}
+
+pub fn maybe_account_for_fees<D: DatabaseInterface>(state: EvmState<D>) -> Result<EvmState<D>> {
+    info!("✔ Accounting for fees in `EthOnEvmEthTxInfos` during EVM block submission...");
+    update_accrued_fees_in_dictionary_and_return_state(state).and_then(account_for_fees_in_eth_tx_infos_in_state)
 }
