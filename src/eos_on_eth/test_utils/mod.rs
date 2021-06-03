b4c08d9c5be5c3a58ae39e148d8c25132d757174
Greg Kapka <greg@oraclize.it>
2021-06-03 11:24:47 +0100

    feat(eos-on-eth): <- adds new test sample and getter to that + updates sample dictionary

--- src/eos_on_eth/test_utils/mod.rs
+++ src/eos_on_eth/test_utils/mod.rs
@@ -49,12 +49,23 @@ pub fn get_eth_submission_material_with_bad_eos_account_name() -> EthSubmissionM
 }
 
 pub fn get_sample_eos_eth_token_dictionary() -> EosEthTokenDictionary {
-    EosEthTokenDictionary::new(vec![EosEthTokenDictionaryEntry::from_str(&
-    "{\"eos_token_decimals\":4,\"eth_token_decimals\":18,\"eos_symbol\":\"EOS\",\"eth_symbol\":\"PEOS\",\"eos_address\":\"eosio.token\",\"eth_address\":\"711c50b31ee0b9e8ed4d434819ac20b4fbbb5532\"}").unwrap()])
+    EosEthTokenDictionary::from_str("[{\"eos_token_decimals\":4,\"eth_token_decimals\":18,\"eos_symbol\":\"EOS\",\"eth_symbol\":\"PEOS\",\"eos_address\":\"eosio.token\",\"eth_address\":\"711c50b31ee0b9e8ed4d434819ac20b4fbbb5532\"},{\"eth_token_decimals\":18,\"eos_token_decimals\":4,\"eth_symbol\":\"TLOS\",\"eos_symbol\":\"TLOS\",\"eth_address\":\"7825e833d495f3d1c28872415a4aee339d26ac88\",\"eos_address\":\"eosio.token\"}]").unwrap()
+}
+
+pub fn get_eth_submission_material_with_two_peg_ins() -> EthSubmissionMaterial {
+    EthSubmissionMaterial::from_str(
+        &read_to_string("src/eos_on_eth/test_utils/eth-submission-material-with-two-eos-on-eth-pegins.json").unwrap(),
+    )
+    .unwrap()
 }
 
 mod tests {
     use super::*;
+
+    #[test]
+    fn should_get_sample_eos_eth_token_dictionary() {
+        get_sample_eos_eth_token_dictionary();
+    }
 
     #[test]
     fn should_get_eos_submission_material_n() {
