293532c5e4185b5ec25439fe2afabe6a9c0e3579
Greg Kapka <greg@oraclize.it>
2021-07-02 17:36:26 +0100

    feat(core_type): <- adds enum iter to that & `to_db_key_prefix` helper

--- src/core_type.rs
+++ src/core_type.rs
@@ -1,12 +1,21 @@
 #![allow(dead_code)]
 use std::fmt;
 
+use strum_macros::EnumIter;
+
+#[derive(Clone, Copy, EnumIter)]
 pub enum CoreType {
     BtcOnEth,
     BtcOnEos,
     EosOnEth,
     Erc20OnEos,
     Erc20OnEvm,
+}
+
+impl CoreType {
+    pub fn to_db_key_prefix(&self) -> String {
+        self.to_string().to_lowercase().replace("_", "-")
+    }
 }
 
 impl fmt::Display for CoreType {
@@ -19,5 +28,20 @@ impl fmt::Display for CoreType {
             Self::Erc20OnEvm => "ERC20_ON_EVM",
         };
         write!(f, "{}", s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use strum::IntoEnumIterator;
+
+    use super::*;
+
+    #[test]
+    fn should_get_core_type_as_db_key_prefix() {
+        let expected_results = vec!["btc-on-eth", "btc-on-eos", "eos-on-eth", "erc20-on-eos", "erc20-on-evm"];
+        CoreType::iter()
+            .zip(expected_results.iter())
+            .for_each(|(core_type, expected_result)| assert_eq!(&core_type.to_db_key_prefix(), *expected_result))
     }
 }
