98da0193f3e7d1195ab6f6dac0b21833a98ef2a3
Greg Kapka <greg@oraclize.it>
2021-07-02 17:37:40 +0100

    ref(fee_constants): <- use `CoreType` enum in that to DRY it out

--- src/fees/fee_constants.rs
+++ src/fees/fee_constants.rs
@@ -11,10 +11,11 @@ pub const DISABLE_FEES: bool = true;
 pub const MAX_FEE_BASIS_POINTS: u64 = 1000;
 
 lazy_static! {
-    pub static ref BTC_ON_ETH_FEE_DB_KEYS: FeeConstantDbKeys = FeeConstantDbKeys::new(CoreType::BtcOnEth);
-    pub static ref BTC_ON_EOS_FEE_DB_KEYS: FeeConstantDbKeys = FeeConstantDbKeys::new(CoreType::BtcOnEos);
+    pub static ref BTC_ON_ETH_FEE_DB_KEYS: FeeConstantDbKeys = FeeConstantDbKeys::new_for_btc_on_eth();
+    pub static ref BTC_ON_EOS_FEE_DB_KEYS: FeeConstantDbKeys = FeeConstantDbKeys::new_for_btc_on_eos();
 }
 
+#[derive(Clone)]
 pub struct FeeConstantDbKeys {
     pub core_type: CoreType,
     pub accrued_fees_db_key: Bytes,
@@ -25,26 +26,34 @@ pub struct FeeConstantDbKeys {
 
 impl FeeConstantDbKeys {
     pub fn new(core_type: CoreType) -> Self {
-        match core_type {
-            CoreType::BtcOnEth => Self {
-                core_type,
-                accrued_fees_db_key: get_prefixed_db_key("btc-on-eth-accrued-fees-key").to_vec(),
-                peg_in_basis_points_db_key: get_prefixed_db_key("btc-on-eth-last-fee-withdrawal-timestamp").to_vec(),
-                last_fee_withdrawal_db_key: get_prefixed_db_key("btc-on-eth-peg-in-basis-points-key").to_vec(),
-                peg_out_basis_points_db_key: get_prefixed_db_key("btc-on-eth-peg-out-basis-points-key").to_vec(),
-            },
-            CoreType::BtcOnEos => Self {
-                core_type,
-                accrued_fees_db_key: get_prefixed_db_key("btc-on-eos-accrued-fees-key").to_vec(),
-                peg_in_basis_points_db_key: get_prefixed_db_key("btc-on-eos-last-fee-withdrawal-timestamp").to_vec(),
-                last_fee_withdrawal_db_key: get_prefixed_db_key("btc-on-eos-peg-in-basis-points-key").to_vec(),
-                peg_out_basis_points_db_key: get_prefixed_db_key("btc-on-eos-peg-out-basis-points-key").to_vec(),
-            },
-            _ => unimplemented!(
-                "`FeeConstantDbKeys` struct not implemented for core type: {}!",
-                core_type
-            ),
+        Self {
+            core_type,
+            accrued_fees_db_key: get_prefixed_db_key(&format!("{}-accrued-fees-key", core_type.to_db_key_prefix()))
+                .to_vec(),
+            peg_in_basis_points_db_key: get_prefixed_db_key(&format!(
+                "{}-last-fee-withdrawal-timestamp",
+                core_type.to_db_key_prefix()
+            ))
+            .to_vec(),
+            last_fee_withdrawal_db_key: get_prefixed_db_key(&format!(
+                "{}-peg-in-basis-points-key",
+                core_type.to_db_key_prefix()
+            ))
+            .to_vec(),
+            peg_out_basis_points_db_key: get_prefixed_db_key(&format!(
+                "{}-peg-out-basis-points-key",
+                core_type.to_db_key_prefix()
+            ))
+            .to_vec(),
         }
+    }
+
+    pub fn new_for_btc_on_eth() -> Self {
+        Self::new(CoreType::BtcOnEth)
+    }
+
+    pub fn new_for_btc_on_eos() -> Self {
+        Self::new(CoreType::BtcOnEos)
     }
 
     pub fn to_json(&self) -> JsonValue {
