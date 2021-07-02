c9a40ef8562137ebe87caf8d532c3e2fc078e989
Greg Kapka <greg@oraclize.it>
2021-07-02 16:19:03 +0100

    ref(fee-debug-fxns): <- move those to respective instances' dir

--- src/fees/mod.rs
+++ src/fees/mod.rs
@@ -1,6 +1,5 @@
 pub(crate) mod fee_constants;
 pub(crate) mod fee_database_utils;
-pub(crate) mod fee_debug_functions;
 pub(crate) mod fee_enclave_state;
 pub(crate) mod fee_utils;
 pub(crate) mod fee_withdrawals;
