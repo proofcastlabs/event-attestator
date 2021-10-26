31eaef2c0d2ac5a052db20ea634f45683927b461
Greg Kapka <greg@oraclize.it>
2021-10-26 10:43:22 +0100

    feat(btc-on-eos): <- increment the EOS nonce during BTC block reprocessing in that

--- src/btc_on_eos/debug_functions/block_reprocessors.rs
+++ src/btc_on_eos/debug_functions/block_reprocessors.rs
@@ -27,13 +27,19 @@ use crate::{
     },
     chains::{
         btc::{
-            btc_database_utils::{get_btc_chain_id_from_db, get_btc_latest_block_from_db, start_btc_db_transaction},
+            btc_database_utils::{
+                end_btc_db_transaction,
+                get_btc_chain_id_from_db,
+                get_btc_latest_block_from_db,
+                start_btc_db_transaction,
+            },
             btc_state::BtcState,
             btc_submission_material::parse_submission_material_and_put_in_state,
             filter_p2sh_deposit_txs::filter_p2sh_deposit_txs_and_add_to_state,
             get_btc_block_in_db_format::create_btc_block_in_db_format_and_put_in_state,
             get_deposit_info_hash_map::get_deposit_info_hash_map_and_put_in_state,
             increment_btc_account_nonce::maybe_increment_btc_signature_nonce_and_return_eos_state,
+            increment_eos_nonce::maybe_increment_eos_nonce,
             validate_btc_block_header::validate_btc_block_header_in_state,
             validate_btc_difficulty::validate_difficulty_of_btc_block_in_state,
             validate_btc_merkle_root::validate_btc_merkle_root,
@@ -155,7 +161,7 @@ fn debug_reprocess_btc_block_for_stale_eos_tx_maybe_accruing_fees<D: DatabaseInt
         })
         .and_then(|state| {
             info!("✔ Maybe signing reprocessed minting txs...");
-            get_signed_eos_ptoken_issue_txs(
+            let eos_signed_txs = get_signed_eos_ptoken_issue_txs(
                 state.get_eos_ref_block_num()?,
                 state.get_eos_ref_block_prefix()?,
                 &get_eos_chain_id_from_db(&state.db)?,
@@ -163,15 +169,14 @@ fn debug_reprocess_btc_block_for_stale_eos_tx_maybe_accruing_fees<D: DatabaseInt
                 &get_eos_account_name_string_from_db(&state.db)?,
                 &state.btc_on_eos_minting_params,
                 &get_btc_chain_id_from_db(&state.db)?,
-            )
-            .and_then(|eos_signed_txs| {
-                info!("✔ EOS Signed Txs: {:?}", eos_signed_txs);
-                state.add_eos_signed_txs(eos_signed_txs)
-            })
+            )?;
+            info!("✔ EOS Signed Txs: {:?}", eos_signed_txs);
+            state.add_eos_signed_txs(eos_signed_txs)
         })
+        .and_then(maybe_increment_eos_nonce)
         .and_then(|state| {
             info!("✔ Getting BTC output json and putting in state...");
-            Ok(serde_json::to_string(&BtcOutput {
+            let output = serde_json::to_string(&BtcOutput {
                 btc_latest_block_number: get_btc_latest_block_from_db(&state.db)?.height,
                 eos_signed_transactions: match &state.eos_signed_txs.len() {
                     0 => vec![],
@@ -181,30 +186,12 @@ fn debug_reprocess_btc_block_for_stale_eos_tx_maybe_accruing_fees<D: DatabaseInt
                         get_eos_account_nonce_from_db(&state.db)?,
                     )?,
                 },
-            })?)
-            .and_then(|output| state.add_output_json_string(output))
+            })?;
+            state.add_output_json_string(output)
         })
+        .and_then(end_btc_db_transaction)
         .and_then(get_btc_output_as_string)
         .map(prepend_debug_output_marker_to_string)
-}
-
-/// # Debug Reprocess EOS Block For Stale Transaction
-///
-/// This function will take a passed in EOS block submission material and run it through the
-/// submission pipeline, signing any signatures for pegouts it may find in the block
-///
-/// ### NOTE:
-///
-/// This version of the function _will_ account for fees so the outputted transaction's value is
-/// correct, but it will __NOT__ accrue those fees onto the balance stored in the encrypted database.
-/// This is to not double-count the fee if this block had already had a failed processing via an
-/// organic block submission.
-///
-/// ### BEWARE:
-/// If you don't broadcast the transaction outputted from this function, ALL future BTC transactions will
-/// fail due to the core having an incorret set of UTXOs!
-pub fn debug_reprocess_eos_block<D: DatabaseInterface>(db: D, block_json: &str) -> Result<String> {
-    debug_reprocess_eos_block_maybe_accruing_fees(db, block_json, false)
 }
 
 /// # Debug Reprocess BTC Block For Stale Transaction
@@ -222,11 +209,10 @@ pub fn debug_reprocess_eos_block<D: DatabaseInterface>(db: D, block_json: &str) 
 /// organic block submission.
 ///
 /// ### BEWARE:
-/// This function does NOT increment the EOS  nonce (since it is not critical for correct
-/// transaction creation) and so outputted reports will NOT contain correct nonces. This is to ensure
-/// future transactions written by the proper submit-ETH-block pipeline will remain contiguous. The
-/// user of this function should understand why this is the case, and thus should be able to modify
-/// the outputted reports to slot into the external database correctly.
+/// This function CAN increment the EOS nonce if transactions are signed. The user of this function
+/// should understand what this means when inserting the report outputted from this debug function.
+/// If this output is to _replace_ an existing report, the nonces in the report and in the core's
+/// database should be modified accordingly.
 pub fn debug_reprocess_btc_block_for_stale_eos_tx<D: DatabaseInterface>(
     db: D,
     block_json_string: &str,
@@ -249,11 +235,10 @@ pub fn debug_reprocess_btc_block_for_stale_eos_tx<D: DatabaseInterface>(
 /// the blocks organic submission to the core.
 ///
 /// ### BEWARE:
-/// This function does NOT increment the EOS  nonce (since it is not critical for correct
-/// transaction creation) and so outputted reports will NOT contain correct nonces. This is to ensure
-/// future transactions written by the proper submit-ETH-block pipeline will remain contiguous. The
-/// user of this function should understand why this is the case, and thus should be able to modify
-/// the outputted reports to slot into the external database correctly.
+/// This function CAN increment the EOS nonce if transactions are signed. The user of this function
+/// should understand what this means when inserting the report outputted from this debug function.
+/// If this output is to _replace_ an existing report, the nonces in the report and in the core's
+/// database should be modified accordingly.
 pub fn debug_reprocess_btc_block_for_stale_eos_tx_with_fee_accrual<D: DatabaseInterface>(
     db: D,
     block_json_string: &str,
@@ -278,4 +263,23 @@ pub fn debug_reprocess_btc_block_for_stale_eos_tx_with_fee_accrual<D: DatabaseIn
 /// fail due to the core having an incorret set of UTXOs!
 pub fn debug_reprocess_eos_block_with_fee_accrual<D: DatabaseInterface>(db: D, block_json: &str) -> Result<String> {
     debug_reprocess_eos_block_maybe_accruing_fees(db, block_json, true)
+}
+
+/// # Debug Reprocess EOS Block For Stale Transaction
+///
+/// This function will take a passed in EOS block submission material and run it through the
+/// submission pipeline, signing any signatures for pegouts it may find in the block
+///
+/// ### NOTE:
+///
+/// This version of the function _will_ account for fees so the outputted transaction's value is
+/// correct, but it will __NOT__ accrue those fees onto the balance stored in the encrypted database.
+/// This is to not double-count the fee if this block had already had a failed processing via an
+/// organic block submission.
+///
+/// ### BEWARE:
+/// If you don't broadcast the transaction outputted from this function, ALL future BTC transactions will
+/// fail due to the core having an incorret set of UTXOs!
+pub fn debug_reprocess_eos_block<D: DatabaseInterface>(db: D, block_json: &str) -> Result<String> {
+    debug_reprocess_eos_block_maybe_accruing_fees(db, block_json, false)
 }
