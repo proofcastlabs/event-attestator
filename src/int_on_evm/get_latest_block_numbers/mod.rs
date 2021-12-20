a2a64cf1ff750c08ce399e186663f5571868d64a
Greg Kapka <greg@oraclize.it>
2021-12-20 10:20:07 +0000

    feat(v2): <- use correct prefix in block number getters in that

--- src/int_on_evm/get_latest_block_numbers/mod.rs
+++ src/int_on_evm/get_latest_block_numbers/mod.rs
@@ -9,7 +9,7 @@ use crate::{
 
 #[derive(Serialize, Deserialize)]
 struct BlockNumbers {
-    eth_latest_block_number: usize,
+    int_latest_block_number: usize,
     evm_latest_block_number: usize,
 }
 
@@ -18,12 +18,12 @@ struct BlockNumbers {
 /// This function returns a JSON containing the last processed block number of each of the
 /// blockchains this instance manages.
 pub fn get_latest_block_numbers<D: DatabaseInterface>(db: D) -> Result<String> {
-    info!("✔ Getting latest `ERC20-on-EVM` block numbers...");
+    info!("✔ Getting latest `int-on-evm` block numbers...");
     let eth_db_utils = EthDbUtils::new(&db);
     let evm_db_utils = EvmDbUtils::new(&db);
     check_core_is_initialized(&eth_db_utils, &evm_db_utils).and_then(|_| {
         Ok(serde_json::to_string(&BlockNumbers {
-            eth_latest_block_number: eth_db_utils.get_latest_eth_block_number()?,
+            int_latest_block_number: eth_db_utils.get_latest_eth_block_number()?,
             evm_latest_block_number: evm_db_utils.get_latest_eth_block_number()?,
         })?)
     })
