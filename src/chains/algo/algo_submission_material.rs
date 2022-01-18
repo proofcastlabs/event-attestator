72896eee06d22b59dfebbcdf0864af4201c9b175
Greg Kapka <greg@oraclize.it>
2022-01-18 21:10:28 +0000

    feat(algo-sub-mat): <- adds mod for that

--- src/chains/algo/algo_submission_material.rs
+++ src/chains/algo/algo_submission_material.rs
@@ -1,9 +1,12 @@
+use std::str::FromStr;
+
+use rust_algorand::AlgorandBlock;
+
 use crate::{chains::algo::algo_state::AlgoState, traits::DatabaseInterface, types::Result};
 
 pub fn parse_algo_submission_material_and_put_in_state<'a, D: DatabaseInterface>(
-    algo_block_json: &str,
+    algo_block_json_str: &str,
     state: AlgoState<'a, D>,
 ) -> Result<AlgoState<'a, D>> {
-    unimplemented!();
-    Ok(state)
+    state.add_algo_block(&AlgorandBlock::from_str(&algo_block_json_str)?)
 }
