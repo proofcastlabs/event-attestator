945052e04bc17157c21f3acda836b0bd5dcc7f0f
Greg Kapka <greg@oraclize.it>
2022-01-25 17:49:01 +0000

    feat(algo-tests-utils): <- adds more block samples to those

--- src/chains/algo/test_utils/mod.rs
+++ src/chains/algo/test_utils/mod.rs
@@ -17,13 +17,22 @@ macro_rules! write_paths_and_getter_fxn {
                     _ => Err(AppError::Custom(format!("Cannot find sample block num: {}", n).into())),
                 }
             }
+
+            pub fn get_all_sample_blocks() -> Vec<AlgorandBlock> {
+                vec![
+                    $(AlgorandBlock::from_str(&read_to_string($path).unwrap()).unwrap(),)*
+                ]
+            }
         }
     }
 }
 
 write_paths_and_getter_fxn!(
     0 => "src/chains/algo/test_utils/block-17962555.json",
-    1 => "src/chains/algo/test_utils/block-17962556.json"
+    1 => "src/chains/algo/test_utils/block-17962556.json",
+    2 => "src/chains/algo/test_utils/block-17962557.json",
+    3 => "src/chains/algo/test_utils/block-17962558.json",
+    4 => "src/chains/algo/test_utils/block-17962559.json"
 );
 
 fn get_sample_block_json_str_n(n: usize) -> String {
