3ae85afe7e0e63a5ae004e2959434ed5a9407a74
Greg Kapka <greg@oraclize.it>
2022-01-26 12:11:51 +0000

    feat(algo): <- adds tail block updater that

--- src/chains/algo/update_algo_canon_block_details.rs
+++ src/chains/algo/update_algo_canon_block_details.rs
@@ -19,9 +19,12 @@ pub fn maybe_update_algo_canon_block_hash_and_return_state<D: DatabaseInterface>
         state.algo_db_utils.maybe_get_new_canon_block_candidate()?,
     )
     .and_then(|maybe_new_canon_block_hash| match maybe_new_canon_block_hash {
-        None => Ok(state),
+        None => {
+            info!("✔ No new ALGO canon block candidate found ∴ not updating!");
+            Ok(state)
+        },
         Some(hash) => {
-            info!("✔ Updating ALGO canon block hash...");
+            info!("✔ New ALGO canon block candidate found ∴ updating...");
             state.algo_db_utils.put_canon_block_hash_in_db(&hash)?;
             Ok(state)
         },
