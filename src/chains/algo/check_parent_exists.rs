deb704f4267248e6d66f0b2e9e5c24e1e7f4a36d
Greg Kapka <greg@oraclize.it>
2022-01-26 16:15:46 +0000

    feat(algo): <- adds mod to check parent exists

---
+++ src/chains/algo/check_parent_exists.rs
@@ -0,0 +1,46 @@
+use crate::{chains::algo::algo_state::AlgoState, traits::DatabaseInterface, types::Result};
+
+pub fn check_parent_of_algo_block_in_state_exists<D: DatabaseInterface>(state: AlgoState<D>) -> Result<AlgoState<D>> {
+    info!("✔ Checking if ALGO block's parent exists in database...");
+    let parent_hash = state.get_submitted_algo_block()?.get_previous_block_hash()?;
+    if state.algo_db_utils.get_block(&parent_hash).is_ok() {
+        info!("✔ ALGO block's parent exists in database!");
+        Ok(state)
+    } else {
+        Err("✘ ALGO block Rejected - no parent exists in database!".into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        chains::algo::test_utils::get_sample_contiguous_blocks,
+        errors::AppError,
+        test_utils::get_test_database,
+    };
+
+    #[test]
+    fn should_check_parent_exists_correctly() {
+        let db = get_test_database();
+        let state = AlgoState::init(&db);
+        let blocks = get_sample_contiguous_blocks();
+        blocks
+            .iter()
+            .for_each(|block| state.algo_db_utils.put_block_in_db(&block).unwrap());
+        let block = blocks[1].clone();
+        let expected_error = "✘ ALGO block Rejected - no parent exists in database!";
+        blocks.iter().enumerate().for_each(|(i, block)| {
+            let state = AlgoState::init(&db).add_submitted_algo_block(&block).unwrap();
+            if i == 0 {
+                match check_parent_of_algo_block_in_state_exists(state) {
+                    Ok(_) => panic!("Should not have succeeded!"),
+                    Err(AppError::Custom(error)) => assert_eq!(error, expected_error),
+                    Err(_) => panic!("Wrong error received!"),
+                }
+            } else {
+                assert!(check_parent_of_algo_block_in_state_exists(state).is_ok())
+            }
+        })
+    }
+}
--- src/chains/algo/mod.rs
+++ src/chains/algo/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod algo_database_transactions;
 pub(crate) mod algo_database_utils;
 pub(crate) mod algo_state;
 pub(crate) mod algo_submission_material;
+pub(crate) mod check_parent_exists;
 pub(crate) mod check_submitted_block_is_subsequent;
 pub(crate) mod core_initialization;
 pub(crate) mod get_candidate_block_hash;
