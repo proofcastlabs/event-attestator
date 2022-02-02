801cf8e5f74865e29c65ae9a27e74a3599651134
Greg Kapka <greg@oraclize.it>
2022-02-02 17:47:05 +0000

    feat(macros): <- factor out that to top level of repo

--- src/macros.rs
+++ src/macros.rs
@@ -70,3 +70,62 @@ macro_rules! create_db_utils_with_getters {
         }
     }
 }
+
+macro_rules! create_diversion_fxns {
+    ($struct_name:expr => $state_name:expr => $tx_infos_name:expr => $($contract_name:expr),*) => {
+        paste! {
+            impl [< $struct_name s>] {
+                $(
+                    pub fn [<divert_to_safe_address_if_destination_is_ $contract_name _contract_address>](&self) -> Self {
+                        Self::new(
+                            self.iter()
+                                .map(|info| {
+                                    info.[<divert_to_safe_address_if_destination_is_ $contract_name _contract_address>]()
+                                })
+                                .collect::<Vec<[<$struct_name>]>>(),
+                        )
+                    }
+                )*
+            }
+
+            impl [< $struct_name >] {
+                fn update_destination_address(&self, new_address: EthAddress) -> Self {
+                    let mut new_self = self.clone();
+                    new_self.destination_address = new_address;
+                    new_self
+                }
+
+                fn divert_to_safe_address_if_destination_matches_address(&self, address: &EthAddress) -> Self {
+                    if self.destination_address == *address {
+                        self.update_destination_address(*SAFE_ETH_ADDRESS)
+                    } else {
+                        self.clone()
+                    }
+                }
+
+                $(
+                    pub fn [<divert_to_safe_address_if_destination_is_ $contract_name _contract_address>](&self) -> Self {
+                        info!("✔ Checking if the destination address matches the {} contract address...", $contract_name);
+                        self.divert_to_safe_address_if_destination_matches_address(&self.[<eth_ $contract_name _address>])
+                    }
+                )*
+            }
+
+            $(
+                pub fn [<maybe_divert_txs_to_safe_address_if_destination_is_ $contract_name _address>]<D: DatabaseInterface>(
+                    state: [< $state_name State>]<D>,
+                ) -> Result<[< $state_name State>]<D>> {
+                    if state.[< $tx_infos_name >].is_empty() {
+                        Ok(state)
+                    } else {
+                        info!("✔ Maybe diverting txs to safe address if destination matches {} address...", $contract_name);
+                        let new_infos = state
+                            .[< $tx_infos_name >]
+                            .[<divert_to_safe_address_if_destination_is_ $contract_name _contract_address>]();
+                        state.[<replace_ $tx_infos_name>](new_infos)
+                    }
+                }
+            )*
+        }
+    }
+}
