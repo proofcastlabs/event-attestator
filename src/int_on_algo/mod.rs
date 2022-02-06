b936e2bbd887f2e1e9cd8221d7e71055fc9c585f
Greg Kapka <greg@oraclize.it>
2022-02-06 13:34:12 +0000

    feat(int-on-algo): <- adds & re-exports method to opt in to asset receipt

--- src/int_on_algo/mod.rs
+++ src/int_on_algo/mod.rs
@@ -17,12 +17,14 @@ pub(crate) mod check_core_is_initialized;
 pub(crate) mod get_enclave_state;
 pub(crate) mod get_latest_block_numbers;
 pub(crate) mod int;
+pub(crate) mod opt_in_to_asset;
 
 pub use crate::int_on_algo::{
     algo::{initialize_algo_core::maybe_initialize_algo_core, submit_algo_block::submit_algo_block_to_core},
     get_enclave_state::get_enclave_state,
     get_latest_block_numbers::get_latest_block_numbers,
     int::{initialize_int_core::maybe_initialize_int_core, submit_int_block::submit_int_block_to_core},
+    opt_in_to_asset::opt_in_to_asset,
 };
 /*
 pub(crate) mod debug_functions;
