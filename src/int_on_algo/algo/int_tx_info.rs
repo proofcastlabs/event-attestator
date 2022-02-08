0a0282915f73af2b9418a77e66aee04c03a9df52
Greg Kapka <greg@oraclize.it>
2022-02-08 12:08:50 +0000

    feat(int-on-algo): <- adds `IntTxInfo` mod to that

---
+++ src/int_on_algo/algo/int_tx_info.rs
@@ -0,0 +1,22 @@
+use derive_more::{Constructor, Deref, IntoIterator};
+use ethereum_types::{Address as EthAddress, H256 as EthHash, U256};
+use rust_algorand::AlgorandAddress;
+
+use crate::{metadata::metadata_chain_id::MetadataChainId, types::Bytes};
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, Constructor)]
+pub struct IntOnAlgoIntTxInfo {
+    pub user_data: Bytes,
+    pub algo_asset_id: u64,
+    pub token_sender: EthAddress,
+    pub native_token_amount: U256,
+    pub router_address: EthAddress,
+    pub originating_tx_hash: EthHash,
+    pub evm_token_address: EthAddress,
+    pub origin_chain_id: MetadataChainId,
+    pub destination_address: AlgorandAddress,
+    pub destination_chain_id: MetadataChainId,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Constructor, Deref, IntoIterator)]
+pub struct IntOnAlgoIntTxInfos(pub Vec<IntOnAlgoIntTxInfo>);
--- src/int_on_algo/algo/mod.rs
+++ src/int_on_algo/algo/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod get_algo_output;
 pub(crate) mod initialize_algo_core;
+pub(crate) mod int_tx_info;
 pub(crate) mod submit_algo_block;
