952f6dd68e6f411645ce449e17542d4f17a79bcd
Greg Kapka <greg@oraclize.it>
2022-03-14 17:31:00 +0000

    feat(utils): <- use `TryInto` in thos to convert bytes to unsigned ints

--- src/utils.rs
+++ src/utils.rs
@@ -57,29 +57,11 @@ pub fn get_prefixed_db_key(suffix: &str) -> [u8; 32] {
 }
 
 pub fn convert_bytes_to_u64(bytes: &[Byte]) -> Result<u64> {
-    match bytes.len() {
-        0..=7 => Err("✘ Not enough bytes to convert to u64!".into()),
-        U64_NUM_BYTES => {
-            let mut arr = [0u8; U64_NUM_BYTES];
-            let bytes = &bytes[..U64_NUM_BYTES];
-            arr.copy_from_slice(bytes);
-            Ok(u64::from_le_bytes(arr))
-        },
-        _ => Err("✘ Too many bytes to convert to u64 without overflowing!".into()),
-    }
+    Ok(u64::from_le_bytes(bytes.try_into()?))
 }
 
 pub fn convert_bytes_to_u8(bytes: &[Byte]) -> Result<u8> {
-    match bytes.len() {
-        0 => Err("✘ Not enough bytes to convert to u8!".into()),
-        1 => {
-            let mut arr = [0u8; 1];
-            let bytes = &bytes[..1];
-            arr.copy_from_slice(bytes);
-            Ok(u8::from_le_bytes(arr))
-        },
-        _ => Err("✘ Too many bytes to convert to u8 without overflowing!".into()),
-    }
+    Ok(u8::from_le_bytes(bytes.try_into()?))
 }
 
 pub fn right_pad_with_zeroes(s: &str, width: usize) -> String {
