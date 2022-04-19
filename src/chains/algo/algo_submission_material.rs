51ff0892da93e12af29ad7dafde8f62d57a86274
Greg Kapka <greg@oraclize.it>
2022-04-19 15:09:08 +0100

    feat(algo): <- adds bytes serde methods to `AlgoSubmissionMaterial`

--- src/chains/algo/algo_submission_material.rs
+++ src/chains/algo/algo_submission_material.rs
@@ -4,9 +4,14 @@ use rust_algorand::{AlgorandBlock, AlgorandBlockJson, AlgorandTransactionProof, 
 use serde::{Deserialize, Serialize};
 use serde_json;
 
-use crate::{chains::algo::algo_state::AlgoState, errors::AppError, traits::DatabaseInterface, types::Result};
+use crate::{
+    chains::algo::algo_state::AlgoState,
+    errors::AppError,
+    traits::DatabaseInterface,
+    types::{Byte, Bytes, Result},
+};
 
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
+#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
 pub struct AlgoSubmissionMaterial {
     block: AlgorandBlock,
     proofs: Vec<AlgorandTransactionProof>,
@@ -21,6 +26,25 @@ impl AlgoSubmissionMaterial {
                 .iter()
                 .map(|proof_json| Ok(AlgorandTransactionProof::from_json(proof_json)?))
                 .collect::<Result<Vec<AlgorandTransactionProof>>>()?,
+        })
+    }
+
+    pub fn from_bytes(bytes: &[Byte]) -> Result<Self> {
+        Self::from_json(&serde_json::from_slice::<AlgoSubmissionMaterialJson>(bytes)?)
+    }
+
+    pub fn to_bytes(&self) -> Result<Bytes> {
+        Ok(serde_json::to_vec(&self.to_json()?)?)
+    }
+
+    pub fn to_json(&self) -> Result<AlgoSubmissionMaterialJson> {
+        Ok(AlgoSubmissionMaterialJson {
+            block: self.block.to_json()?,
+            proofs: self
+                .proofs
+                .iter()
+                .map(|proof| proof.to_json())
+                .collect::<Vec<AlgorandTransactionProofJson>>(),
         })
     }
 }
@@ -65,5 +89,23 @@ mod test {
         let submission_material_str = get_sample_block_json_str_n(0);
         let result = AlgoSubmissionMaterial::from_str(&submission_material_str);
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_serde_submission_material_to_and_from_json() {
+        let submission_material_str = get_sample_block_json_str_n(0);
+        let submission_material = AlgoSubmissionMaterial::from_str(&submission_material_str).unwrap();
+        let json = submission_material.to_json().unwrap();
+        let result = AlgoSubmissionMaterial::from_json(&json).unwrap();
+        assert_eq!(result, submission_material)
+    }
+
+    #[test]
+    fn should_serde_algo_submission_material_to_and_from_bytes() {
+        let submission_material_str = get_sample_block_json_str_n(0);
+        let submission_material = AlgoSubmissionMaterial::from_str(&submission_material_str).unwrap();
+        let bytes = submission_material.to_bytes().unwrap();
+        let result = AlgoSubmissionMaterial::from_bytes(&bytes).unwrap();
+        //assert_eq!(result, submission_material);
     }
 }
