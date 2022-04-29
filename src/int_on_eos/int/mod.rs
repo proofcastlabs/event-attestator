ed27630256b0aaa26e156e7cbe301d6fca984445
Greg Kapka <greg@oraclize.it>
2022-04-29 12:03:00 +0100

    feat(int-on-eos): <- adds tx info parser to INT side of that

--- src/int_on_eos/int/mod.rs
+++ src/int_on_eos/int/mod.rs
@@ -4,5 +4,6 @@
 //pub(crate) mod divert_to_safe_address;
 //pub(crate) mod get_output_json;
 //pub(crate) mod initialize_eth_core;
-pub(crate) mod filter_tx_infos;
 pub(crate) mod eos_tx_info;
+pub(crate) mod filter_tx_infos;
+pub(crate) mod parse_tx_info;
