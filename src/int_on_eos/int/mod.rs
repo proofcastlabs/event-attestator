dc0293fe852dd5fb8eabe1a68bc5ba3ffc4db363
Greg Kapka <greg@oraclize.it>
2022-04-29 12:44:05 +0100

    feat(int-on-eos): <- adds safe address diverter to INT side of that

--- src/int_on_eos/int/mod.rs
+++ src/int_on_eos/int/mod.rs
@@ -1,7 +1,7 @@
 //pub mod submit_eth_block;
 
 //pub(crate) mod account_for_fees;
-//pub(crate) mod divert_to_safe_address;
+pub(crate) mod divert_to_safe_address;
 pub(crate) mod eos_tx_info;
 pub(crate) mod filter_tx_infos;
 pub(crate) mod get_output_json;
