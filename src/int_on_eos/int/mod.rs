2f2aafbb72687e3039c7cf45b5d77c4e4ef5d553
Greg Kapka <greg@oraclize.it>
2022-04-29 11:19:27 +0100

    feat(int-on-eos): <- adds EOS tx info to INT side of that

---
+++ src/int_on_eos/int/mod.rs
@@ -0,0 +1,7 @@
+//pub mod submit_eth_block;
+
+//pub(crate) mod account_for_fees;
+//pub(crate) mod divert_to_safe_address;
+//pub(crate) mod get_output_json;
+//pub(crate) mod initialize_eth_core;
+pub(crate) mod eos_tx_info;
