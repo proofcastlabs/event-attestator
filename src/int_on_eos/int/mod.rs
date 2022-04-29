cb6288424f0f1da7381b0d3b1737f5514d63332b
Greg Kapka <greg@oraclize.it>
2022-04-29 12:48:00 +0100

    ref(int-on-eos): <- rename tx filterer in that for clarity

--- src/int_on_eos/int/mod.rs
+++ src/int_on_eos/int/mod.rs
@@ -1,9 +1,8 @@
 //pub mod submit_eth_block;
 
-//pub(crate) mod account_for_fees;
 pub(crate) mod divert_to_safe_address;
 pub(crate) mod eos_tx_info;
-pub(crate) mod filter_tx_infos;
+pub(crate) mod filter_out_zero_tx_infos;
 pub(crate) mod get_output_json;
 pub(crate) mod initialize_int_core;
 pub(crate) mod parse_tx_info;
