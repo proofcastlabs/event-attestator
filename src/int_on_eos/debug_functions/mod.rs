661f39e99758cedc296b10c2b378791a38120853
Greg Kapka <greg@oraclize.it>
2022-05-03 13:05:43 +0100

    feat(int-on-eos): <- rm migration tx debug fxn

--- src/int_on_eos/debug_functions/mod.rs
+++ src/int_on_eos/debug_functions/mod.rs
@@ -18,7 +18,6 @@ use crate::{
         eth::{
             eth_contracts::erc20_vault::{
                 encode_erc20_vault_add_supported_token_fx_data,
-                encode_erc20_vault_migrate_fxn_data,
                 encode_erc20_vault_remove_supported_token_fx_data,
             },
             eth_crypto::eth_transaction::EthTransaction,
@@ -128,7 +127,7 @@ pub fn debug_get_all_db_keys() -> Result<String> {
 ///     "eth_token_decimals": <num-decimals>,
 ///     "eos_token_decimals": <num-decimals>,
 /// }
-pub fn debug_add_eos_eth_token_dictionary_entry<D: DatabaseInterface>(
+pub fn debug_add_token_dictionary_entry<D: DatabaseInterface>(
     db: D,
     dictionary_entry_json_string: &str,
 ) -> Result<String> {
@@ -141,66 +140,12 @@ pub fn debug_add_eos_eth_token_dictionary_entry<D: DatabaseInterface>(
 /// This function will remove an entry pertaining to the passed in ETH address from the
 /// `EosEthTokenDictionary` held in the encrypted database, should that entry exist. If it is
 /// not extant, nothing is changed.
-pub fn debug_remove_eos_eth_token_dictionary_entry<D: DatabaseInterface>(
+pub fn debug_remove_token_dictionary_entry<D: DatabaseInterface>(
     db: D,
     eth_address_str: &str,
 ) -> Result<String> {
     check_core_is_initialized(&EthDbUtils::new(&db), &EosDbUtils::new(&db))
         .and_then(|_| remove_eos_eth_token_dictionary_entry(&db, eth_address_str))
-}
-
-/// # Debug Get ERC20_VAULT Migration Transaction
-///
-/// This function will create and sign a transaction that calls the `migrate` function on the
-/// current `pERC20-on-EOS` smart-contract, migrationg it to the ETH address provided as an
-/// argument. It then updates the smart-contract address stored in the encrypted database to that
-/// new address.
-///
-/// ### NOTE:
-/// This function will increment the core's ETH nonce, meaning the outputted reports will have a
-/// gap in their report IDs!
-///
-/// ### BEWARE:
-/// This function outputs a signed transaction which if NOT broadcast will result in the enclave no
-/// longer working.  Use with extreme caution and only if you know exactly what you are doing!
-pub fn debug_get_erc20_vault_migration_tx<D: DatabaseInterface>(
-    db: D,
-    new_vault_contract_address_str: &str,
-) -> Result<String> {
-    db.start_transaction()?;
-    info!("✔ Debug getting migration transaction...");
-    let eth_db_utils = EthDbUtils::new(&db);
-    let current_eth_account_nonce = eth_db_utils.get_eth_account_nonce_from_db()?;
-    let current_vault_contract_address = eth_db_utils.get_int_on_eos_smart_contract_address_from_db()?;
-    let new_vault_contract_address = get_eth_address_from_str(new_vault_contract_address_str)?;
-    check_debug_mode()
-        .and_then(|_| check_core_is_initialized(&eth_db_utils, &EosDbUtils::new(&db)))
-        .and_then(|_| eth_db_utils.increment_eth_account_nonce_in_db(1))
-        .and_then(|_| eth_db_utils.put_erc20_on_eos_smart_contract_address_in_db(&new_vault_contract_address))
-        .and_then(|_| encode_erc20_vault_migrate_fxn_data(new_vault_contract_address))
-        .and_then(|tx_data| {
-            let chain_id = eth_db_utils.get_eth_chain_id_from_db()?;
-            Ok(EthTransaction::new_unsigned(
-                tx_data,
-                current_eth_account_nonce,
-                0,
-                current_vault_contract_address,
-                &chain_id,
-                chain_id.get_erc20_vault_migrate_gas_limit(),
-                eth_db_utils.get_eth_gas_price_from_db()?,
-            ))
-        })
-        .and_then(|unsigned_tx| unsigned_tx.sign(&eth_db_utils.get_eth_private_key_from_db()?))
-        .map(|signed_tx| signed_tx.serialize_hex())
-        .and_then(|hex_tx| {
-            db.end_transaction()?;
-            Ok(json!({
-                "success": true,
-                "eth_signed_tx": hex_tx,
-                "migrated_to_address:": new_vault_contract_address_str.to_string(),
-            })
-            .to_string())
-        })
 }
 
 /// # Debug Get Add Supported Token Transaction
