abed275a0ab76a3643a6033de521f0ab53244ff7
Greg Kapka <greg@oraclize.it>
2022-05-09 11:38:23 +0100

    fix(int-on-eos): <- use correct v2 redeem action name in EOS reprocessor

--- src/int_on_eos/debug_functions/block_reprocessors.rs
+++ src/int_on_eos/debug_functions/block_reprocessors.rs
@@ -4,7 +4,7 @@ use crate::{
     chains::{
         eos::{
             add_schedule::maybe_add_new_eos_schedule_to_db_and_return_state,
-            eos_constants::REDEEM_ACTION_NAME,
+            eos_constants::V2_REDEEM_ACTION_NAME,
             eos_database_transactions::{
                 end_eos_db_transaction_and_return_state,
                 start_eos_db_transaction_and_return_state,
@@ -123,7 +123,7 @@ fn reprocess_eos_block<D: DatabaseInterface>(db: D, block_json: &str, maybe_nonc
         .and_then(maybe_filter_out_invalid_action_receipt_digests)
         .and_then(maybe_filter_out_proofs_with_invalid_merkle_proofs)
         .and_then(maybe_filter_out_proofs_with_wrong_action_mroot)
-        .and_then(|state| maybe_filter_proofs_for_action_name(state, REDEEM_ACTION_NAME))
+        .and_then(|state| maybe_filter_proofs_for_action_name(state, V2_REDEEM_ACTION_NAME))
         .and_then(maybe_parse_int_tx_infos_and_put_in_state)
         .and_then(|state| {
             let tx_infos = state.int_on_eos_int_tx_infos.clone();
