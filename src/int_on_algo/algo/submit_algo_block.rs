e7c3519180a6c18de123529d6a43735c9a92071e
Greg Kapka <greg@oraclize.it>
2022-06-09 16:05:48 +0100

    feat(int-on-algo): <- adds test for an algo app call internal tx peg out

--- src/int_on_algo/algo/submit_algo_block.rs
+++ src/int_on_algo/algo/submit_algo_block.rs
@@ -73,6 +73,7 @@ mod tests {
 
     use rust_algorand::{AlgorandAddress, AlgorandGenesisId};
     use serde_json::json;
+    use serial_test::serial;
 
     use super::*;
     use crate::{
@@ -94,7 +95,8 @@ mod tests {
             algo::get_algo_output::AlgoOutput,
             maybe_initialize_algo_core,
             test_utils::{
-                get_sample_contiguous_algo_submission_json_strings,
+                get_sample_contiguous_algo_submission_json_strings_for_application_call_peg_out,
+                get_sample_contiguous_algo_submission_json_strings_for_asset_transfer_peg_out,
                 get_sample_contiguous_int_submission_json_strings_for_algo_address_peg_in,
                 get_sample_evm_algo_dictionary_entry,
                 get_sample_router_address,
@@ -106,10 +108,11 @@ mod tests {
     };
 
     #[test]
-    fn should_submit_algo_block_successfully() {
+    #[serial]
+    fn should_submit_algo_block_with_asset_transfer_peg_out_successfully() {
         let db = get_test_database();
         let int_submission_material = get_sample_contiguous_int_submission_json_strings_for_algo_address_peg_in();
-        let algo_submission_material = get_sample_contiguous_algo_submission_json_strings();
+        let algo_submission_material = get_sample_contiguous_algo_submission_json_strings_for_asset_transfer_peg_out();
         let int_init_block = int_submission_material[0].clone();
         let algo_init_block = algo_submission_material[0].clone();
         let algo_peg_in_block = algo_submission_material[2].clone();
@@ -176,9 +179,10 @@ mod tests {
             .add_and_update_in_db(get_sample_evm_algo_dictionary_entry(), &db)
             .unwrap();
 
+        // NOTE: Submit the block containing the peg in, though there will be no output due to 1 confirmations.
         submit_algo_block_to_core(&db, &algo_submission_material[1]).unwrap();
 
-        // NOTE: Submit the block containing the peg in, though there will be no output due to 1 confirmations.
+        // NOTE: Submit the next block to the core, which will result in a signed transaction.
         let output = submit_algo_block_to_core(&db, &algo_peg_in_block).unwrap();
         let expected_result_json = json!({
             "algo_latest_block_number":20642398,
@@ -196,6 +200,172 @@ mod tests {
                 "destination_chain_id":"0x0069c322",
                 "native_token_address":"0x4262d1f878d191fbc66dca73bad57309916b1412",
                 "int_signed_tx":"f9034b808504a817c8008306ddd094e0806ce04978224e27c6bb10e27fd30a7785ae9d80b902e422965469000000000000000000000000ec1700a39972482d5db20e73bb3ffe6829b0c1020000000000000000000000004262d1f878d191fbc66dca73bad57309916b14120000000000000000000000000000000000000000000000000000001f21241900000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000002400300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010003c38e670000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001400069c3220000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001a0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002200000000000000000000000000000000000000000000000000000000000000003c0ffee0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003a474b54355858364e3435555633454e4d494f41564637455151594c37375034355846485949504246414a554f4e37524255435150583537325449000000000000000000000000000000000000000000000000000000000000000000000000002a307866656466653236313665623336363163623866656432373832663566306363393164353964636163000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000029a0d0ff7ad55cdfc290c578a32efa663a2824fea724d3957c0045b543a0dcbff782a022e90dc4bfaf66d8a0a7a1c0062776f6e2480e9866c36e52730907980cf51b88",
+                "int_latest_block_number":12221813,
+                "broadcast_tx_hash":null,
+                "broadcast_timestamp":null
+            }]
+        });
+        let expected_result = AlgoOutput::from_str(&expected_result_json.to_string()).unwrap();
+        let result = AlgoOutput::from_str(&output).unwrap();
+
+        // NOTE: We don't assert against the timestamp because it's not deterministic!
+        assert_eq!(
+            result.algo_latest_block_number,
+            expected_result.algo_latest_block_number
+        );
+        assert_eq!(
+            result.int_signed_transactions[0]._id,
+            expected_result.int_signed_transactions[0]._id
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].broadcast,
+            expected_result.int_signed_transactions[0].broadcast
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].int_tx_hash,
+            expected_result.int_signed_transactions[0].int_tx_hash
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].int_tx_amount,
+            expected_result.int_signed_transactions[0].int_tx_amount
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].host_token_address,
+            expected_result.int_signed_transactions[0].host_token_address
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].originating_tx_hash,
+            expected_result.int_signed_transactions[0].originating_tx_hash
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].originating_address,
+            expected_result.int_signed_transactions[0].originating_address
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].native_token_address,
+            expected_result.int_signed_transactions[0].native_token_address
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].int_signed_tx,
+            expected_result.int_signed_transactions[0].int_signed_tx
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].int_account_nonce,
+            expected_result.int_signed_transactions[0].int_account_nonce
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].int_latest_block_number,
+            expected_result.int_signed_transactions[0].int_latest_block_number
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].broadcast_tx_hash,
+            expected_result.int_signed_transactions[0].broadcast_tx_hash
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].broadcast_timestamp,
+            expected_result.int_signed_transactions[0].broadcast_timestamp
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].int_tx_recipient,
+            expected_result.int_signed_transactions[0].int_tx_recipient
+        );
+        assert_eq!(
+            result.int_signed_transactions[0].destination_chain_id,
+            expected_result.int_signed_transactions[0].destination_chain_id
+        );
+    }
+
+    #[test]
+    #[serial]
+    fn should_submit_algo_block_with_application_call_peg_out_successfully() {
+        let db = get_test_database();
+        let int_submission_material = get_sample_contiguous_int_submission_json_strings_for_algo_address_peg_in();
+        let algo_submission_material =
+            get_sample_contiguous_algo_submission_json_strings_for_application_call_peg_out();
+        let int_init_block = int_submission_material[0].clone();
+        let algo_init_block = algo_submission_material[0].clone();
+        let algo_peg_in_block = algo_submission_material[1].clone();
+        let router_address = get_sample_router_address();
+        let vault_address = get_sample_vault_address();
+        let int_confirmations = 0;
+        let algo_confirmations = 0;
+        let gas_price = 20_000_000_000;
+        let algo_fee = 1000;
+        let app_id = 1337;
+
+        // NOTE: Initialize the INT side of the core...
+        initialize_eth_core_with_vault_and_router_contracts_and_return_state(
+            &int_init_block,
+            &EthChainId::Ropsten,
+            gas_price,
+            int_confirmations,
+            EthState::init(&db),
+            &vault_address,
+            &router_address,
+            &VaultUsingCores::IntOnAlgo,
+        )
+        .unwrap();
+
+        // NOTE: Initialize the ALGO side of the core...
+        maybe_initialize_algo_core(
+            &db,
+            &algo_init_block,
+            &AlgorandGenesisId::Mainnet.to_string(),
+            algo_fee,
+            algo_confirmations,
+            app_id,
+        )
+        .unwrap();
+
+        // NOTE: Overwrite the INT address & private key since it's generated randomly above...
+        let int_address = convert_hex_to_eth_address("0x49B9d619E3402de8867A8113C7bc204653F5DB4c").unwrap();
+        let int_private_key = EthPrivateKey::from_slice(
+            &hex::decode("e87a3a4b16ffc44c78d53f633157f0c08dc085a33483c2cbae78aa5892247e4c").unwrap(),
+        )
+        .unwrap();
+        let eth_db_utils = EthDbUtils::new(&db);
+        eth_db_utils
+            .put_eth_address_in_db(&eth_db_utils.get_eth_address_key(), &int_address)
+            .unwrap();
+        eth_db_utils.put_eth_private_key_in_db(&int_private_key).unwrap();
+        assert_eq!(eth_db_utils.get_public_eth_address_from_db().unwrap(), int_address);
+        assert_eq!(eth_db_utils.get_eth_private_key_from_db().unwrap(), int_private_key);
+
+        // NOTE: Overwrite the ALGO address since it's generated randomly above...
+        let algo_db_utils = AlgoDbUtils::new(&db);
+        let algo_address =
+            AlgorandAddress::from_str("N4F4VB7GYZWL2RRTMQVMBKM5GKTKDTOHVB5PHGQYFB6XSXR3MRYIVOPTWE").unwrap();
+        db.put(
+            get_prefixed_db_key("algo_redeem_address_key").to_vec(),
+            algo_address.to_bytes(),
+            MIN_DATA_SENSITIVITY_LEVEL,
+        )
+        .unwrap();
+        assert_eq!(algo_db_utils.get_redeem_address().unwrap(), algo_address);
+
+        // NOTE Save the token dictionary into the db...
+        EvmAlgoTokenDictionary::new(vec![])
+            .add_and_update_in_db(get_sample_evm_algo_dictionary_entry(), &db)
+            .unwrap();
+
+        // NOTE: Submit the block containing the peg in, though there will be no output due to 1 confirmations.
+        let output = submit_algo_block_to_core(&db, &algo_peg_in_block).unwrap();
+        let expected_result_json = json!({
+            "algo_latest_block_number": 21515431,
+            "int_signed_transactions":[{
+                "_id":"pint-on-algo-int-0",
+                "broadcast":false,
+                "int_tx_hash":"0x63514419a5a3aa8b8373e8acb926e0f88b6aacd5496e54940a7c550d193223e5",
+                "int_tx_amount":"100000000000",
+                "int_account_nonce":0,
+                "int_tx_recipient":"0xc8D59c57B8C58Eac1622C7A639E10bF8B1E3DF9D",
+                "witnessed_timestamp":1650986069,
+                "host_token_address":"714666072",
+                "originating_tx_hash":"XVAZRFMTRRKKHMNKOXZSIVGQ74XNTAIVPKAFE3A2GF6URFKYGBMQ",
+                "originating_address":"E644GKJQW2YOJACA6DFT4OCHNQE6SJVC7K2ORLGZWFBAKRTAM44M63VHGA",
+                "destination_chain_id":"0x00f34368",
+                "native_token_address":"0x4262d1f878d191fbc66dca73bad57309916b1412",
+                "int_signed_tx":"f9032b808504a817c8008306ddd094e0806ce04978224e27c6bb10e27fd30a7785ae9d80b902c422965469000000000000000000000000ec1700a39972482d5db20e73bb3ffe6829b0c1020000000000000000000000004262d1f878d191fbc66dca73bad57309916b1412000000000000000000000000000000000000000000000000000000174876e800000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000002200300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010003c38e6700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000012000f3436800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018000000000000000000000000000000000000000000000000000000000000001e000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003a45363434474b4a515732594f4a41434136444654344f43484e514536534a5643374b324f524c475a574642414b5254414d34344d363356484741000000000000000000000000000000000000000000000000000000000000000000000000002a307863386435396335376238633538656163313632326337613633396531306266386231653364663964000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000029a004af6aa128fe9f4503d119bc6b4a8bfce4de916a617868149d94523389790352a0352695ed32b73e3a1ae3bd4d020260e35158cc5e5d92b4d735506a9f2543b412",
                 "int_latest_block_number":12221813,
                 "broadcast_tx_hash":null,
                 "broadcast_timestamp":null
