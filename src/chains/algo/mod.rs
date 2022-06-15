4886298777ac6e49911fbcf5e171debe23948a4b
Greg Kapka <greg@oraclize.it>
2022-06-15 17:13:02 +0100

    feat(algo): <- adds step to block submissions to maybe update expired participation accounts in latest block

--- src/chains/algo/mod.rs
+++ src/chains/algo/mod.rs
@@ -15,6 +15,7 @@ pub(crate) mod check_submitted_block_is_subsequent;
 pub(crate) mod core_initialization;
 pub(crate) mod get_candidate_block_hash;
 pub(crate) mod increment_eth_account_nonce;
+pub(crate) mod maybe_update_latest_block_with_expired_participants;
 pub(crate) mod remove_all_txs_from_submission_material_in_state;
 pub(crate) mod remove_irrelevant_txs_from_submission_material_in_state;
 pub(crate) mod remove_old_algo_tail_submission_material;
