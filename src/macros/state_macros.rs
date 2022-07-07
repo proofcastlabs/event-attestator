6c40f979ba9fdef1df2039384822a22b80ca48ef
Greg Kapka <greg@oraclize.it>
2022-07-07 11:10:49 +0100

    fix(macros): <- use crate reference correctly in those

--- src/macros/state_macros.rs
+++ src/macros/state_macros.rs
@@ -7,7 +7,7 @@ macro_rules! make_state_setters_and_getters {
                     pub fn [<add_ $field_name>](mut self, thing: $field_type) -> Result<Self> {
                         info!("✔ Adding `{}` to state...", $field_name);
                         match self.[< $field_name:snake:lower >] {
-                            Some(_) => Err(crate::utils::get_no_overwrite_state_err($field_name).into()),
+                            Some(_) => Err($crate::utils::get_no_overwrite_state_err($field_name).into()),
                             None => {
                                 self.[< $field_name >] = Some(thing);
                                 Ok(self)
@@ -19,7 +19,7 @@ macro_rules! make_state_setters_and_getters {
                         info!("✔ Getting `{}` from state...", $field_name);
                         match self.[< $field_name:snake:lower >] {
                             Some(ref thing) => Ok(thing),
-                            None => Err(crate::utils::get_not_in_state_err($field_name).into()),
+                            None => Err($crate::utils::get_not_in_state_err($field_name).into()),
                         }
                     }
                 )*
