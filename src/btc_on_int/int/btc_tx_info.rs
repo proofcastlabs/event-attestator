f6b6edb6467714e70dd0f20e0382ae190486b532
Greg Kapka <greg@oraclize.it>
2022-07-12 13:57:11 +0100

    feat(btc-on-int): <- impl `Display` for `BtcOnIntBtcTxInfo` in that

--- src/btc_on_int/int/btc_tx_info.rs
+++ src/btc_on_int/int/btc_tx_info.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use derive_more::{Constructor, Deref, IntoIterator};
 use ethereum_types::{Address as EthAddress, H256 as EthHash, U256};
 
@@ -13,4 +15,28 @@ pub struct BtcOnIntBtcTxInfo {
     pub amount_in_satoshis: u64,
     pub token_address: EthAddress,
     pub originating_tx_hash: EthHash,
+}
+
+impl fmt::Display for BtcOnIntBtcTxInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "BtcOnIntBtcTxInfo: {{
+                to: {},
+                from: {},
+                recipient: {},
+                amount_in_wei: {},
+                amount_in_satoshis: {},
+                token_address: {},
+                originating_tx_has: {}
+            }}",
+            self.to,
+            self.from,
+            self.recipient,
+            self.amount_in_wei,
+            self.amount_in_satoshis,
+            self.token_address,
+            self.originating_tx_hash,
+        )
+    }
 }
