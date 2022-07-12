73aa934ed2ee44b51fb68ad7b171226898ef7c86
Greg Kapka <greg@oraclize.it>
2022-07-12 14:12:57 +0100

    feat(btc-on-int): <- use method in `erc20_token` to filter list of tx infos in that

--- src/btc_on_int/int/filter_tx_info_with_no_erc20_transfer_event.rs
+++ src/btc_on_int/int/filter_tx_info_with_no_erc20_transfer_event.rs
@@ -1,50 +1,17 @@
 use crate::{
     btc_on_int::int::btc_tx_info::{BtcOnIntBtcTxInfo, BtcOnIntBtcTxInfos},
     chains::eth::{
-        eth_contracts::erc20_token::{Erc20TokenTransferEvent, Erc20TokenTransferEvents},
+        eth_contracts::erc20_token::{Erc20TokenTransferEvent, Erc20TokenTransferEvents, ToErc20TokenTransferEvent},
         eth_database_utils::EthDbUtilsExt,
         eth_state::EthState,
-        eth_submission_material::EthSubmissionMaterial,
     },
     traits::DatabaseInterface,
     types::Result,
 };
 
-impl BtcOnIntBtcTxInfo {
-    pub fn to_erc20_transfer_event(&self) -> Erc20TokenTransferEvent {
+impl ToErc20TokenTransferEvent for BtcOnIntBtcTxInfo {
+    fn to_erc20_token_transfer_event(&self) -> Erc20TokenTransferEvent {
         Erc20TokenTransferEvent::new(self.amount_in_wei, self.to, self.from, self.token_address)
-    }
-}
-
-impl BtcOnIntBtcTxInfos {
-    // FIXME Need to make this smarter so same peg out info > 1 in a block requires BOTH transfer
-    // events!
-    pub fn filter_those_with_no_corresponding_erc20_transfer_event(
-        &self,
-        submission_material: &EthSubmissionMaterial,
-    ) -> Result<Self> {
-        info!("✔ Number of `BtcOnIntBtcTxInfos` before: {}", self.len());
-        Erc20TokenTransferEvents::from_eth_submission_material(submission_material).map(|erc20_transfer_events| {
-            let filtered = self
-                .iter()
-                .filter(|tx_info| {
-                    let transfer_exists =
-                        erc20_transfer_events.erc20_transfer_exists(&tx_info.to_erc20_transfer_event());
-                    if transfer_exists {
-                        true
-                    } else {
-                        warn!(
-                            "✘ Tx info filtered out ∵ no ERC20 transfer event found for it! {:?}",
-                            tx_info
-                        );
-                        false
-                    }
-                })
-                .cloned()
-                .collect::<Vec<BtcOnIntBtcTxInfo>>();
-            info!("✔ Number of `BtcOnIntBtcTxInfos` after: {}", filtered.len());
-            Self::new(filtered)
-        })
     }
 }
 
@@ -55,11 +22,8 @@ pub fn maybe_filter_those_with_no_corresponding_erc20_transfer_event<D: Database
     state
         .eth_db_utils
         .get_eth_canon_block_from_db()
-        .and_then(|canon_block_submission_material| {
-            state
-                .btc_on_int_btc_tx_infos
-                .clone()
-                .filter_those_with_no_corresponding_erc20_transfer_event(&canon_block_submission_material)
-        })
-        .and_then(|filtered| state.replace_btc_on_int_btc_tx_infos(filtered))
+        .and_then(|canon_block| Erc20TokenTransferEvents::from_eth_submission_material(&canon_block))
+        .map(|erc20_transfers| erc20_transfers.filter_if_no_transfer_event(&state.btc_on_int_btc_tx_infos))
+        .map(BtcOnIntBtcTxInfos::new)
+        .and_then(|filtered_tx_infos| state.replace_btc_on_int_btc_tx_infos(filtered_tx_infos))
 }
