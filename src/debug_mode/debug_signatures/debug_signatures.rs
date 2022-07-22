0994a819f82da2e0587babdb6cf76a1e80302279
Greg Kapka <greg@oraclize.it>
2022-07-22 12:31:53 +0100

    feat(debug-signatures): <- adds method to sign and recover those when signed with eth prefix

--- src/debug_mode/debug_signatures/debug_signatures.rs
+++ src/debug_mode/debug_signatures/debug_signatures.rs
@@ -7,11 +7,34 @@ use crate::{
         eth_traits::EthSigningCapabilities,
         eth_utils::convert_eth_address_to_string,
     },
+    crypto_utils::keccak_hash_bytes,
     debug_mode::debug_signatures::debug_signatory::DebugSignatory,
-    types::{Byte, Result},
+    types::{Byte, Bytes, Result},
 };
 
 impl DebugSignatory {
+    fn get_eth_prefixed_message_bytes(message: &str) -> Bytes {
+        let eth_prefixed_message_bytes =
+            keccak_hash_bytes(format!("\x19Ethereum Signed Message:\n{}{}", message.len(), message).as_bytes())[..]
+                .to_vec();
+        debug!(
+            "ETH prefixed message bytes: {}",
+            hex::encode(&eth_prefixed_message_bytes)
+        );
+        eth_prefixed_message_bytes
+    }
+
+    fn hash_with_eth_prefix(&self, debug_command_hash: &H256) -> Result<H256> {
+        self.hash_to_hex(debug_command_hash)
+            .map(|message| Self::get_eth_prefixed_message_bytes(&message))
+            .map(|bytes| H256::from_slice(&bytes))
+    }
+
+    pub fn sign_with_eth_prefix(&self, pk: &EthPrivateKey, debug_command_hash: &H256) -> Result<EthSignature> {
+        self.hash_with_eth_prefix(debug_command_hash)
+            .and_then(|hash| pk.sign_hash_and_set_eth_recovery_param(hash))
+    }
+
     pub fn sign(&self, pk: &EthPrivateKey, debug_command_hash: &H256) -> Result<EthSignature> {
         self.hash(debug_command_hash)
             .and_then(|hash| pk.sign_hash_and_set_eth_recovery_param(hash))
@@ -19,6 +42,14 @@ impl DebugSignatory {
 
     pub fn recover_signer_address(&self, signature: &EthSignature, debug_command_hash: &H256) -> Result<EthAddress> {
         signature.recover_signer_address(&self.hash(debug_command_hash)?)
+    }
+
+    pub fn recover_signer_address_using_eth_prefix(
+        &self,
+        signature: &EthSignature,
+        debug_command_hash: &H256,
+    ) -> Result<EthAddress> {
+        signature.recover_signer_address(&self.hash_with_eth_prefix(debug_command_hash)?)
     }
 
     pub fn validate(
@@ -29,7 +60,7 @@ impl DebugSignatory {
     ) -> Result<()> {
         let recovered_addresses = vec![
             self.recover_signer_address(signature, debug_command_hash)?,
-            // TODO recover it WITH the prefix too!
+            self.recover_signer_address_using_eth_prefix(signature, debug_command_hash)?,
         ];
         if recovered_addresses.contains(eth_address) {
             Ok(())
@@ -52,6 +83,7 @@ mod tests {
             get_sample_debug_signatory,
             get_sample_private_key,
         },
+        errors::AppError,
     };
 
     #[test]
@@ -73,7 +105,7 @@ mod tests {
         let result = signatory
             .recover_signer_address(&signature, &debug_command_hash)
             .unwrap();
-        let expected_result = convert_hex_to_eth_address("0xfEDFe2616EB3661CB8FEd2782F5F0cC91D59DCaC").unwrap();
+        let expected_result = pk.to_public_key().to_address();
         assert_eq!(result, expected_result);
     }
 
@@ -83,8 +115,58 @@ mod tests {
         let signatory = get_sample_debug_signatory();
         let debug_command_hash = get_sample_debug_command_hash();
         let signature = signatory.sign(&pk, &debug_command_hash).unwrap();
-        let eth_address = convert_hex_to_eth_address("0xfEDFe2616EB3661CB8FEd2782F5F0cC91D59DCaC").unwrap();
+        let eth_address = pk.to_public_key().to_address();
         let result = signatory.validate(&signature, &debug_command_hash, &eth_address);
         assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_sign_with_eth_prefix() {
+        let debug_signatory = get_sample_debug_signatory();
+        let debug_command_hash = get_sample_debug_command_hash();
+        let pk = get_sample_private_key();
+        let result = debug_signatory.sign_with_eth_prefix(&pk, &debug_command_hash).unwrap();
+        let expected_result = EthSignature::from_hex("0xda1a3b8f1bb8c0964b15785b5408ca3dfe35ed512d860d03bc543656e0c8f2a72c550b23a15b4c6624b3625217380ce1849e85710278ddd4aaee5d8b4f26d1521c").unwrap();
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_recover_address_from_eth_prefixed_signature() {
+        let pk = get_sample_private_key();
+        let signatory = get_sample_debug_signatory();
+        let debug_command_hash = get_sample_debug_command_hash();
+        let signature = signatory.sign_with_eth_prefix(&pk, &debug_command_hash).unwrap();
+        let result = signatory
+            .recover_signer_address_using_eth_prefix(&signature, &debug_command_hash)
+            .unwrap();
+        let expected_result = pk.to_public_key().to_address();
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_validate_with_eth_prefix_signature() {
+        let debug_signatory = get_sample_debug_signatory();
+        let debug_command_hash = get_sample_debug_command_hash();
+        let pk = get_sample_private_key();
+        let eth_prefixed_signature = debug_signatory.sign_with_eth_prefix(&pk, &debug_command_hash).unwrap();
+        let eth_address = pk.to_public_key().to_address();
+        let result = debug_signatory.validate(&eth_prefixed_signature, &debug_command_hash, &eth_address);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn should_fail_to_validate_signature_using_wrong_eth_address() {
+        let wrong_eth_address = convert_hex_to_eth_address("0xea674fdde714fd979de3edf0f56aa9716b898ec8").unwrap();
+        let pk = get_sample_private_key();
+        assert_ne!(pk.to_public_key().to_address(), wrong_eth_address);
+        let debug_signatory = get_sample_debug_signatory();
+        let debug_command_hash = get_sample_debug_command_hash();
+        let signature = debug_signatory.sign(&pk, &debug_command_hash).unwrap();
+        let expected_error = "Could not validate debug signature!".to_string();
+        match debug_signatory.validate(&signature, &debug_command_hash, &wrong_eth_address) {
+            Ok(_) => panic!("Should not have succeeded!"),
+            Err(AppError::Custom(error)) => assert_eq!(error, expected_error),
+            Err(_) => panic!("Wrong error received!"),
+        }
     }
 }
