c0117db556c57f73111cc3d415f71662d2cf8b70
Greg Kapka <greg@oraclize.it>
2022-07-27 11:04:21 +0100

    feat(core-types) <- adds INT cores to that enum

--- src/core_type.rs
+++ src/core_type.rs
@@ -6,9 +6,14 @@ use strum_macros::EnumIter;
 #[derive(Clone, Copy, EnumIter)]
 pub enum CoreType {
     BtcOnEth,
+    BtcOnInt,
+    IntOnEos,
+    IntOnEvm,
     BtcOnEos,
     EosOnEth,
+    IntOnAlgo,
     Erc20OnEos,
+    Erc20OnInt,
     Erc20OnEvm,
 }
 
@@ -24,8 +29,13 @@ impl fmt::Display for CoreType {
             Self::BtcOnEth => "BTC_ON_ETH",
             Self::BtcOnEos => "BTC_ON_EOS",
             Self::EosOnEth => "EOS_ON_ETH",
+            Self::BtcOnInt => "BTC_ON_INT",
+            Self::IntOnEos => "INT_ON_EOS",
+            Self::IntOnEvm => "INT_ON_EVM",
+            Self::IntOnAlgo => "INT_ON_ALGO",
             Self::Erc20OnEos => "ERC20_ON_EOS",
             Self::Erc20OnEvm => "ERC20_ON_EVM",
+            Self::Erc20OnInt => "ERC20_ON_INT",
         };
         write!(f, "{}", s)
     }
@@ -39,7 +49,18 @@ mod tests {
 
     #[test]
     fn should_get_core_type_as_db_key_prefix() {
-        let expected_results = vec!["btc-on-eth", "btc-on-eos", "eos-on-eth", "erc20-on-eos", "erc20-on-evm"];
+        let expected_results = vec![
+            "btc-on-eth",
+            "btc-on-int",
+            "int-on-eos",
+            "int-on-evm",
+            "btc-on-eos",
+            "eos-on-eth",
+            "int-on-algo",
+            "erc20-on-eos",
+            "erc20-on-int",
+            "erc20-on-evm",
+        ];
         CoreType::iter()
             .zip(expected_results.iter())
             .for_each(|(core_type, expected_result)| assert_eq!(&core_type.as_db_key_prefix(), *expected_result))
