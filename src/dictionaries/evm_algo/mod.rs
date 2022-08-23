2054b36312a7e51d3fa5c2b1503b82e09afe68c6
Greg Kapka <greg@oraclize.it>
2022-08-23 15:31:44 +0100

    ref(dictionary): <- fix wrong info trace in that

--- src/dictionaries/evm_algo/mod.rs
+++ src/dictionaries/evm_algo/mod.rs
@@ -21,7 +21,7 @@ pub fn get_evm_algo_token_dictionary_and_add_to_eth_state<D: DatabaseInterface>(
 pub fn get_evm_algo_token_dictionary_and_add_to_algo_state<D: DatabaseInterface>(
     state: AlgoState<D>,
 ) -> Result<AlgoState<D>> {
-    info!("✔ Getting `EvmAlgoTokenDictionary` and adding to ETH state...");
+    info!("✔ Getting `EvmAlgoTokenDictionary` and adding to ALGO state...");
     EvmAlgoTokenDictionary::get_from_db(state.algo_db_utils.get_db())
         .and_then(|dictionary| state.add_evm_algo_dictionary(dictionary))
 }
