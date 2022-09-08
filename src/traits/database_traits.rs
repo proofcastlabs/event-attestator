c4497e9acca386ac4ad1aabaa4c76d6660afc91c
Greg Kapka <greg@oraclize.it>
2022-09-08 10:44:29 +0100

    fix(db-trait): <- use correct type in that

--- src/traits/database_traits.rs
+++ src/traits/database_traits.rs
@@ -1,13 +1,9 @@
-use crate::types::{Bytes, Result};
+use crate::types::{Bytes, DataSensitivity, Result};
 
 pub trait DatabaseInterface {
     fn end_transaction(&self) -> Result<()>;
-
     fn start_transaction(&self) -> Result<()>;
-
     fn delete(&self, key: Bytes) -> Result<()>;
-
-    fn get(&self, key: Bytes, data_sensitivity: Option<u8>) -> Result<Bytes>;
-
-    fn put(&self, key: Bytes, value: Bytes, data_sensitivity: Option<u8>) -> Result<()>;
+    fn get(&self, key: Bytes, data_sensitivity: DataSensitivity) -> Result<Bytes>;
+    fn put(&self, key: Bytes, value: Bytes, data_sensitivity: DataSensitivity) -> Result<()>;
 }
