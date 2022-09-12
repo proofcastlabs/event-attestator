85fa63fd4231c758911f3938df48464a36528b42
Greg Kapka <greg@oraclize.it>
2022-09-12 11:37:07 +0100

    fix(eip712-sigs): <- fix wrongly named file for that mod

--- src/debug_functions/debug_signers/mod.rs
+++ src/debug_functions/debug_signers/mod.rs
@@ -2,7 +2,7 @@ pub(crate) mod debug_functions;
 pub(crate) mod debug_signatories;
 pub(crate) mod debug_signatory;
 pub(crate) mod debug_signatures;
-pub(crate) mod eip_172_signature_hash_generator;
+pub(crate) mod eip_712_signature_hash_generator;
 pub(crate) mod get_debug_signature_info;
 #[cfg(test)]
 pub(crate) mod test_utils;
