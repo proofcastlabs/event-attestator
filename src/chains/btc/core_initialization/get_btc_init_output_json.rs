c98245aa4d417204a2b161951450c17f364cdbe6
Greg Kapka <greg@oraclize.it>
2022-11-30 17:19:07 +0000

    feat(btc): <- adds warning to init output of that if zero confs set

--- src/chains/btc/core_initialization/get_btc_init_output_json.rs
+++ src/chains/btc/core_initialization/get_btc_init_output_json.rs
@@ -1,24 +1,23 @@
-use derive_more::Constructor;
-use serde::{Deserialize, Serialize};
+use serde_json::json;
 
-use crate::{chains::btc::btc_state::BtcState, traits::DatabaseInterface, types::Result};
-
-#[derive(Clone, Debug, Serialize, Deserialize, Constructor)]
-pub struct BtcInitializationOutput {
-    pub btc_address: String,
-    pub btc_latest_block_num: u64,
-}
-
-fn json_stringify(output: BtcInitializationOutput) -> Result<String> {
-    match serde_json::to_string(&output) {
-        Ok(res) => Ok(res),
-        Err(err) => Err(err.into()),
-    }
-}
+use crate::{
+    chains::btc::btc_state::BtcState,
+    constants::ZERO_CONFS_WARNING,
+    traits::DatabaseInterface,
+    types::Result,
+};
 
 pub fn get_btc_init_output_json<D: DatabaseInterface>(state: BtcState<D>) -> Result<String> {
-    json_stringify(BtcInitializationOutput::new(
-        state.btc_db_utils.get_btc_address_from_db()?,
-        state.btc_db_utils.get_latest_btc_block_number()?,
-    ))
+    let number_of_confirmations = state.btc_db_utils.get_btc_canon_to_tip_length_from_db()?;
+    Ok(json!({
+        "btc_address": state.btc_db_utils.get_btc_address_from_db()?,
+        "btc_latest_block_num": state.btc_db_utils.get_latest_btc_block_number()?,
+        "number_of_confirmations":
+        if number_of_confirmations == 0 {
+            ZERO_CONFS_WARNING.to_string()
+        } else {
+            number_of_confirmations.to_string()
+        },
+    })
+    .to_string())
 }
--- src/constants.rs
+++ src/constants.rs
@@ -8,6 +8,7 @@ pub const DEBUG_OUTPUT_MARKER: &str = "DEBUG_OUTPUT_MARKER";
 pub const MAX_DATA_SENSITIVITY_LEVEL: Option<u8> = Some(255);
 pub const CORE_IS_VALIDATING: bool = !cfg!(feature = "non-validating");
 pub const CORE_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
+pub const ZERO_CONFS_WARNING: &str = "WARNING: NUMBER OF CONFIRMATIONS IS SET TO ZERO!";
 
 lazy_static! {
     pub static ref THIRTY_TWO_ZERO_BYTES: Vec<u8> = vec![0; 32];
