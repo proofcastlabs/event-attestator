f93191dadab886138413621dcfd715a8915e1504
Greg Kapka <greg@oraclize.it>
2022-11-30 17:23:30 +0000

    feat(eth): <- adds warning to init output of that if zero confs set

--- src/chains/eth/core_initialization/get_eth_core_init_output_json.rs
+++ src/chains/eth/core_initialization/get_eth_core_init_output_json.rs
@@ -4,6 +4,7 @@ use serde_json::to_string;
 
 use crate::{
     chains::eth::{eth_database_utils::EthDbUtilsExt, eth_state::EthState},
+    constants::ZERO_CONFS_WARNING,
     traits::DatabaseInterface,
     types::Result,
 };
@@ -12,6 +13,7 @@ use crate::{
 pub struct EthInitializationOutput {
     pub eth_address: String,
     pub eth_latest_block_num: usize,
+    pub number_of_confirmations: String,
     pub eth_ptoken_contract_tx: Option<String>,
     pub smart_contract_address: Option<String>,
 }
@@ -22,6 +24,7 @@ impl EthInitializationOutput {
         contract_address: Option<&EthAddress>,
         contract_tx: Option<&str>,
     ) -> Result<Self> {
+        let number_of_confirmations = db_utils.get_eth_canon_to_tip_length_from_db()?;
         Ok(Self {
             eth_address: format!(
                 "0x{}",
@@ -30,6 +33,11 @@ impl EthInitializationOutput {
             eth_latest_block_num: db_utils.get_latest_eth_block_number()?,
             eth_ptoken_contract_tx: contract_tx.map(|tx| tx.to_string()),
             smart_contract_address: contract_address.map(|address| format!("0x{}", hex::encode(address))),
+            number_of_confirmations: if number_of_confirmations == 0 {
+                ZERO_CONFS_WARNING.to_string()
+            } else {
+                number_of_confirmations.to_string()
+            },
         })
     }
 
