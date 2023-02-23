9095589919a28665ac7ce8eb7b31e2bbf8ca422f
gskapka <greg@oraclize.it>
2023-02-23 17:01:58 +0000

    feat(sentinel): <- adds endpoint check when rpc client is gotten

--- v3_bridges/sentinel/src/lib/get_rpc_client.rs
+++ v3_bridges/sentinel/src/lib/get_rpc_client.rs
@@ -1,7 +1,11 @@
 use anyhow::Result;
 use jsonrpsee::ws_client::{WsClient, WsClientBuilder};
 
+use crate::check_endpoint;
+
 pub async fn get_rpc_client(url: &str) -> Result<WsClient> {
     info!("[+] Getting RPC client...");
-    Ok(WsClientBuilder::default().build(&url).await?)
+    let ws_client = WsClientBuilder::default().build(&url).await?;
+    check_endpoint(&ws_client).await?;
+    Ok(ws_client)
 }
