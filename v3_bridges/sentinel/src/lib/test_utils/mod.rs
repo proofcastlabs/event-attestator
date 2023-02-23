d1189ffaea93373d96782d332d8051a2ed71374f
gskapka <greg@oraclize.it>
2023-02-23 13:16:30 +0000

    fix(tests): <- update env var info in that

--- v3_bridges/sentinel/src/lib/test_utils/mod.rs
+++ v3_bridges/sentinel/src/lib/test_utils/mod.rs
@@ -8,11 +8,11 @@ use jsonrpsee::ws_client::WsClient;
 
 use crate::get_rpc_client;
 
-const TEST_WS_ENDPOINT_ENV_VAR: &str = "TEST_WS_ENDPOINT";
+const TEST_ENDPOINT_ENV_VAR: &str = "TEST_ENDPOINT";
 
 pub async fn get_test_ws_client() -> Result<WsClient> {
     dotenv().ok();
-    let url = env::var(TEST_WS_ENDPOINT_ENV_VAR)
-        .map_err(|_| anyhow!("Please set env var '{TEST_WS_ENDPOINT_ENV_VAR}' to a working endpoint!"))?;
+    let url = env::var(TEST_ENDPOINT_ENV_VAR)
+        .map_err(|_| anyhow!("Please set env var '{TEST_ENDPOINT_ENV_VAR}' to a working endpoint!"))?;
     get_rpc_client(&url).await
 }
