b39c96013e0259844d27383f8b728d778574953e
gskapka <greg@oraclize.it>
2023-02-23 13:04:16 +0000

    feat(sentinel): <- adds test utils & ws client getter to that

--- v3_bridges/sentinel/src/lib/lib.rs
+++ v3_bridges/sentinel/src/lib/lib.rs
@@ -1,11 +1,16 @@
-pub mod constants;
-pub mod get_block;
-pub mod get_config;
-pub mod get_receipts;
-pub mod get_rpc_client;
-pub mod get_sub_mat;
+mod constants;
+mod get_block;
+mod get_config;
+mod get_receipts;
+mod get_rpc_client;
+mod get_sub_mat;
+mod test_utils;
 
-pub use self::{get_rpc_client::get_rpc_client, get_sub_mat::get_sub_mat};
+pub use self::{
+    get_config::{Config, Endpoints},
+    get_rpc_client::get_rpc_client,
+    get_sub_mat::get_sub_mat,
+};
 
 #[macro_use]
 extern crate anyhow;
---
+++ v3_bridges/sentinel/src/lib/test_utils/mod.rs
@@ -0,0 +1,18 @@
+#![cfg(test)]
+
+use std::env;
+
+use anyhow::Result;
+use dotenv::dotenv;
+use jsonrpsee::ws_client::WsClient;
+
+use crate::get_rpc_client;
+
+const TEST_WS_ENDPOINT_ENV_VAR: &str = "TEST_WS_ENDPOINT";
+
+pub async fn get_test_ws_client() -> Result<WsClient> {
+    dotenv().ok();
+    let url = env::var(TEST_WS_ENDPOINT_ENV_VAR)
+        .map_err(|_| anyhow!("Please set env var '{TEST_WS_ENDPOINT_ENV_VAR}' to a working endpoint!"))?;
+    get_rpc_client(&url).await
+}
