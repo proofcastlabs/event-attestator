fc4e0523c7a77c0d73f7c741632614410a33d697
gskapka <greg@oraclize.it>
2023-02-24 17:12:16 +0000

    feat(sentinel): <- rm redundant validation from sub mat getter

--- v3_bridges/sentinel/src/cli/get_sub_mat.rs
+++ v3_bridges/sentinel/src/cli/get_sub_mat.rs
@@ -30,19 +30,6 @@ async fn get_sub_mat_cli(endpoints: &Endpoints, args: &SubMatGetterArgs, is_nati
     let sub_mat_type = if is_native { "native" } else { "host" };
     info!("[+] Getting {sub_mat_type} submission material...");
     let sub_mat = get_sub_mat(&ws_client, args.block_num).await?;
-
-    // FIXME TODO get from config
-    let block_is_valid = sub_mat.block_is_valid(&common_chain_ids::EthChainId::Mainnet)?;
-
-    let receipts_are_valid = sub_mat.receipts_are_valid()?;
-    if !block_is_valid {
-        return Err(anyhow!("Block header is not valid!"));
-    }
-    let merkle_root = sub_mat.receipts.get_merkle_root().unwrap();
-    if !receipts_are_valid {
-        println!("Invalid receipts root: {}", merkle_root);
-        return Err(anyhow!("Receipts are not valid!"));
-    };
     let block_num = sub_mat.get_block_number()?;
     let s = serde_json::to_string(&sub_mat)?;
     let path = args.path.clone().unwrap_or_else(|| ".".into());
