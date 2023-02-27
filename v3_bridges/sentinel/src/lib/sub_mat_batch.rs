06e6174ba69402556ed5e365c69ba99f7acc7987
gskapka <greg@oraclize.it>
2023-02-27 14:40:15 +0000

    feat(batching): <- use mutable references for efficient handling in that

--- v3_bridges/sentinel/src/lib/sub_mat_batch.rs
+++ v3_bridges/sentinel/src/lib/sub_mat_batch.rs
@@ -18,39 +18,35 @@ impl SubMatBatch {
         }
     }
 
-    pub fn enable_batching(mut self) -> Self {
+    pub fn enable_batching(&mut self) {
         self.batching_is_disabled = false;
-        self
     }
 
-    pub fn disable_batching(mut self) -> Self {
+    pub fn disable_batching(&mut self) {
         self.batching_is_disabled = true;
-        self
     }
 
     pub fn batching_is_enabled(&self) -> bool {
         !self.batching_is_disabled
     }
 
-    pub fn set_time_of_last_submission(mut self) -> Self {
+    pub fn set_time_of_last_submission(&mut self) {
         self.last_submitted = SystemTime::now();
-        self
     }
 
     pub fn get_time_of_last_submission(&self) -> SystemTime {
         self.last_submitted
     }
 
-    pub fn push(mut self, sub_mat: EthSubmissionMaterial) -> Self {
+    pub fn push(&mut self, sub_mat: EthSubmissionMaterial) {
         self.batch.push(sub_mat);
-        self
     }
 
     pub fn is_empty(&self) -> bool {
         self.batch.is_empty()
     }
 
-    pub fn drain(mut self) -> Self {
+    pub fn drain(&mut self) {
         self.batch = vec![];
         self.set_time_of_last_submission()
     }
@@ -62,45 +58,47 @@ mod tests {
 
     #[test]
     fn should_enable_batching() {
-        let batch = SubMatBatch::new();
-        let updated_batch = batch.disable_batching();
-        assert!(!updated_batch.batching_is_enabled());
-        let result = updated_batch.enable_batching().batching_is_enabled();
+        let mut batch = SubMatBatch::new();
+        batch.disable_batching();
+        assert!(!batch.batching_is_enabled());
+        batch.enable_batching();
+        let result = batch.batching_is_enabled();
         assert!(result);
     }
 
     #[test]
     fn should_disable_batching() {
-        let batch = SubMatBatch::new();
-        let result = batch.disable_batching().batching_is_enabled();
+        let mut batch = SubMatBatch::new();
+        batch.disable_batching();
+        let result = batch.batching_is_enabled();
         assert!(!result);
     }
 
     #[test]
     fn should_set_time_of_last_submission() {
-        let batch = SubMatBatch::new();
+        let mut batch = SubMatBatch::new();
         let timestamp_before = batch.get_time_of_last_submission();
-        let updated_batch = batch.set_time_of_last_submission();
-        let result = updated_batch.get_time_of_last_submission();
+        batch.set_time_of_last_submission();
+        let result = batch.get_time_of_last_submission();
         assert!(result > timestamp_before);
     }
 
     #[test]
     fn should_push_to_batch() {
-        let batch = SubMatBatch::new();
+        let mut batch = SubMatBatch::new();
         assert!(batch.is_empty());
         let sub_mat = EthSubmissionMaterial::default();
-        let updated_batch = batch.push(sub_mat);
-        assert!(!updated_batch.is_empty());
+        batch.push(sub_mat);
+        assert!(!batch.is_empty());
     }
 
     #[test]
     fn should_drain_batch() {
-        let batch = SubMatBatch::new();
+        let mut batch = SubMatBatch::new();
         let sub_mat = EthSubmissionMaterial::default();
-        let updated_batch = batch.push(sub_mat);
-        assert!(!updated_batch.is_empty());
-        let final_batch = updated_batch.drain();
-        assert!(final_batch.is_empty());
+        batch.push(sub_mat);
+        assert!(!batch.is_empty());
+        batch.drain();
+        assert!(batch.is_empty());
     }
 }
