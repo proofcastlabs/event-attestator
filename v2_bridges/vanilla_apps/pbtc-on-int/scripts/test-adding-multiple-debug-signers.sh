2ac467344e4c836bcc6564a05856443c7dfed667
gskapka <greg@oraclize.it>
2023-03-01 16:20:53 +0000

    feat(v2): <- adds pbtc-on-int bin crate to that

---
+++ v2_bridges/vanilla_apps/pbtc-on-int/scripts/test-adding-multiple-debug-signers.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+cd "$(dirname -- $0)"
+
+. ./get-binary-name.sh
+. ../../scripts/get-sample-debug-signers.sh
+
+BINARY_NAME=$(getBinaryName)
+BINARY_PATH="../../../../target/release/$BINARY_NAME"
+
+echo ✔ Testing adding multiple debug signatories to \'$BINARY_NAME\' core...
+
+../../scripts/clean-up.sh $BINARY_NAME
+
+get_first_debug_signer_address() {
+	$BINARY_PATH getEnclaveState | jq '.info.debug_signatories[0].ethAddress'
+}
+
+get_second_debug_signer_address() {
+	$BINARY_PATH getEnclaveState | jq '.info.debug_signatories[1].ethAddress'
+}
+
+./initialize-int.sh
+./initialize-btc.sh
+
+if [[ $(get_first_debug_signer_address) != null ]];then
+	../../scripts/clean-up.sh $BINARY_NAME
+	echo ✘ Unexpected debug signatories before the test was started!
+	exit 1
+fi
+
+# NOTE: This valid signature only adds addresses with no private keys as signers and is therefore useless.
+$BINARY_PATH debugAddDebugSigners $(getSampleDebugSigners) --sig 0xd6caade2ff2bb2c47428d2854c7a4040a9ef7a1d6956f448ca637e37d06b751518c7d5f84e24a27dd7f26dd7efbb43be8b4c199fcd235bdc9ea65b2dc4e9274d1b
+
+if [[
+	$(get_first_debug_signer_address) == \"0x0000000000000000000000000000000000000001\"
+	&&
+	$(get_second_debug_signer_address) == \"0x0000000000000000000000000000000000000002\"
+   ]]; then
+	../../scripts/clean-up.sh $BINARY_NAME
+	echo ✔ Adding multiple debug signatories to \'$BINARY_NAME\' core test passed!
+else
+	../../scripts/clean-up.sh $BINARY_NAME
+	echo ✘ Add debug signatory test to \'$BINARY_NAME\' FAILED!
+	exit 1
+fi
---
+++ v2_bridges/vanilla_apps/pbtc-on-int/scripts/test-host-block-submission.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+cd "$(dirname -- $0)"
+
+. ./get-binary-name.sh
+
+BINARY_NAME=$(getBinaryName)
+BINARY_PATH="../../../../target/release/$BINARY_NAME"
+
+echo ✔ Testing HOST block submission to \'$BINARY_NAME\' core...
+
+../../scripts/clean-up.sh $BINARY_NAME
+
+getCoreLatestBlockNumber() {
+	$BINARY_PATH getEnclaveState | jq .eth.eth_latest_block_number
+}
+
+getInitBlockNumber() {
+	cat ./int-init-block.json | jq .block.number
+}
+
+getExpectedBlockNumber() {
+	cat ./int-subsequent-block-3.json | jq .block.number
+}
+
+./initialize-int.sh
+./initialize-btc.sh
+
+if [[ $(getCoreLatestBlockNumber) == null ]]; then
+	../../scripts/clean-up.sh $BINARY_NAME
+	echo ✘ Something went wrong with core initalization!
+	exit 1
+fi
+
+# Let's submit our sample blocks...
+$BINARY_PATH submitIntBlock --file=./int-subsequent-block-1.json
+$BINARY_PATH submitIntBlock --file=./int-subsequent-block-2.json
+$BINARY_PATH submitIntBlock --file=./int-subsequent-block-3.json
+
+[[ $(getCoreLatestBlockNumber) == $(getExpectedBlockNumber) ]] && result=true || result=false
+
+../../scripts/clean-up.sh $BINARY_NAME
+
+if [[ $result == true ]]; then
+	echo ✔ HOST block submission test to \'$BINARY_NAME\' core test PASSED!
+else
+	echo ✘ HOST block submission test to \'$BINARY_NAME\' core test FAILED!
+	exit 1
+fi
