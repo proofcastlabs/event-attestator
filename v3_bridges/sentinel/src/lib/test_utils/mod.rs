5f7c34059d51f2f68c0ff9e6e97f59e50217cb0b
gskapka <greg@oraclize.it>
2023-03-07 14:12:00 +0000

    feat(sentinel): <- adds sample batch to tests

--- v3_bridges/sentinel/src/lib/test_utils/mod.rs
+++ v3_bridges/sentinel/src/lib/test_utils/mod.rs
@@ -1,12 +1,12 @@
 #![cfg(test)]
-
-use std::env;
+use std::{env, fs::read_to_string, str::FromStr};
 
 use anyhow::Result;
+use common_eth::EthSubmissionMaterial;
 use dotenv::dotenv;
 use jsonrpsee::ws_client::WsClient;
 
-use crate::{check_endpoint, get_rpc_client};
+use crate::{check_endpoint, get_rpc_client, SubMatBatch};
 
 const ENV_VAR: &str = "TEST_ENDPOINT";
 
@@ -19,11 +19,45 @@ pub async fn get_test_ws_client() -> Result<WsClient> {
     Ok(ws_client)
 }
 
+pub fn get_sample_sub_mat_n(n: usize) -> EthSubmissionMaterial {
+    let suffix = match n {
+        1 => "host-sub-mat-num-16776500.json",
+        2 => "host-sub-mat-num-16776501.json",
+        3 => "host-sub-mat-num-16776502.json",
+        4 => "host-sub-mat-num-16776503.json",
+        5 => "host-sub-mat-num-16776504.json",
+        6 => "host-sub-mat-num-16776505.json",
+        7 => "host-sub-mat-num-16776506.json",
+        8 => "host-sub-mat-num-16776507.json",
+        _ => "host-sub-mat-num-16776508.json",
+    };
+    let prefix = "src/lib/test_utils/";
+    let path = format!("{prefix}{suffix}");
+    EthSubmissionMaterial::from_str(&read_to_string(path).unwrap()).unwrap()
+}
+
+pub fn get_sample_batch() -> SubMatBatch {
+    let mut batch = SubMatBatch::default();
+    (1..10).for_each(|i| batch.push(get_sample_sub_mat_n(i)));
+    batch
+}
+
 mod tests {
     use super::*;
 
     #[tokio::test]
     async fn should_get_test_ws_client() {
         get_test_ws_client().await.unwrap();
+    }
+
+    #[test]
+    fn should_get_sample_sub_mat_n() {
+        let n = 1;
+        get_sample_sub_mat_n(n);
+    }
+
+    #[test]
+    fn should_get_sample_batch() {
+        get_sample_batch();
     }
 }
