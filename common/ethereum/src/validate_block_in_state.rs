258c5e6aa71880109db179ed4e06b09e7b884b7a
gskapka <greg@oraclize.it>
2023-03-09 19:05:54 +0000

    feat(ethereum): <- adds block validator that's generic of state

--- common/ethereum/src/validate_block_in_state.rs
+++ common/ethereum/src/validate_block_in_state.rs
@@ -1,34 +1,34 @@
 use common::{constants::CORE_IS_VALIDATING, traits::DatabaseInterface, types::Result};
 use common_chain_ids::EthChainId;
 
-use crate::{EthDbUtilsExt, EthState};
+use crate::{EthDbUtilsExt, EthState, EthStateCompatible};
 
-fn validate_block_in_state<D: DatabaseInterface>(state: EthState<D>, is_for_eth: bool) -> Result<EthState<D>> {
+fn validate_block_in_state<D: DatabaseInterface>(state: &impl EthStateCompatible<D>, is_for_eth: bool) -> Result<()> {
     let symbol = if is_for_eth { "ETH" } else { "EVM" };
     if !CORE_IS_VALIDATING {
         info!("✔ Skipping {} block header validaton!", symbol);
-        Ok(state)
+        Ok(())
     } else {
         let chain_id = if is_for_eth {
-            state.eth_db_utils.get_eth_chain_id_from_db()?
+            state.get_eth_db_utils().get_eth_chain_id_from_db()?
         } else {
-            state.evm_db_utils.get_eth_chain_id_from_db()?
+            state.get_evm_db_utils().get_eth_chain_id_from_db()?
         };
         info!("✔ Validating block in {} state using chain ID: {}", symbol, chain_id);
         if chain_id == EthChainId::Rinkeby {
             // NOTE: We cannot validate Rinkeby blocks. However it's been deprecated now so
             // this no longer matters.
             info!("✔ Skipping RINKEBY {} block header validaton!", symbol);
-            Ok(state)
+            Ok(())
         } else {
             info!("✔ Validating {} block header...", symbol);
             state
-                .get_eth_submission_material()?
+                .get_sub_mat()?
                 .get_block()?
                 .is_valid(&chain_id)
                 .and_then(|is_valid| {
                     if is_valid {
-                        Ok(state)
+                        Ok(())
                     } else {
                         Err(format!("✘ Not accepting {} block - header hash not valid!", symbol).into())
                     }
@@ -39,10 +39,10 @@ fn validate_block_in_state<D: DatabaseInterface>(state: EthState<D>, is_for_eth:
 
 pub fn validate_evm_block_in_state<D: DatabaseInterface>(state: EthState<D>) -> Result<EthState<D>> {
     info!("✔ Validating EVM block in state...");
-    validate_block_in_state(state, false)
+    validate_block_in_state(&state, false).and(Ok(state))
 }
 
 pub fn validate_eth_block_in_state<D: DatabaseInterface>(state: EthState<D>) -> Result<EthState<D>> {
     info!("✔ Validating ETH block in state...");
-    validate_block_in_state(state, true)
+    validate_block_in_state(&state, true).and(Ok(state))
 }
