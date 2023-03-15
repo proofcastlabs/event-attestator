1f63e02d6a4fc656d60e42ac9419293ba2532716
gskapka <greg@oraclize.it>
2023-03-15 10:26:20 +0000

    feat(sentinel): <- use batch for block num tracking in syncer loop

--- v3_bridges/sentinel/src/sentinel/syncer/syncer_loop.rs
+++ v3_bridges/sentinel/src/sentinel/syncer/syncer_loop.rs
@@ -15,22 +15,21 @@ use tokio::{
     time::{sleep, Duration},
 };
 
-async fn host_loop(
+async fn main_loop(
     mut batch: Batch,
     processor_tx: MpscTx<ProcessorMessages>,
     _host_syncer_rx: BroadcasterRx<SyncerMessages>,
 ) -> Result<(), SentinelError> {
-    let log_prefix = if batch.is_native() { "native" } else { "host" };
+    let log_prefix = format!("{} syncer", if batch.is_native() { "native" } else { "host" });
     let ws_client = batch.get_rpc_client().await?;
     let sleep_duration = batch.get_sleep_duration();
-    let mut block_num = 16778137; // TODO get block num from db! Or store it in the batch?
 
     'main_loop: loop {
-        match get_sub_mat(&ws_client, block_num).await {
+        match get_sub_mat(&ws_client, batch.get_block_num()).await {
             Ok(block) => {
                 batch.push(block);
                 if !batch.is_ready_to_submit() {
-                    block_num += 1;
+                    batch.increment_block_num();
                     continue 'main_loop;
                 } else {
                     // TODO check if batch is chained correctly!
@@ -40,15 +39,15 @@ async fn host_loop(
                     processor_tx.send(ProcessorMessages::ProcessHost(args)).await?;
                     match resp_rx.await? {
                         Ok(_) => {
-                            debug!("host oneshot channel returned ok");
-                            block_num += 1;
+                            debug!("{log_prefix} oneshot channel returned ok");
+                            batch.increment_block_num();
                         },
                         Err(SentinelError::SyncerRestart(n)) => {
-                            warn!("host oneshot channel returned a syncer restart err {n}");
-                            block_num = n;
+                            warn!("{log_prefix} oneshot channel returned a syncer restart err {n}");
+                            batch.set_block_num(n);
                         },
                         Err(e) => {
-                            warn!("host oneshot channel returned err {e}");
+                            warn!("{log_prefix} oneshot channel returned err {e}");
                             break 'main_loop Err(e);
                         },
                     };
@@ -75,12 +74,11 @@ async fn native_loop(
 ) -> Result<(), SentinelError> {
     let ws_client = batch.get_rpc_client().await?;
     let sleep_duration = batch.get_sleep_duration();
-    let mut block_num = 16778137;
 
     sleep(Duration::from_millis(1000000)).await; // FIXME rm!
 
     'native_loop: loop {
-        let maybe_block = get_sub_mat(&ws_client, block_num).await;
+        let maybe_block = get_sub_mat(&ws_client, batch.get_block_num()).await;
 
         if let Ok(block) = maybe_block {
             batch.push(block);
@@ -97,7 +95,7 @@ async fn native_loop(
                 // TODO start block number is wrong!
                 continue 'native_loop;
             } else {
-                block_num += 1;
+                batch.increment_block_num();
                 continue 'native_loop;
             }
         } else if let Err(SentinelError::Endpoint(EndpointError::NoBlock(_))) = maybe_block {
@@ -111,12 +109,14 @@ async fn native_loop(
 }
 
 pub async fn native_syncer_loop(
-    batch: Batch,
+    mut batch: Batch,
     _broadcast_rx: BroadcasterRx<BroadcastMessages>,
     syncer_rx: BroadcasterRx<SyncerMessages>,
     processor_tx: MpscTx<ProcessorMessages>,
 ) -> Result<(), SentinelError> {
+    let block_num = 16778137; // FIXME get this from the core!
     let log_prefix = "native_syncer:".to_string();
+    batch.set_block_num(block_num);
 
     tokio::select! {
         res = native_loop(&log_prefix, batch, processor_tx, syncer_rx) => res,
@@ -128,15 +128,17 @@ pub async fn native_syncer_loop(
 }
 
 pub async fn host_syncer_loop(
-    batch: Batch,
+    mut batch: Batch,
     _broadcast_rx: BroadcasterRx<BroadcastMessages>,
     syncer_rx: BroadcasterRx<SyncerMessages>,
     processor_tx: MpscTx<ProcessorMessages>,
 ) -> Result<(), SentinelError> {
+    let block_num = 16778137; // FIXME get this from the core!
     let log_prefix = "host_syncer:";
+    batch.set_block_num(block_num);
 
     tokio::select! {
-        res = host_loop(batch, processor_tx, syncer_rx) => res,
+        res = main_loop(batch, processor_tx, syncer_rx) => res,
         _ = tokio::signal::ctrl_c() => {
             warn!("{log_prefix} shutting down...");
             Err(SentinelError::SigInt("host syncer".into()))
