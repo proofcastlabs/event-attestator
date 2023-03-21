ea541f3a0ed1ecdff32f35d05e007d951c3d95a2
gskapka <greg@oraclize.it>
2023-03-21 17:42:32 +0000

    feat(sentinel): <- fix `in_sync` definition to max of confs or batch size

---
+++ v3_bridges/sentinel/src/lib/messages/eth_rpc.rs
@@ -0,0 +1,23 @@
+use common::{BridgeSide, CoreType};
+use tokio::sync::{oneshot, oneshot::Receiver};
+use ethereum_types::Address as EthAddress;
+
+use crate::{CoreState, Responder, SentinelError};
+
+#[derive(Debug)]
+pub enum EthRpcMessages {
+    GetLatestBlockNum(Responder<u64>),
+    GetNonce((EthAddress, Responder<u64>)),
+}
+
+impl EthRpcMessages {
+    pub fn get_nonce_msg(a: EthAddress) -> (Self, Receiver<Result<u64, SentinelError>>) {
+        let (tx, rx) = oneshot::channel();
+        (Self::GetNonce((a, tx)), rx)
+    }
+
+    pub fn get_latest_block_num_msg() -> (Self, Receiver<Result<u64, SentinelError>>) {
+        let (tx, rx) = oneshot::channel();
+        (Self::GetLatestBlockNum(tx), rx)
+    }
+}
--- v3_bridges/sentinel/src/lib/messages/processor.rs
+++ v3_bridges/sentinel/src/lib/messages/processor.rs
@@ -16,14 +16,14 @@ impl ProcessArgs {
     pub fn is_in_sync(&self) -> Result<bool, SentinelError> {
         // NOTE: We define the core as being in sync if the about-to-be-submitted batch's last
         // block number is within x blocks of the chain's tip, where x is whichever is the
-        // smaller of the batch size OR the number of confs.
+        // larger of the batch size OR the number of confs.
         let batch_last_block_num = self.batch.get_last_block_num()?;
         let delta = if self.latest_block_number > batch_last_block_num {
             self.latest_block_number - batch_last_block_num
         } else {
             0
         };
-        let required_delta = self.confs.min(self.batch_size);
+        let required_delta = self.confs.max(self.batch_size);
         Ok(delta <= required_delta)
     }
 }
