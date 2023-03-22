b636ecb759c0f0ef22e01509d0e2538965273c7c
gskapka <greg@oraclize.it>
2023-03-22 11:18:15 +0000

    feat(sentinel): <- adds "PushTx" ETH rpc message

--- v3_bridges/sentinel/src/lib/messages/eth_rpc.rs
+++ v3_bridges/sentinel/src/lib/messages/eth_rpc.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)] // FIXME rm once this is used
-use ethereum_types::Address as EthAddress;
+use common_eth::EthTransaction;
+use ethereum_types::{Address as EthAddress, H256 as EthHash};
 use tokio::sync::{oneshot, oneshot::Receiver};
 
 use crate::{Responder, SentinelError};
@@ -8,6 +9,7 @@ use crate::{Responder, SentinelError};
 pub enum EthRpcMessages {
     GetLatestBlockNum(Responder<u64>),
     GetNonce((EthAddress, Responder<u64>)),
+    PushTx((EthTransaction, Responder<EthHash>)),
 }
 
 impl EthRpcMessages {
@@ -19,5 +21,10 @@ impl EthRpcMessages {
     pub fn get_latest_block_num_msg() -> (Self, Receiver<Result<u64, SentinelError>>) {
         let (tx, rx) = oneshot::channel();
         (Self::GetLatestBlockNum(tx), rx)
+    }
+
+    pub fn get_push_txs_msg(t: EthTransaction) -> (Self, Receiver<Result<EthHash, SentinelError>>) {
+        let (tx, rx) = oneshot::channel();
+        (Self::PushTx((t, tx)), rx)
     }
 }
