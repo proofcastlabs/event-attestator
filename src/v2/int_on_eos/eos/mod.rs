123804ba6f72406c5612b0adbce65f39953f8f9d
gskapka <greg@oraclize.it>
2023-05-03 10:41:08 +0100

    ref(int-on-eos): <- limit publicity of new mod in that

--- src/v2/int_on_eos/eos/mod.rs
+++ src/v2/int_on_eos/eos/mod.rs
@@ -19,9 +19,10 @@ pub(super) use self::{
         divert_tx_infos_to_safe_address_if_destination_is_vault_address,
         divert_tx_infos_to_safe_address_if_destination_is_zero_address,
     },
-    filter_for_redeems::maybe_filter_for_relevant_redeem_actions,
     get_eos_output::{get_tx_infos_from_signed_txs, EosOutput},
     increment_int_nonce::maybe_increment_int_nonce_in_db_and_return_eos_state,
     parse_tx_info::maybe_parse_int_tx_infos_and_put_in_state,
 };
+
+pub(in crate::v2::int_on_eos::eos) use self::filter_for_redeems::maybe_filter_for_relevant_redeem_actions;
 pub use self::{initialize_eos_core::maybe_initialize_eos_core, submit_eos_block::submit_eos_block_to_core};
