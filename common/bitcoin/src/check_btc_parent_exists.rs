75515a019737804bab07118e0185c8c6f9a1ae0b
gskapka <greg@oraclize.it>
2023-06-16 14:08:44 +0100

    feat(bitcoin): <- adds block number to no parent error in that

--- common/bitcoin/src/check_btc_parent_exists.rs
+++ common/bitcoin/src/check_btc_parent_exists.rs
@@ -4,14 +4,14 @@ use crate::BtcState;
 
 pub fn check_for_parent_of_btc_block_in_state<D: DatabaseInterface>(state: BtcState<D>) -> Result<BtcState<D>> {
     info!("✔ Checking BTC block's parent exists in database...");
-    if state
-        .btc_db_utils
-        .get_btc_block_from_db(&state.get_btc_block_and_id()?.block.header.prev_blockhash)
-        .is_ok()
-    {
+    let sub_mat = state.get_btc_block_and_id()?;
+    let n = sub_mat.height;
+    let prev_hash = sub_mat.block.header.prev_blockhash;
+    debug!("searching db for block under hash: {prev_hash}");
+    if state.btc_db_utils.get_btc_block_from_db(&prev_hash).is_ok() {
         info!("✔ BTC block's parent exists in database!");
         Ok(state)
     } else {
-        Err("✘ BTC block Rejected - no parent exists in database!".into())
+        Err(format!("✘ BTC block Rejected - no parent for block {n} exists in database!").into())
     }
 }
