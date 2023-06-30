68ff556e4504163df53a9b51723bc56de0c0e16d
gskapka <greg@oraclize.it>
2023-06-30 20:26:29 +0100

    fix(sentinel): <- bump gnosis chain's gas limit up

--- v3_bridges/sentinel/src/lib/user_ops/cancellation_gas_limit.rs
+++ v3_bridges/sentinel/src/lib/user_ops/cancellation_gas_limit.rs
@@ -5,7 +5,7 @@ use super::{UserOp, USER_OP_CANCEL_TX_GAS_LIMIT};
 impl UserOp {
     pub fn cancellation_gas_limit(chain_id: &EthChainId) -> usize {
         match chain_id {
-            EthChainId::XDaiMainnet => 150_000,
+            EthChainId::XDaiMainnet => 200_000,
             EthChainId::ArbitrumMainnet => 2_000_000,
             _ => USER_OP_CANCEL_TX_GAS_LIMIT as usize,
         }
