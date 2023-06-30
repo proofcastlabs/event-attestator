13b5a6087420136b2d9aca6afe75e3992a3180a7
gskapka <greg@oraclize.it>
2023-06-30 17:15:14 +0100

    feat(sentinel): <- adds check in cancel cli for state of user op

--- v3_bridges/sentinel/src/cli/get_cancel_tx.rs
+++ v3_bridges/sentinel/src/cli/get_cancel_tx.rs
@@ -1,22 +1,25 @@
 use common_eth::{convert_hex_to_h256, EthDbUtilsExt, HostDbUtils, NativeDbUtils};
 use common_rocksdb_database::get_db_at_path;
+use ethereum_types::H256 as EthHash;
 use lib::{
     check_init,
     get_gas_price,
     get_nonce,
+    get_user_op_state,
+    push_tx,
     ConfigT,
     DbUtilsT,
     SentinelConfig,
     SentinelDbUtils,
     SentinelError,
     UserOp,
-    DEFAULT_SLEEP_TIME,
+    UserOpSmartContractState,
     USER_OP_CANCEL_TX_GAS_LIMIT,
 };
 use serde_json::json;
 
 #[derive(Clone, Debug, Default, Args)]
-pub struct GetCancelTxArgs {
+pub struct CancelTxArgs {
     /// User op identifaction hash
     uid: String,
 
@@ -25,15 +28,21 @@ pub struct GetCancelTxArgs {
     nonce: Option<u64>,
 
     /// Gas price to use. If omitted it will use the gas price suggested by the RPC
-    #[arg(long, short)]
+    #[arg(long)]
     gas_price: Option<u64>,
 
     /// Gas limit to use. If omitted it will use the default amount instead
-    #[arg(long, short)]
+    #[arg(long)]
     gas_limit: Option<usize>,
+
+    /// Push the tx to the endpoint
+    #[arg(long, short)]
+    push: bool,
 }
 
-pub async fn get_cancel_tx(config: &SentinelConfig, args: &GetCancelTxArgs) -> Result<String, SentinelError> {
+const SLEEP_TIME: u64 = 2000;
+
+pub async fn get_cancel_tx(config: &SentinelConfig, args: &CancelTxArgs) -> Result<String, SentinelError> {
     let db = get_db_at_path(&config.get_db_path())?;
     let db_utils = SentinelDbUtils::new(&db);
     check_init(&db)?;
@@ -50,7 +59,8 @@ pub async fn get_cancel_tx(config: &SentinelConfig, args: &GetCancelTxArgs) -> R
                     "user op is not enqueued, cannot cancel it".into(),
                 ))
             } else {
-                let side = op.side();
+                let side = op.destination_side();
+
                 let h_db_utils = HostDbUtils::new(&db);
                 let n_db_utils = NativeDbUtils::new(&db);
 
@@ -59,6 +69,22 @@ pub async fn get_cancel_tx(config: &SentinelConfig, args: &GetCancelTxArgs) -> R
                 } else {
                     config.host().endpoints().get_first_ws_client().await?
                 };
+
+                let state_manager = if side.is_native() {
+                    config.native().state_manager()
+                } else {
+                    config.host().state_manager()
+                };
+
+                let user_op_state = get_user_op_state(&op, &state_manager, &ws_client, SLEEP_TIME, side).await?;
+
+                if user_op_state != UserOpSmartContractState::Enqueued {
+                    return Err(SentinelError::Custom(format!(
+                        "cannot cancel user op, user op state is {user_op_state}"
+                    )));
+                }
+
+                debug!("user op state: {user_op_state}");
 
                 let address = if side.is_native() {
                     n_db_utils.get_public_eth_address_from_db()?
@@ -70,7 +96,7 @@ pub async fn get_cancel_tx(config: &SentinelConfig, args: &GetCancelTxArgs) -> R
                     debug!("using passed in nonce {n}");
                     n
                 } else {
-                    let n = get_nonce(&ws_client, &address, DEFAULT_SLEEP_TIME, side).await?;
+                    let n = get_nonce(&ws_client, &address, SLEEP_TIME, side).await?;
                     debug!("using nonce from RPC {n}");
                     n
                 };
@@ -79,7 +105,7 @@ pub async fn get_cancel_tx(config: &SentinelConfig, args: &GetCancelTxArgs) -> R
                     debug!("using passed in gas price {p}");
                     p
                 } else {
-                    let p = get_gas_price(&ws_client, DEFAULT_SLEEP_TIME, side).await?;
+                    let p = get_gas_price(&ws_client, SLEEP_TIME, side).await?;
                     debug!("using gas price from RPC: {p}");
                     p
                 };
@@ -110,19 +136,30 @@ pub async fn get_cancel_tx(config: &SentinelConfig, args: &GetCancelTxArgs) -> R
                     )
                 };
 
-                let tx = op
-                    .cancel(nonce, gas_price, &state_manager, gas_limit, &pk, &chain_id)?
-                    .serialize_hex();
-                debug!("signed tx: 0x{tx}");
+                let tx = op.cancel(nonce, gas_price, &state_manager, gas_limit, &pk, &chain_id)?;
+                let tx_hex = tx.serialize_hex();
+
+                debug!("signed tx: 0x{tx_hex}");
+
+                let tx_hash = if args.push {
+                    info!("pushing tx to endpoint");
+                    push_tx(&tx, &ws_client, SLEEP_TIME, side).await?
+                } else {
+                    info!("not pushing tx to endpoint");
+                    EthHash::zero()
+                };
 
                 let r = json!({
                     "jsonrpc": "2.0",
                     "result": {
                         "nonce": nonce,
                         "uid": args.uid,
-                        "signed_tx": tx,
+                        "pushed": args.push,
                         "gas_price": gas_price,
                         "gas_limit": gas_limit,
+                        "destination_side": side,
+                        "signed_tx": format!("0x{tx_hex}"),
+                        "tx_hash": format!("0x{}", hex::encode(tx_hash.as_bytes())),
                     }
                 })
                 .to_string();
