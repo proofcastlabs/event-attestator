bec547fa286166664821e90254e250d1b00629af
gskapka <gregkapka@gmail.com>
2023-08-31 12:28:36 +0100

    feat(sentinel): <- adds websocket messages required for initting a strongbox core

--- common/sentinel/src/messages/websocket.rs
+++ common/sentinel/src/messages/websocket.rs
@@ -1,20 +1,49 @@
 use base64::{engine::general_purpose, Engine};
+use common_metadata::MetadataChainId;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 use crate::SentinelError;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub enum WebSocketMessages {
-    GetNativeLatestBlockNum,
+#[derive(Error, Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub enum Error {
+    #[error("core not initialized for chain id: {0}")]
+    Uninitialized(MetadataChainId),
+
+    #[error("core already initialized for chain id: {0}")]
+    AlreadyInitialized(MetadataChainId),
 }
 
-impl WebSocketMessages {
-    fn encode(&self) -> Result<String, SentinelError> {
-        Ok(general_purpose::STANDARD_NO_PAD.encode(serde_json::to_vec(self)?))
-    }
+pub type Confirmations = u64;
 
-    fn decode(s: &str) -> Result<Self, SentinelError> {
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub enum WebSocketMessages {
+    Error(Error),
+    GetLatestBlockNum(MetadataChainId),
+    Initialize(MetadataChainId, Confirmations),
+}
+
+impl TryFrom<String> for WebSocketMessages {
+    type Error = SentinelError;
+
+    fn try_from(s: String) -> Result<Self, SentinelError> {
+        Self::try_from(s.as_str())
+    }
+}
+
+impl TryFrom<&str> for WebSocketMessages {
+    type Error = SentinelError;
+
+    fn try_from(s: &str) -> Result<Self, SentinelError> {
         Ok(serde_json::from_slice(&general_purpose::STANDARD_NO_PAD.decode(s)?)?)
+    }
+}
+
+impl TryInto<String> for WebSocketMessages {
+    type Error = SentinelError;
+
+    fn try_into(self) -> Result<String, SentinelError> {
+        Ok(general_purpose::STANDARD_NO_PAD.encode(serde_json::to_vec(&self)?))
     }
 }
 
@@ -23,12 +52,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn should_make_serde_roundtrip() {
-        let m = WebSocketMessages::GetNativeLatestBlockNum;
-        let s = m.encode().unwrap();
-        let expected_s = "IkdldE5hdGl2ZUxhdGVzdEJsb2NrTnVtIg";
+    fn websocket_messages_should_make_serde_roundtrip() {
+        let id = MetadataChainId::EthereumMainnet;
+        let m = WebSocketMessages::GetLatestBlockNum(id);
+        let s: String = m.clone().try_into().unwrap();
+        let expected_s = "eyJHZXRMYXRlc3RCbG9ja051bSI6IkV0aGVyZXVtTWFpbm5ldCJ9";
         assert_eq!(s, expected_s);
-        let r = WebSocketMessages::decode(&s).unwrap();
+        let r = WebSocketMessages::try_from(s).unwrap();
         assert_eq!(r, m);
     }
 }
