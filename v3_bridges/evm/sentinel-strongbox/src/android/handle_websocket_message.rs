0b668eeb379540aa9824981c825f42c0a7a6fb1c
gskapka <gregkapka@gmail.com>
2023-09-11 16:22:16 +0100

    feat(strongbox): <- adds init checker to msg handler

--- v3_bridges/evm/sentinel-strongbox/src/android/handle_websocket_message.rs
+++ v3_bridges/evm/sentinel-strongbox/src/android/handle_websocket_message.rs
@@ -1,13 +1,14 @@
 use std::result::Result;
 
 use common_metadata::MetadataChainId;
-use common_sentinel::{SentinelError, WebSocketMessagesEncodable};
+use common_sentinel::{check_init, SentinelError, WebSocketMessagesEncodable};
 
 use super::State;
 
 pub fn handle_websocket_message(state: State) -> Result<State, SentinelError> {
     info!("handling web socket message...");
     state.db().start_transaction()?;
+    check_init(state.db())?;
     let msg = state.msg();
     let final_state = match msg {
         WebSocketMessagesEncodable::Initialize(args) => super::handlers::init(*args.clone(), state),
