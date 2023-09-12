9a5c70906cc00a2bfee6e7ce1eb691120891793c
gskapka <gregkapka@gmail.com>
2023-09-12 16:51:02 +0100

    feat(sentinel-common): <- adds core timeout field to config

--- common/sentinel/src/sentinel_config/sentinel_core_config.rs
+++ common/sentinel/src/sentinel_config/sentinel_core_config.rs
@@ -1,21 +1,28 @@
 use std::{path::Path, result::Result, str::FromStr};
 
 use common::{CoreType, V3CoreType};
+use derive_getters::Getters;
 use serde::Deserialize;
 
-use crate::SentinelError;
+use crate::{constants::MILLISECONDS_MULTIPLIER, SentinelError};
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct SentinelCoreToml {
+    timeout: u64,
     db_path: String,
     core_type: String,
     max_cancellable_time_delta: u64,
 }
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, Getters)]
 pub struct SentinelCoreConfig {
-    pub db_path: String,
-    pub core_type: CoreType,
+    timeout: u64,
+    db_path: String,
+
+    #[getter(skip)]
+    core_type: CoreType,
+
+    #[getter(skip)]
     max_cancellable_time_delta: u64,
 }
 
@@ -31,6 +38,7 @@ impl SentinelCoreConfig {
     pub fn from_toml(toml: &SentinelCoreToml) -> Result<Self, SentinelError> {
         Ok(Self {
             db_path: toml.db_path.clone(),
+            timeout: toml.timeout * MILLISECONDS_MULTIPLIER, // TODO sanity check?
             max_cancellable_time_delta: toml.max_cancellable_time_delta,
             core_type: CoreType::V3(V3CoreType::from_str(&toml.core_type)?),
         })
