c1de1ecf8aa178b922b9b236a968e26146f2b03d
gskapka <gregkapka@gmail.com>
2023-09-15 12:48:05 +0100

    feat(sentinel-lib): <- adds broadcast channel messages enum for rpc server

--- common/sentinel/src/messages/broadcast_channel.rs
+++ common/sentinel/src/messages/broadcast_channel.rs
@@ -2,6 +2,8 @@ use std::fmt;
 
 use common_chain_ids::EthChainId;
 
+// TODO Move this into dir and separate accordingly
+
 #[derive(Debug, Clone)]
 pub enum SyncerBroadcastChannelMessages {
     Stop,
@@ -10,19 +12,38 @@ pub enum SyncerBroadcastChannelMessages {
     CoreDisconnected,
 }
 
+#[derive(Debug, Clone)]
+pub enum RpcServerBroadcastChannelMessages {
+    CoreConnected,
+    CoreDisconnected,
+}
+
 impl fmt::Display for SyncerBroadcastChannelMessages {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let prefix = "syncer broadcast channel message:";
         let s = match self {
             Self::Stop => "stop",
             Self::Start => "start",
             Self::CoreConnected => "core connected",
             Self::CoreDisconnected => "core disconnected",
         };
-        write!(f, "{s}")
+        write!(f, "{prefix} {s}")
+    }
+}
+
+impl fmt::Display for RpcServerBroadcastChannelMessages {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let prefix = "rpc server broadcast channel message:";
+        let s = match self {
+            Self::CoreConnected => "core connected",
+            Self::CoreDisconnected => "core disconnected",
+        };
+        write!(f, "{prefix} {s}")
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum BroadcastChannelMessages {
+    RpcServer(RpcServerBroadcastChannelMessages),
     Syncer(EthChainId, SyncerBroadcastChannelMessages),
 }
