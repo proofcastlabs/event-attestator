99fe43466098968fb29423c51f365f13b93bea3b
gskapka <gregkapka@gmail.com>
2023-09-19 18:31:18 +0100

    ref(sentinel-cli): <- rm that completely

--- v3_bridges/evm/sentinel-cli/src/cmds/get_core_state.rs
+++
@@ -1,9 +0,0 @@
-use common_rocksdb_database::get_db_at_path;
-use common_sentinel::{check_init, CoreState, SentinelConfig, SentinelError};
-use serde_json::json;
-
-pub fn get_core_state(config: &SentinelConfig) -> Result<String, SentinelError> {
-    let db = get_db_at_path(&config.get_db_path())?;
-    check_init(&db)?;
-    Ok(json!({"jsonrpc": "2.0", "result": CoreState::get(&db, &config.core().core_type)?}).to_string())
-}
