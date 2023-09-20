c2c557a04be207c13891522e648f59a25ae8d0a5
gskapka <gregkapka@gmail.com>
2023-09-20 17:06:02 +0100

    feat(sentinel-strongbox): <- don't print exception error to console for failed db key gets

--- v3_bridges/evm/sentinel-strongbox/src/android/database.rs
+++ v3_bridges/evm/sentinel-strongbox/src/android/database.rs
@@ -42,30 +42,38 @@ impl<'a> Database<'a> {
     }
 
     pub fn start_transaction(&self) -> Result<(), SentinelError> {
+        let print_exceptions = true;
         match self.env.call_method(self.db_java_class, "startTransaction", "()V", &[]) {
-            Ok(_) => self.check_and_handle_java_exceptions(),
-            Err(e) => self.handle_error(Err(e)),
+            Ok(_) => self.check_and_handle_java_exceptions(print_exceptions),
+            Err(e) => self.handle_error(Err(e), print_exceptions),
         }
     }
 
     pub fn end_transaction(&self) -> Result<(), SentinelError> {
+        let print_exceptions = true;
         match self.env.call_method(self.db_java_class, "endTransaction", "()V", &[]) {
-            Ok(_) => self.check_and_handle_java_exceptions(),
-            Err(e) => self.handle_error(Err(e)),
+            Ok(_) => self.check_and_handle_java_exceptions(print_exceptions),
+            Err(e) => self.handle_error(Err(e), print_exceptions),
         }
     }
 
     pub fn delete(&self, k: &ByteArray) -> Result<(), SentinelError> {
+        let print_exceptions = true;
         match self
             .env
             .call_method(self.db_java_class, "delete", "([B)V", &[self.to_java_byte_array(k)?])
         {
-            Ok(_) => self.check_and_handle_java_exceptions(),
-            Err(e) => self.handle_error(Err(e)),
+            Ok(_) => self.check_and_handle_java_exceptions(print_exceptions),
+            Err(e) => self.handle_error(Err(e), print_exceptions),
         }
     }
 
     pub fn get(&self, k: &ByteArray, sensitivity: DataSensitivity) -> Result<Bytes, SentinelError> {
+        // NOTE: Exceptions here are if there is no key in the db, which case should _always_
+        // be handled by the rust core. Since we hit this case often (checking if blocks exist
+        // etc), we don't want to pollute the logcat output by printing out many lines every
+        // time we don't find an item in the db.
+        let print_exceptions = false;
         let args = [
             self.to_java_byte_array(k)?,
             JValue::from(sensitivity.unwrap_or_default()),
@@ -76,27 +84,33 @@ impl<'a> Database<'a> {
             .and_then(|ret| ret.l())
             .and_then(|j_value| self.env.convert_byte_array(j_value.into_inner()))
         {
-            Ok(r) => self.check_and_handle_java_exceptions().map(|_| r),
-            Err(e) => self.handle_error(Err(e)),
+            Ok(r) => self.check_and_handle_java_exceptions(print_exceptions).map(|_| r),
+            Err(e) => self.handle_error(Err(e), print_exceptions),
         }
     }
 
     pub fn put(&self, k: &ByteArray, v: &ByteArray, sensitivity: Option<u8>) -> Result<(), SentinelError> {
+        let print_exceptions = true;
         let args = [
             self.to_java_byte_array(k)?,
             self.to_java_byte_array(v)?,
             JValue::from(sensitivity.unwrap_or_default()),
         ];
         match self.env.call_method(self.db_java_class, "put", "([B[BB)V", &args) {
-            Ok(_) => self.check_and_handle_java_exceptions(),
-            Err(e) => self.handle_error(Err(e)),
+            Ok(_) => self.check_and_handle_java_exceptions(print_exceptions),
+            Err(e) => self.handle_error(Err(e), print_exceptions),
         }
     }
 
-    fn handle_error<T, E: Into<SentinelError> + std::fmt::Display>(&self, r: Result<T, E>) -> Result<T, SentinelError> {
+    fn handle_error<T, E: Into<SentinelError> + std::fmt::Display>(
+        &self, r: Result<T, E>,
+        print_exceptions: bool,
+    ) -> Result<T, SentinelError> {
         if let Err(e) = r {
             error!("{e}");
-            self.env.exception_describe()?;
+            if print_exceptions {
+                self.env.exception_describe()?;
+            };
             self.env.exception_clear()?;
             Err(e.into())
         } else {
@@ -104,9 +118,11 @@ impl<'a> Database<'a> {
         }
     }
 
-    fn check_and_handle_java_exceptions(&self) -> Result<(), SentinelError> {
+    fn check_and_handle_java_exceptions(&self, print_exceptions: bool) -> Result<(), SentinelError> {
         if matches!(self.env.exception_check(), Ok(true)) {
-            self.env.exception_describe()?;
+            if print_exceptions {
+                self.env.exception_describe()?;
+            };
             self.env.exception_clear()?;
             Err(SentinelError::JavaExceptionOccurred)
         } else {
