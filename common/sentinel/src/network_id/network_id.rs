564f8abd62b3648d4f0d09af7509710f23fcbde3
gskapka <gregkapka@gmail.com>
2023-09-27 09:03:51 +0100

    feat(sentinel-lib): rm generic param from network ID

--- common/sentinel/src/network_id/network_id.rs
+++ common/sentinel/src/network_id/network_id.rs
@@ -1,9 +1,11 @@
 use std::fmt;
 
 use common::{Byte, Bytes};
-use common_chain_ids::ChainIdT;
+use common_chain_ids::EthChainId;
+use common_metadata::{MetadataChainId, MetadataChainIdError};
 use derive_more::Deref;
 use ethabi::{encode as ethabi_encode, Token};
+use ethereum_types::U256;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
@@ -37,19 +39,19 @@ impl fmt::Display for Bytes4 {
 }
 
 #[derive(Clone, Debug, Default, Eq, PartialEq, Serialize, Deserialize)]
-pub struct NetworkId<C: ChainIdT> {
-    chain_id: C,
+pub struct NetworkId {
+    chain_id: u64,       // FIXME make this a u64, since that's how it's encoded
     disambiguator: Byte, // NOTE: Can be rolled in case of collisions.
     protocol_id: ProtocolId,
     version: NetworkIdVersion,
 }
 
-impl<C: ChainIdT + Clone> NetworkId<C> {
-    pub fn new(chain_id: C, protocol_id: ProtocolId) -> Self {
+impl NetworkId {
+    pub fn new(chain_id: u64, protocol_id: ProtocolId) -> Self {
         Self::new_v1(chain_id, protocol_id)
     }
 
-    fn new_v1(chain_id: C, protocol_id: ProtocolId) -> Self {
+    fn new_v1(chain_id: u64, protocol_id: ProtocolId) -> Self {
         Self {
             chain_id,
             protocol_id,
@@ -59,12 +61,12 @@ impl<C: ChainIdT + Clone> NetworkId<C> {
     }
 }
 
-impl<C: ChainIdT + Clone> NetworkId<C> {
+impl NetworkId {
     fn abi_encode(&self) -> Bytes {
         ethabi_encode(&[
             Token::FixedBytes([<NetworkIdVersion as Into<u8>>::into(self.version)].to_vec()),
             Token::FixedBytes([<ProtocolId as Into<u8>>::into(self.protocol_id)].to_vec()),
-            Token::Uint(self.chain_id.clone().into()),
+            Token::Uint(U256::from(self.chain_id)),
             Token::FixedBytes([self.disambiguator].to_vec()),
         ])
     }
@@ -80,11 +82,47 @@ impl<C: ChainIdT + Clone> NetworkId<C> {
     }
 }
 
-impl<C: ChainIdT + Clone> TryInto<Bytes4> for NetworkId<C> {
+impl TryInto<Bytes4> for NetworkId {
     type Error = NetworkIdError;
 
     fn try_into(self) -> Result<Bytes4, NetworkIdError> {
         self.to_bytes_4()
+    }
+}
+
+impl TryFrom<MetadataChainId> for NetworkId {
+    type Error = MetadataChainIdError;
+
+    fn try_from(m: MetadataChainId) -> Result<NetworkId, Self::Error> {
+        match m {
+            MetadataChainId::EthUnknown => Ok(NetworkId::new(EthChainId::Unknown(0).to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::BscMainnet => Ok(NetworkId::new(EthChainId::BscMainnet.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::XDaiMainnet => Ok(NetworkId::new(EthChainId::XDaiMainnet.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::FantomMainnet => {
+                Ok(NetworkId::new(EthChainId::FantomMainnet.to_u64(), ProtocolId::Ethereum))
+            },
+            MetadataChainId::InterimChain => {
+                Ok(NetworkId::new(EthChainId::InterimChain.to_u64(), ProtocolId::Ethereum))
+            },
+            MetadataChainId::EthereumGoerli => Ok(NetworkId::new(EthChainId::Goerli.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::PolygonMainnet => Ok(NetworkId::new(
+                EthChainId::PolygonMainnet.to_u64(),
+                ProtocolId::Ethereum,
+            )),
+            MetadataChainId::EthereumSepolia => Ok(NetworkId::new(EthChainId::Sepolia.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::ArbitrumMainnet => Ok(NetworkId::new(
+                EthChainId::ArbitrumMainnet.to_u64(),
+                ProtocolId::Ethereum,
+            )),
+            MetadataChainId::EthereumMainnet => Ok(NetworkId::new(EthChainId::Mainnet.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::EthereumRopsten => Ok(NetworkId::new(EthChainId::Ropsten.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::EthereumRinkeby => Ok(NetworkId::new(EthChainId::Rinkeby.to_u64(), ProtocolId::Ethereum)),
+            MetadataChainId::LuxochainMainnet => Ok(NetworkId::new(
+                EthChainId::LuxochainMainnet.to_u64(),
+                ProtocolId::Ethereum,
+            )),
+            mcid => Err(Self::Error::CannotConvertTo(mcid, "NetworkId".to_string())),
+        }
     }
 }
 
