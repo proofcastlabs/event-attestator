ca704a7df034fb499c3588aee755dde024e8bd2e
gskapka <gregkapka@gmail.com>
2023-10-24 10:34:36 +0100

    fix(sentinel-lib): <- update user ops cancellation to latest abi

--- common/sentinel/src/constants.rs
+++ common/sentinel/src/constants.rs
@@ -5,6 +5,7 @@ pub const DEFAULT_SLEEP_TIME: u64 = 15_000;
 pub const MILLISECONDS_MULTIPLIER: u64 = 1000;
 
 lazy_static! {
+    pub static ref ACTOR_TYPE: crate::ActorType = crate::ActorType::Sentinel;
     pub static ref HOST_PROTOCOL_ID: crate::ProtocolId = crate::ProtocolId::Ethereum;
     pub static ref NATIVE_PROTOCOL_ID: crate::ProtocolId = crate::ProtocolId::Ethereum;
 }
