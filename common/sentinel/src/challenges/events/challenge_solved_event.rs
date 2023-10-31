ccff930909b681363b668c1c98926fc24b7b30b3
gskapka <gregkapka@gmail.com>
2023-10-31 13:25:06 +0000

    feat(sentinel-lib): <- adds way to get challenge ids from challenge solved events

--- common/sentinel/src/challenges/events/challenge_solved_event.rs
+++ common/sentinel/src/challenges/events/challenge_solved_event.rs
@@ -4,7 +4,7 @@ use common_eth::EthSubmissionMaterial;
 use derive_more::{Constructor, Deref};
 use ethereum_types::{Address as EthAddress, H256 as EthHash};
 
-use crate::challenges::{ChallengeEvent, ChallengesError};
+use crate::challenges::{Challenge, ChallengeEvent, Challenges, ChallengesError};
 
 lazy_static! {
     pub(in crate::challenges) static ref CHALLENGE_SOLVED_EVENT_TOPIC: EthHash =
@@ -28,5 +28,13 @@ impl ChallengeSolvedEvents {
             .map(ChallengeEvent::try_from)
             .collect::<Result<Vec<ChallengeEvent>, ChallengesError>>()?;
         Ok(Self::new(events))
+    }
+
+    fn to_challenges(&self) -> Challenges {
+        Challenges::new(self.iter().map(Challenge::from).collect())
+    }
+
+    pub(crate) fn to_ids(&self) -> Result<Vec<EthHash>, ChallengesError> {
+        self.to_challenges().iter().map(|c| c.id()).collect()
     }
 }
