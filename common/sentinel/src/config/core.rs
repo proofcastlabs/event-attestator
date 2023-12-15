5f2f33b8717171358416fec78c8fdeb062fe5db9
gskapka <gregkapka@gmail.com>
2023-12-15 11:00:03 +0000

    feat(sentinel-lib): <- adds new config value & sanity checking

--- common/sentinel/src/config/core.rs
+++ common/sentinel/src/config/core.rs
@@ -4,6 +4,5 @@ use serde::Deserialize;
 #[derive(Debug, Default, Clone, Getters, Deserialize)]
 pub struct SentinelCoreConfig {
     timeout: u64,
-    max_cancellable_time_delta: u64,
     challenge_response_frequency: u64,
 }
--- common/sentinel/src/config/network.rs
+++ common/sentinel/src/config/network.rs
@@ -17,6 +17,7 @@ pub struct NetworkToml {
     endpoints: Vec<String>,
     gas_price: Option<u64>,
     pre_filter_receipts: bool,
+    base_challenge_period_duration: u64,
 }
 
 #[derive(Debug, Clone, Default, Getters)]
@@ -31,6 +32,7 @@ pub struct NetworkConfig {
     gas_price: Option<u64>,
     pnetwork_hub: EthAddress,
     pre_filter_receipts: bool,
+    base_challenge_period_duration: u64,
 }
 
 impl NetworkConfig {
@@ -47,6 +49,9 @@ impl NetworkConfig {
             batch_size: Self::sanity_check_batch_size(toml.batch_size)?,
             pnetwork_hub: convert_hex_to_eth_address(&toml.pnetwork_hub)?,
             batch_duration: Self::sanity_check_batch_duration(toml.batch_duration)?,
+            base_challenge_period_duration: Self::sanity_check_base_challenge_period_duration(
+                toml.base_challenge_period_duration,
+            )?,
         })
     }
 
@@ -59,7 +64,7 @@ impl NetworkConfig {
     }
 
     fn sanity_check_batch_size(batch_size: u64) -> Result<u64, SentinelError> {
-        info!("Sanity checking batch size...");
+        info!("sanity checking batch size...");
         const MIN: u64 = 0;
         const MAX: u64 = 1000;
         if batch_size > MIN && batch_size <= MAX {
@@ -74,7 +79,7 @@ impl NetworkConfig {
     }
 
     fn sanity_check_batch_duration(batch_duration: u64) -> Result<u64, SentinelError> {
-        info!("Sanity checking batch duration...");
+        info!("sanity checking batch duration...");
         // NOTE: A batch duration of 0 means we submit material one at a time...
         const MAX: u64 = 60 * 10; // NOTE: Ten mins
         if batch_duration <= MAX {
@@ -84,6 +89,23 @@ impl NetworkConfig {
                 max: MAX,
                 size: batch_duration,
             }))
+        }
+    }
+
+    fn sanity_check_base_challenge_period_duration(duration: u64) -> Result<u64, SentinelError> {
+        info!("sanity checking base challenge period duration...");
+        const MAX: u64 = 60 * 60; // NOTE: an hour
+        const MIN: u64 = 60 * 5; // NOTE: 5 minutes
+        if duration > MIN && duration <= MAX {
+            Ok(duration)
+        } else {
+            Err(SentinelError::SentinelConfig(
+                SentinelConfigError::BaseChallengePeriodDuration {
+                    max: MAX,
+                    min: MIN,
+                    size: duration,
+                },
+            ))
         }
     }
 }
