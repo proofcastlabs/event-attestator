ac90591a938bf8f39ee2ce74a15f9f6f813c3cb2
gskapka <gregkapka@gmail.com>
2024-02-07 19:50:44 +0000

    chore(scripts): <- adds one of those to test `include-origin-tx-details` features

---
+++ scripts/test-build-with-origin-tx-details.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit early if non-zero exit code encountered
+set -e
+
+echo testing cores with origin chain tx pass through enabled
+
+featureFlag="--features=include-origin-tx-details"
+
+packages=(
+	"int_on_evm" # NOTE: array in case we add this feature to future cores
+)
+
+for package in "${packages[@]}"
+do
+  echo testing cores with origin chain tx pass through enabled for package: \'$package\'...
+  cargo test --package=$package $featureFlag
+done
