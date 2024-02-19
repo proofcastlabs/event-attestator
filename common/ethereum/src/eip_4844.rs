335e0b387cc4a69c9fa12475deedb9268eb93f72
Matteo Lisotto <matteo.lisotto@gmail.com>
2024-02-19 17:40:19 +0100

    fix(ethereum): remove `Eip4844::get_activation_block_number`

--- common/ethereum/src/eip_4844.rs
+++ common/ethereum/src/eip_4844.rs
@@ -1,7 +1,4 @@
-use common::types::Result;
-use common_chain_ids::EthChainId;
 use derive_more::Constructor;
-use ethereum_types::U256;
 
 use crate::EthBlock;
 
@@ -9,15 +6,6 @@ use crate::EthBlock;
 pub struct Eip4844 {}
 
 impl Eip4844 {
-    #[allow(dead_code)]
-    pub fn get_activation_block_number(&self, eth_chain_id: &EthChainId) -> Result<U256> {
-        match eth_chain_id {
-            EthChainId::Goerli => Ok(U256::from(10_456_115)),
-            EthChainId::Sepolia => Ok(U256::from(5_207_710)),
-            _ => Err(format!("{} does not have an `EIP4844` activation block number! ", eth_chain_id).into()),
-        }
-    }
-
     pub fn is_active(&self, block: &EthBlock) -> Result<bool> {
         Ok(block.blob_gas_used.is_some())
     }
@@ -27,15 +15,6 @@ impl Eip4844 {
 mod tests {
     use super::*;
     use crate::test_utils::get_sample_eip4844_sepolia_submission_material;
-
-    #[test]
-    fn should_eip4844_get_activation_block() {
-        let eip4844 = Eip4844::new();
-        let chain_id = EthChainId::Sepolia;
-        let result = eip4844.get_activation_block_number(&chain_id).unwrap();
-        let expected_result = U256::from(5_207_710);
-        assert_eq!(result, expected_result);
-    }
 
     #[test]
     fn eip4844_should_be_active() {
