5fc043980ee22da56c3f9ef17041fd99abea1df5
gskapka <gregkapka@gmail.com>
2024-02-20 12:41:03 +0000

    feat(btc-on-int): <- swallow `BlockAlreadyInDb` errors in batch submitter in int side of that

--- v2_bridges/btc_on_int/src/int/submit_int_block.rs
+++ v2_bridges/btc_on_int/src/int/submit_int_block.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use common::{core_type::CoreType, traits::DatabaseInterface, types::Result};
+use common::{core_type::CoreType, traits::DatabaseInterface, types::Result, AppError};
 use common_eth::{
     check_for_parent_of_eth_block_in_state,
     maybe_add_eth_block_and_receipts_to_db_and_return_state,
@@ -75,10 +75,24 @@ pub fn submit_int_blocks_to_core<D: DatabaseInterface>(db: &D, blocks: &str) -> 
         .and_then(|_| db.start_transaction())
         .and_then(|_| EthSubmissionMaterialJsons::from_str(blocks))
         .and_then(|jsons| {
-            jsons
-                .iter()
-                .map(|block| submit_int_block(db, block))
-                .collect::<Result<Vec<_>>>()
+            let mut outputs = vec![];
+
+            for json in jsons.iter() {
+                match submit_int_block(db, json) {
+                    Ok(o) => {
+                        outputs.push(o);
+                        continue;
+                    },
+                    Err(AppError::BlockAlreadyInDbError(e)) => {
+                        warn!("block already in db error: {e}");
+                        info!("moving on to next block in batch!");
+                        continue;
+                    },
+                    Err(e) => return Err(e),
+                }
+            }
+
+            Ok(outputs)
         })
         .map(IntOutputs::new)
         .and_then(|outputs| {
@@ -250,5 +264,99 @@ mod tests {
 
         // NOTE: Check the tx is decodable...
         assert!(convert_hex_tx_to_btc_transaction(tx_info.btc_signed_tx).is_ok());
+    }
+
+    #[test]
+    fn should_batch_submit_int_blocks_successfully_even_if_one_already_in_db() {
+        // Init the BTC side...
+        let btc_pk = "93GJ65qHNjGFHzQVTzEEAdBS7vMxe3XASfWE8RUASSfd3EtfmzP";
+        let db = get_test_database();
+        let btc_db_utils = BtcDbUtils::new(&db);
+        let btc_state = BtcState::init(&db);
+        let btc_fee = 15;
+        let btc_difficulty = 1;
+        let btc_network = "Testnet";
+        let btc_canon_to_tip_length = 2;
+        let btc_block_0 = get_sample_btc_submission_material_json_str_n(0);
+        init_btc_core(
+            btc_state,
+            &btc_block_0,
+            btc_fee,
+            btc_difficulty,
+            btc_network,
+            btc_canon_to_tip_length,
+        )
+        .unwrap();
+
+        // NOTE: Overwrite the BTC private key fields since they're randomly generated upon init.
+        let btc_pk = BtcPrivateKey::from_wif(btc_pk).unwrap();
+        let address = btc_pk.to_p2pkh_btc_address();
+        btc_db_utils.put_btc_private_key_in_db(&btc_pk).unwrap();
+        btc_db_utils.put_btc_address_in_db(&address).unwrap();
+        btc_db_utils
+            .put_btc_pub_key_slice_in_db(&btc_pk.to_public_key_slice())
+            .unwrap();
+
+        // Init the ETH side...
+        let eth_block_0 = get_sample_int_submission_material_json_str_n(0);
+        let eth_state = EthState::init(&db);
+        let eth_chain_id = 3;
+        let eth_gas_price = 20_000_000_000;
+        let eth_canon_to_tip_length = 2;
+        let ptoken_address_hex = "0x0f513aA8d67820787A8FDf285Bfcf967bF8E4B8b";
+        let ptoken_address = convert_hex_to_eth_address(ptoken_address_hex).unwrap();
+        let router_address_hex = "0x88d19e08cd43bba5761c10c588b2a3d85c75041f";
+        let router_address = convert_hex_to_eth_address(router_address_hex).unwrap();
+        init_int_core(
+            eth_state,
+            &eth_block_0,
+            eth_chain_id,
+            eth_gas_price,
+            eth_canon_to_tip_length,
+            &ptoken_address,
+            &router_address,
+        )
+        .unwrap();
+
+        // NOTE: Overwrite the ETH private key fields since they're randomly generated upon init.
+        let eth_db_utils = EthDbUtils::new(&db);
+        let eth_pk_bytes = hex::decode("262e2a3a7fa5ae40ea04584f20b51fc3918b42e7dd89926b9f4e2196c8a032ba").unwrap();
+        let eth_pk = EthPrivateKey::from_slice(&eth_pk_bytes).unwrap();
+        eth_db_utils.put_eth_private_key_in_db(&eth_pk).unwrap();
+        eth_db_utils
+            .put_public_eth_address_in_db(&eth_pk.to_public_key().to_address())
+            .unwrap();
+
+        // NOTE First we submit enough BTC blocks to have a UTXO to spend...
+        let btc_block_1 = get_sample_btc_submission_material_json_str_n(1);
+        submit_btc_block_to_core(&db, &btc_block_1).unwrap();
+        let btc_block_2 = get_sample_btc_submission_material_json_str_n(2);
+        submit_btc_block_to_core(&db, &btc_block_2).unwrap();
+        let btc_block_3 = get_sample_btc_submission_material_json_str_n(3);
+        submit_btc_block_to_core(&db, &btc_block_3).unwrap();
+        let utxo_nonce = get_utxo_nonce_from_db(&db).unwrap();
+        assert_eq!(utxo_nonce, 1);
+
+        // NOTE: Submit first block, this one has a peg in in it.
+        let int_block_1 = get_sample_int_submission_material_json_str_n(1);
+
+        let submission_string = int_block_1.clone();
+        let block_num = 12000342;
+
+        // NOTE: This totally normal submission should succeed
+        submit_int_block_to_core(&db, &submission_string).unwrap();
+
+        // NOTE: However it will fail a second time due to the block already being in the db...
+        match submit_int_block_to_core(&db, &submission_string) {
+            Ok(_) => panic!("should not have succeeded!"),
+            Err(AppError::BlockAlreadyInDbError(e)) => assert_eq!(e.block_num, block_num),
+            Err(e) => panic!("wrong error received: {e}"),
+        };
+
+        // NOTE: However if the same block forms part of a _batch_ of blocks, the
+        // `BlockAlreadyInDbError` should be swallowed, and thus no errors.
+        let batch = format!("[{submission_string},{submission_string},{submission_string}]");
+        let result = submit_int_blocks_to_core(&db, &batch);
+        assert!(result.is_ok());
     }
 }
