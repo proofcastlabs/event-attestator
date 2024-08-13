ab395de278b23856d79df01a092e291fd0cb4055
Ulisse Bordignon <ulisse.bordignon@protonmail.com>
2024-08-13 18:15:53 +0200

    ref(strongbox): get public key in uncompressed format

--- apps/strongbox/src/android/handlers/get_public_key.rs
+++ apps/strongbox/src/android/handlers/get_public_key.rs
@@ -5,7 +5,7 @@ use serde_json::json;
 use crate::android::State;
 
 pub fn get_public_key(state: State) -> Result<State, SentinelError> {
-    let j = json!({"publicKey": ChainDbUtils::new(state.db()).get_public_key()?.public_key.to_string()});
+    let j = json!({"publicKey": format!("0x{}", hex::encode(ChainDbUtils::new(state.db()).get_public_key()?.public_key.serialize_uncompressed()))});
 
     let r = WebSocketMessagesEncodable::Success(j);
     Ok(state.add_response(r))
